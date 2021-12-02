19cdc0513a0a6062be2df63f2e9625aca2a2bc4d
William Higgins <william.higgins@ncino.com>
2021-12-02 00:13:38 -0500

    - added Day 2 problems 1 and 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent Of Code 2021
-To run all problems:
+Day 1: Used Karma test runner to solve the problem
 npm run test
+Day 2: Began writing in node. node js/DayX-Y.js
--- js/node/Day2-2.js
+++ js/node/Day2-2.js
@@ -6,15 +6,17 @@ const data = fs
 
 let horizontal = 0;
 let depth = 0;
+let aim = 0;
 
 data.forEach(function(element) {
 	let movement = element.split(' ');
 	if (movement[0] === 'forward') {
 		horizontal += parseInt(movement[1]);
+		depth += (aim * parseInt(movement[1]));
 	} else if (movement[0] === 'down') {
-		depth += parseInt(movement[1]);
+		aim += parseInt(movement[1]);
 	} else if (movement[0] === 'up') {
-		depth -= parseInt(movement[1]);
+		aim -= parseInt(movement[1]);
 	}
 });
 console.log(horizontal);
