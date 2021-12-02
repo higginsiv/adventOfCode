b4a889bb08cb5f2a4feb43a1fe7fa1b6c0989fac
William Higgins <william.higgins@ncino.com>
2021-12-02 10:33:52 -0500

    - reorg code - added readme with the Day's problems

--- js/Day2/2-1.js
+++ js/Day2/2-1.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 const data = fs
-	.readFileSync("./inputs/Day2.txt")
+	.readFileSync("./js/Day2/input.txt")
 	.toString()
 	.split('\n');
 
 let horizontal = 0;
 let depth = 0;
-let aim = 0;
 
 data.forEach(function(element) {
 	let movement = element.split(' ');
 	if (movement[0] === 'forward') {
 		horizontal += parseInt(movement[1]);
-		depth += (aim * parseInt(movement[1]));
 	} else if (movement[0] === 'down') {
-		aim += parseInt(movement[1]);
+		depth += parseInt(movement[1]);
 	} else if (movement[0] === 'up') {
-		aim -= parseInt(movement[1]);
+		depth -= parseInt(movement[1]);
 	}
 });
 console.log(horizontal);
--- js/Day2/2-2.js
+++ js/Day2/2-2.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 const data = fs
-	.readFileSync("./inputs/Day2.txt")
+	.readFileSync("./js/Day2/input.txt")
 	.toString()
 	.split('\n');
 
 let horizontal = 0;
 let depth = 0;
+let aim = 0;
 
 data.forEach(function(element) {
 	let movement = element.split(' ');
 	if (movement[0] === 'forward') {
 		horizontal += parseInt(movement[1]);
+		depth += (aim * parseInt(movement[1]));
 	} else if (movement[0] === 'down') {
-		depth += parseInt(movement[1]);
+		aim += parseInt(movement[1]);
 	} else if (movement[0] === 'up') {
-		depth -= parseInt(movement[1]);
+		aim -= parseInt(movement[1]);
 	}
 });
 console.log(horizontal);
