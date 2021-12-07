713eb3bb755df64abd951994b16fd5ff3a4e246f
William Higgins <william.higgins@ncino.com>
2021-12-07 00:59:23 -0500

    - added explanation of summation formula

--- js/Day7/problem2.js
+++ js/Day7/problem2.js
@@ -7,6 +7,7 @@ let bestDelta = null;
 for (let i = data[0]; i < data[data.length - 1]; i++) {
 	const delta = data.reduce((previous, current) => {
 		const dif = Math.abs(current - i);
+		// formula for sum 1..n is n(n+1) * .5
 		const summation = dif * (dif + 1) * .5;
 		return previous += summation;
 	}, 0);
