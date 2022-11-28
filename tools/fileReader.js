93661c9270ea198433b664625ec1746d75dd5b94
William Higgins <higginsiv@gmail.com>
2022-11-28 15:19:41 -0500

    updated default split param to be OS agnostic

--- tools/fileReader.js
+++ tools/fileReader.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 module.exports = {
-	getInput: function (year, dayNumber, delimiter = '\n', inputFile = 'input.txt') {
+	getInput: function (year, dayNumber, delimiter = /\r?\n/, inputFile = 'input.txt') {
 		return fs.readFileSync("./" + year + "/js/Day" + dayNumber + "/" + inputFile)
 			.toString()
 			.split(delimiter);
