4a348569ac25f2d26d6289380d62b48033fcb224
William Higgins <higginsiv@gmail.com>
2022-12-01 00:38:45 -0500

    puzzle 1 complete

--- 2022/js/Day01/problem1.js
+++ 2022/js/Day01/problem1.js
@@ -1,5 +1,24 @@
-const fr = require('../../tools/fileReader');
-const data = fr.getInput("2021",null);
+const fr = require('../../../tools/fileReader');
+const {EOL} = require('os');
 
+const year = "2022";
+const day = "01";
+const data = fr.getInput(year,day);
 
-console.log('Day _ Puzzle 1: ' );
+let elfCalories = [0];
+let elfIndex = 0;
+let calorieMax = 0;
+
+data.forEach((element) => {
+    if (element == '') {
+        if (elfCalories[elfIndex] > calorieMax) {
+            calorieMax = elfCalories[elfIndex];
+        }
+        elfIndex++;
+        elfCalories[elfIndex] = 0
+    } else {
+        elfCalories[elfIndex] += parseInt(element);
+    }
+})
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle 1: ' + calorieMax);
--- tools/fileReader.js
+++ tools/fileReader.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const {EOL} = require('os');
 module.exports = {
-	getInput: function (year, dayNumber, delimiter = /\r?\n/, inputFile = 'input.txt') {
+	getInput: function (year, dayNumber, delimiter = EOL, inputFile = 'input.txt') {
 		return fs.readFileSync("./" + year + "/js/Day" + dayNumber + "/" + inputFile)
 			.toString()
 			.split(delimiter);
