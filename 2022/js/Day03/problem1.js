5ece10b92e26330d7f7a0a7e3c43d3988b8cad7d
William Higgins <higginsiv@gmail.com>
2022-12-03 00:54:32 -0500

    Day03 problem2 renamed variables

--- 2022/js/Day03/problem1.js
+++ 2022/js/Day03/problem1.js
@@ -6,13 +6,13 @@ const data = fr.getInput(year,day);
 let priority = 0;
 data.forEach(sack => {
     let [comp1, comp2] = [sack.slice(0, sack.length/2), sack.slice(sack.length/2)];
-    let itemType = comp1.split('').find(item => {
+    let misplacedType = comp1.split('').find(item => {
        return comp2.indexOf(item) !== -1
     });
 
-    let offset = itemType == itemType.toUpperCase() ? 38 : 96;
+    let offset = misplacedType == misplacedType.toUpperCase() ? 38 : 96;
 
-    priority += (itemType.charCodeAt(0) - offset);
+    priority += (misplacedType.charCodeAt(0) - offset);
 })
 
 console.log('Year ' + year + ' Day ' + day + ' Puzzle 1: ' + priority);
