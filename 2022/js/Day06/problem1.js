af0d56f13c46b7d5a73ee5d44e3e6514eae27b29
William Higgins <higginsiv@gmail.com>
2022-12-06 01:38:45 -0500

    code cleanup

--- 2022/js/Day06/problem1.js
+++ 2022/js/Day06/problem1.js
@@ -6,13 +6,17 @@ let answer;
 
 const groupSize = 4;
 for (let i = 0; i < data.length; i++) {
-    let currentGroup = data.slice(i, i + groupSize);
-    if (currentGroup.every((current, index) => {
-        return currentGroup.lastIndexOf(current) === index;
-    })) {
-        answer = i + groupSize;
-        break;
+    if (
+      data
+        .slice(i, i + groupSize)
+        .every(
+          (current, index, currentGroup) =>
+            currentGroup.lastIndexOf(current) === index
+        )
+    ) {
+      answer = i + groupSize;
+      break;
     }
-}
+  }
 
 console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
