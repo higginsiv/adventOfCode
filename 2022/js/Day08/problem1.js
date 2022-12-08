2c90d895a8dc0e665ad555bac013eb8c56b5d286
William Higgins <higginsiv@gmail.com>
2022-12-08 01:33:32 -0500

    day 08

--- 2022/js/Day08/problem1.js
+++ 2022/js/Day08/problem1.js
@@ -1,6 +1,43 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ["2022","08","1"];
-const data = fr.getInput(year,day);
+const fr = require("../../../tools/fileReader");
+const [year, day, part] = ["2022", "08", "1"];
+const data = fr
+  .getInput(year, day)
+  .map((x) => x.split("").map((y) => parseInt(y)));
 
-let answer;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+let numVisible = 0;
+
+for (let x = 0; x < data.length; x++) {
+  for (let y = 0; y < data[x].length; y++) {
+    if (
+      check(x, y, 1, 0, data[x][y], -Infinity) ||
+      check(x, y, -1, 0, data[x][y], -Infinity) ||
+      check(x, y, 0, 1, data[x][y], -Infinity) ||
+      check(x, y, 0, -1, data[x][y], -Infinity)
+    ) {
+      numVisible++;
+    }
+  }
+}
+
+function check(x, y, xDelta, yDelta, originalHeight, newHeight) {
+  if (newHeight >= originalHeight) {
+    return false;
+  }
+
+  if (x === 0 || x === data.length - 1 || y === 0 || y === data[x].length - 1) {
+    return true;
+  }
+
+  return check(
+    x + xDelta,
+    y + yDelta,
+    xDelta,
+    yDelta,
+    originalHeight,
+    data[x + xDelta][y + yDelta]
+  );
+}
+
+console.log(
+  "Year " + year + " Day " + day + " Puzzle " + part + ": " + numVisible
+);
