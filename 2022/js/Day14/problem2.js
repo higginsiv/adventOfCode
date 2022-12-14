6862768cee38613549af72355e41e2f823020fce
William Higgins <higginsiv@gmail.com>
2022-12-14 15:20:06 -0500

    cleanup

--- 2022/js/Day14/problem2.js
+++ 2022/js/Day14/problem2.js
@@ -12,8 +12,6 @@ const sandSource = [500, 0];
 
 let cave = [];
 let highestRock = 0;
-let leftestRock = Infinity;
-let rightestRock = -Infinity;
 let sandFallen = 0;
 
 data.forEach((path) => {
@@ -26,14 +24,6 @@ data.forEach((path) => {
 
     if (highy > highestRock) {
       highestRock = highy;
-    }
-
-    if (lowx < leftestRock) {
-      leftestRock = lowx;
-    }
-
-    if (highx > rightestRock) {
-      rightestRock = highx;
     }
 
     if (p1x === p2x) {
@@ -57,42 +47,24 @@ data.forEach((path) => {
 });
 
 highestRock += 2;
-console.log(leftestRock);
-console.log(rightestRock);
-console.log(highestRock);
-cave[highestRock] = [];
-for (let i = leftestRock - 1; i <= rightestRock + 100; i++) {
-  cave[highestRock][i] = ROCK;
-}
 
-console.log(sandSource[0]);
-console.log(sandSource[1]);
 function sandFall(x, y) {
-//   console.log(x + " " + y);
-  if (y >= highestRock) {
-    return VOID;
-  }
   if (isEmpty(x, y + 1)) {
-    return sandFall(x, y + 1, x, y);
-  } else if (isEmpty(x - 1, y + 1, x, y)) {
-    return sandFall(x - 1, y + 1, x, y);
-  } else if (isEmpty(x + 1, y + 1, x, y)) {
-    return sandFall(x + 1, y + 1, x, y);
-  } else if (x === sandSource[0] && y === sandSource[1]) {
-    sandFallen++;
-    if (cave[y] == null) {
-        cave[y] = [];
-    }
-    cave[y][x] = SAND;
-    return CLOGGED;
+    return sandFall(x, y + 1);
+  } else if (isEmpty(x - 1, y + 1)) {
+    return sandFall(x - 1, y + 1);
+  } else if (isEmpty(x + 1, y + 1)) {
+    return sandFall(x + 1, y + 1);
   } else {
     sandFallen++;
-    // console.log(sandFallen);
     if (cave[y] == null) {
         cave[y] = [];
     }
     cave[y][x] = SAND;
-    return [prevX, prevY];
+    if (x === sandSource[0] && y === sandSource[1]) {
+        return CLOGGED;
+    }
+    return MORE;
   }
 }
 
@@ -130,6 +102,6 @@ function printCave() {
 }
 
 dropSand();
-printCave();
+// printCave();
 let answer = sandFallen;
 console.log("Year " + year + " Day " + day + " Puzzle " + part + ": " + answer);
