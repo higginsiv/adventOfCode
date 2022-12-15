eed210fe6077e82d4743c7a3df1c600c6e4f185a
William Higgins <higginsiv@gmail.com>
2022-12-15 11:00:36 -0500

    clean up

--- 2022/js/Day15/problem1.js
+++ 2022/js/Day15/problem1.js
@@ -17,7 +17,7 @@ data.forEach((x, index) => {
         beaconsOnRow.add(beaconX);
     }
     const distance = getDistance(sensorX, sensorY, beaconX, beaconY);
-    // console.log(index);
+
     if (sensorY < rowToCheck && (sensorY + distance >= rowToCheck)) {
         let rowSpan = sensorY + distance - rowToCheck;
         for (let i = sensorX - rowSpan; i <= sensorX + rowSpan; i++) {
