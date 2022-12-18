c809fb91ee263dfdcae033bf23bda965c7cd67b2
William Higgins <higginsiv@gmail.com>
2022-12-18 17:00:11 -0500

    day 17 p2

--- 2022/js/Day17/problem1.js
+++ 2022/js/Day17/problem1.js
@@ -1,8 +1,8 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ["2022","17","1"];
+const fr = require("../../../tools/fileReader");
+const [year, day, part] = ["2022", "17", "1"];
 
 const [LEFT, RIGHT] = [-1, 1];
-const data = fr.getInput(year,day,'').map(x => x === '<' ? LEFT : RIGHT);
+const data = fr.getInput(year, day, "").map((x) => (x === "<" ? LEFT : RIGHT));
 
 const [HOR, PLUS, EL, VERT, BOX] = [0, 1, 2, 3, 4];
 const SOLID = 1;
@@ -11,117 +11,121 @@ let highY = 0;
 let rowsDeleted = 0;
 
 class Point {
-    x;
-    y;
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  x;
+  y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 class Rock {
-    points = [];
-    constructor(leftEdge, highEdge, type) {
-        switch(type) {
-            case HOR:
-                this.points.push(
-                    new Point(leftEdge + 2, highEdge + 3),
-                    new Point(leftEdge + 3, highEdge + 3),
-                    new Point(leftEdge + 4, highEdge + 3),
-                    new Point(leftEdge + 5, highEdge + 3)
-                    );
-                break;
-            case PLUS:
-                this.points.push(
-                    new Point(leftEdge + 2, highEdge + 4),
-                    new Point(leftEdge + 3, highEdge + 3),
-                    new Point(leftEdge + 3, highEdge + 4),
-                    new Point(leftEdge + 3, highEdge + 5),
-                    new Point(leftEdge + 4, highEdge + 4)
-                );
-                break;
-            case EL:
-                this.points.push(
-                    new Point(leftEdge + 2, highEdge + 3),
-                    new Point(leftEdge + 3, highEdge + 3),
-                    new Point(leftEdge + 4, highEdge + 3),
-                    new Point(leftEdge + 4, highEdge + 4),
-                    new Point(leftEdge + 4, highEdge + 5)
-                );
-                break;
-            case VERT:
-                this.points.push(
-                    new Point(leftEdge + 2, highEdge + 3),
-                    new Point(leftEdge + 2, highEdge + 4),
-                    new Point(leftEdge + 2, highEdge + 5),
-                    new Point(leftEdge + 2, highEdge + 6)
-                );
-                break;
-            case BOX:
-                this.points.push(
-                    new Point(leftEdge + 2, highEdge + 3),
-                    new Point(leftEdge + 3, highEdge + 3),
-                    new Point(leftEdge + 2, highEdge + 4),
-                    new Point(leftEdge + 3, highEdge + 4)
-                );
-                break;
-                
-        }
+  points = [];
+  constructor(leftEdge, highEdge, type) {
+    switch (type) {
+      case HOR:
+        this.points.push(
+          new Point(leftEdge + 2, highEdge + 3),
+          new Point(leftEdge + 3, highEdge + 3),
+          new Point(leftEdge + 4, highEdge + 3),
+          new Point(leftEdge + 5, highEdge + 3)
+        );
+        break;
+      case PLUS:
+        this.points.push(
+          new Point(leftEdge + 2, highEdge + 4),
+          new Point(leftEdge + 3, highEdge + 3),
+          new Point(leftEdge + 3, highEdge + 4),
+          new Point(leftEdge + 3, highEdge + 5),
+          new Point(leftEdge + 4, highEdge + 4)
+        );
+        break;
+      case EL:
+        this.points.push(
+          new Point(leftEdge + 2, highEdge + 3),
+          new Point(leftEdge + 3, highEdge + 3),
+          new Point(leftEdge + 4, highEdge + 3),
+          new Point(leftEdge + 4, highEdge + 4),
+          new Point(leftEdge + 4, highEdge + 5)
+        );
+        break;
+      case VERT:
+        this.points.push(
+          new Point(leftEdge + 2, highEdge + 3),
+          new Point(leftEdge + 2, highEdge + 4),
+          new Point(leftEdge + 2, highEdge + 5),
+          new Point(leftEdge + 2, highEdge + 6)
+        );
+        break;
+      case BOX:
+        this.points.push(
+          new Point(leftEdge + 2, highEdge + 3),
+          new Point(leftEdge + 3, highEdge + 3),
+          new Point(leftEdge + 2, highEdge + 4),
+          new Point(leftEdge + 3, highEdge + 4)
+        );
+        break;
     }
+  }
 }
 
 function moveRock(rock, edge, xDir, yDir) {
-    let tempPoints = [];
-    for (let i = 0; i < rock.points.length; i++) {
-        let p = rock.points[i];
-        let tempX = p.x + xDir;
-        let tempY = p.y + yDir;
+  let tempPoints = [];
+  for (let i = 0; i < rock.points.length; i++) {
+    let p = rock.points[i];
+    let tempX = p.x + xDir;
+    let tempY = p.y + yDir;
 
-        if (tempX < 0 || tempX > 6 || tempY < 0 || (edge[tempY] != null && edge[tempY][tempX] === SOLID)) {
-            return rock;
-        } else {
-            tempPoints.push(new Point(tempX, tempY));
-        }
+    if (
+      tempX < 0 ||
+      tempX > 6 ||
+      tempY < 0 ||
+      (edge[tempY] != null && edge[tempY][tempX] === SOLID)
+    ) {
+      return rock;
+    } else {
+      tempPoints.push(new Point(tempX, tempY));
     }
+  }
 
-    rock.points = tempPoints;
-    return rock;
+  rock.points = tempPoints;
+  return rock;
 }
 
 function getEdge(rockPoints) {
-    rockPoints.forEach(p => {
-        if (p.y + 1> highY) {
-            highY = p.y + 1
-        }
-        if (edge[p.y] == null) {
-            edge[p.y] = [];
-        }
-        edge[p.y][p.x] = SOLID;
-    })
-
-    let rowsToDelete = 0
-    for (let i = 0; i < highY; i++) {
-        let deleteRow = true;
-        for (let j = 0; j < 7; j++) {
-            if (edge[i][j] !== SOLID) {
-                deleteRow = false;
-                break;
-            }
-        }
-        if (deleteRow) {
-            rowsToDelete++;
-        } else {
-            break;
-        }
+  rockPoints.forEach((p) => {
+    if (p.y + 1 > highY) {
+      highY = p.y + 1;
     }
-
-    for (let i = 0; i < rowsToDelete; i++) {
-        edge.shift();
-        highY--;
-        rowsDeleted++;
+    if (edge[p.y] == null) {
+      edge[p.y] = [];
     }
+    edge[p.y][p.x] = SOLID;
+  });
 
-    return edge;
+  let rowsToDelete = 0;
+  for (let i = 0; i < highY; i++) {
+    let deleteRow = true;
+    for (let j = 0; j < 7; j++) {
+      if (edge[i][j] !== SOLID) {
+        deleteRow = false;
+        break;
+      }
+    }
+    if (deleteRow) {
+      rowsToDelete++;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = 0; i < rowsToDelete; i++) {
+    edge.shift();
+    highY--;
+    rowsDeleted++;
+  }
+
+  return edge;
 }
 
 const maxRocks = 2022;
@@ -130,30 +134,28 @@ let rockType = 0;
 let edge = [];
 edge[0] = [];
 
-let print = 1
-let dn = 0;
 for (let i = 0; i < maxRocks; i++) {
-    let rock = new Rock(0, highY, rockType);
+  let rock = new Rock(0, highY, rockType);
 
-    let moving = true;
-    while (moving) {
-        let dir = data.shift();
-        data.push(dir);
+  let moving = true;
+  while (moving) {
+    let dir = data.shift();
+    data.push(dir);
 
-        rock = moveRock(rock, edge, dir, 0);
+    rock = moveRock(rock, edge, dir, 0);
 
-        let ogX = rock.points[0].x;
-        let ogY = rock.points[0].y;
-        rock = moveRock(rock, edge, 0, -1);
+    let ogX = rock.points[0].x;
+    let ogY = rock.points[0].y;
+    rock = moveRock(rock, edge, 0, -1);
 
-        if (rock.points[0].x === ogX && rock.points[0].y === ogY) {    
-            moving = false;
-            edge = getEdge(rock.points);
-        }
+    if (rock.points[0].x === ogX && rock.points[0].y === ogY) {
+      moving = false;
+      edge = getEdge(rock.points);
     }
+  }
 
-    rockType = (rockType + 1) % 5;
+  rockType = (rockType + 1) % 5;
 }
 
 let answer = highY + rowsDeleted;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.log("Year " + year + " Day " + day + " Puzzle " + part + ": " + answer);
