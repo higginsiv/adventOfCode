9bd977bc21ac2c495f583b2a38288bf97942f6f7
William Higgins <higginsiv@gmail.com>
2022-12-19 15:51:24 -0500

    slightly cleaner 19 p1

--- 2022/js/Day19/problem1.js
+++ 2022/js/Day19/problem1.js
@@ -52,77 +52,9 @@ const NO_BUILD = 4;
 const MAX_MIN = 24;
 const ORE_BOT_MAX = 10;
 const CLAY_BOT_MAX = 15;
-const NO_BUILD_MAX = 10;
-
-function build(
-  print
-) {
-    console.log(print)
-    let [oreBots, clayBots, obsBots, geoBots] = [1, 0, 0, 0];
-    let [ore, clay, obs, geos] = [0,0,0,0];
-
-  for (let i = 0; i < MAX_MIN; i++) {
-
-    // setup build
-    let [geoBotMod, obsBotMod, clayBotMod, oreBotMod] = [0, 0, 0, 0];
-    if (ore >= print.geoBotOreCost && obs >= print.geoBotObsCost) {
-      // build Geo Bot
-      ore -= print.geoBotOreCost;
-      obs -= print.geoBotObsCost;
-      geoBotMod++;
-    } else if (
-      ore >= print.obsBotOreCost &&
-      clay >= print.obsBotClayCost &&
-      obsBots < print.geoBotObsCost
-    ) {
-      // build Obs Bot
-      ore -= print.obsBotOreCost;
-      clay -= print.obsBotClayCost;
-      obsBotMod++;
-    } else if (ore >= print.clayBotCost && clayBots < print.obsBotClayCost) {
-      // build clay bot
-      ore -= print.clayBotCost;
-      clayBotMod++;
-    } else if (
-      ore >= print.oreBotCost &&
-      (oreBots < print.geoBotOreCost ||
-        oreBots < print.obsBotOreCost ||
-        oreBots < print.clayBotCost)
-    ) {
-      // build ore bot
-      ore -= print.oreBotCost;
-      oreBotMod++;
-    }
-
-    // collect resources
-    geos += geoBots;
-    obs += obsBots;
-    clay += clayBots;
-    ore += oreBots;
-
-    // finish build
-    geoBots += geoBotMod;
-    obsBots += obsBotMod;
-    clayBots += clayBotMod;
-    oreBots += oreBotMod;
-
-    console.log('*******************');
-    console.log(i + 1)
-    console.log('ore: ' + ore + ' bots: ' + oreBots);
-    console.log('clay: ' + clay + ' bots: ' + clayBots);
-    console.log('obs: ' + obs + ' bots: ' + obsBots);
-    console.log('geo: ' + geos + ' bots: ' + geoBots);
-    console.log('*******************')
-  }
-  return geos;
-}
 
 let sum = data.reduce((sum, curr) => {
-    // let result = build(curr)
     let best = 0;
-    // for (let obm = 1; obm <= ORE_BOT_MAX; obm++) {
-        // for (let cbm = 1; cbm <= CLAY_BOT_MAX; cbm++) {
-            // console.log(curr)
             let result = buildR2(
                 curr,
                 1,
@@ -140,22 +72,11 @@ let sum = data.reduce((sum, curr) => {
                 if (result > best) {
                     best = result;
                 }
-        // }
-    // }
-    
-    console.log(best)
 
     return sum + (best * curr.id)
 }, 0);
 
 console.log("Year " + year + " Day " + day + " Puzzle " + part + ": " + sum);
-
-// what is possible based on bots I have?
-// ff to min required for that possibility
-// plus fac * (time gap + 1)
-// collect resources
-// what is possible?
-
 
 function buildR2(
     print,
@@ -171,7 +92,6 @@ function buildR2(
     oreBotMax,
     clayBotMax
   ) {
-  //     // console.log('min: ' + minutes + ' ore: ' + ore)
     let options = [];
     if (minutes > MAX_MIN) {
       return geos;
@@ -186,10 +106,6 @@ function buildR2(
       if (minutes + oreReqClayBot < MAX_MIN && clayBots < clayBotMax) {
           options.push([oreReqClayBot, CLAY_BOT]);
       }
-    //   if (minutes <= NO_BUILD_MAX) {
-    //     // TODO find oreboxmax and claybotmax
-    //     options.push([0, NO_BUILD])
-    //   }
     }
   
     if (clayBots > 0 && obsBots < print.geoBotObsCost) {
@@ -205,9 +121,7 @@ function buildR2(
       let oreReqGeoBot = Math.max(Math.ceil((print.geoBotOreCost - ore) / oreBots),0);
       let obsReqGeoBot = Math.max(Math.ceil((print.geoBotObsCost - obs) / obsBots),0);
       let timeCost = Math.max(oreReqGeoBot, obsReqGeoBot);
-        if (print.id === 1) {
-            // console.log('geoAttempt: ' + ore + ' ' + obs + ' ' + minutes + ' ' + timeCost)
-        }
+
       if (minutes + timeCost < MAX_MIN) {
           options.push([timeCost, GEO_BOT]);
       }
@@ -254,6 +168,3 @@ function buildR2(
     });
     return best;
 }
-
-// 1302 too low
-
