98915dc9b123ff8ef7a5a287b8845d02af000e7d
William Higgins <higginsiv@gmail.com>
2022-12-22 22:10:44 -0500

    2022 Day 22 P2 done

--- 2022/js/Day22/problem2.js
+++ 2022/js/Day22/problem2.js
@@ -3,11 +3,13 @@ const {EOL} = require('os');
 const [year, day, part] = ["2022","22","2"];
 const data = fr.getInput(year,day, EOL + EOL);
 
-const [FREE, WALL, VOID] = ['.', '#', ' '];
+const [FREE, WALL] = ['.', '#'];
 const [EAST, SOUTH, WEST, NORTH] = [0, 1, 2, 3];
 const [CLOCK, COUNTER] = ['R', 'L'];
 const [ONE, TWO, THREE, FOUR, FIVE, SIX] = [1, 2, 3, 4, 5, 6];
 const [SIZE, START, MAX] = [50, 0, 49];
+
+let startCol;
 
 let face1 = Array.from(Array(SIZE), x => Array(SIZE));
 let face2 = Array.from(Array(SIZE), x => Array(SIZE));
@@ -24,7 +26,7 @@ let faces = new Map(
     [5, face5],
     [6, face6]]
 )
-// console.log(face1);
+
 let grid = data[0].split(EOL).map(x => x.split(''));
 let moves = data[1].replaceAll(CLOCK, ' R ').replaceAll(COUNTER, ' L ').split(' ').map(x => {
     if (x === CLOCK || x === COUNTER) {
@@ -35,15 +37,11 @@ let moves = data[1].replaceAll(CLOCK, ' R ').replaceAll(COUNTER, ' L ').split(' 
 })
 
 buildCube();
-// printFace(3);
-// printFace(4);
-// printFace(5);
-// printFace(6);
 
 function buildCube() {
     grid = data[0].split(EOL).map(x => x.split(''));
 
-    let startCol = grid[0].indexOf(FREE) < grid[0].indexOf(WALL) && grid[0].indexOf(FREE) !== -1 ? grid[0].indexOf(FREE) : grid[0].indexOf(WALL);
+    startCol = grid[0].indexOf(FREE) < grid[0].indexOf(WALL) && grid[0].indexOf(FREE) !== -1 ? grid[0].indexOf(FREE) : grid[0].indexOf(WALL);
     for (let row = 0; row < SIZE; row++) {
         for (let col = 0; col < SIZE; col++) {
             face1[row][col] = grid[row][col + startCol];
@@ -92,7 +90,7 @@ function wrap() {
         } else if (pos.dir === WEST) {
             destFace = FOUR;
             destDir = EAST;
-            destRow = MAX - pos.col;
+            destRow = MAX - pos.row;
             destCol = START
         } else if (pos.dir === NORTH) {
             destFace = SIX;
@@ -126,7 +124,7 @@ function wrap() {
         if (pos.dir === EAST) {
             destFace = TWO;
             destDir = NORTH;
-            destRow = START;
+            destRow = MAX;
             destCol = pos.row;
         } else if (pos.dir === SOUTH) {
             destFace = FIVE;
@@ -158,7 +156,7 @@ function wrap() {
         } else if (pos.dir === WEST) {
             destFace = ONE;
             destDir = EAST;
-            destRow = MAX - ROW;
+            destRow = MAX - pos.row;
             destCol = START;
         } else if (pos.dir === NORTH) {
             destFace = THREE;
@@ -196,9 +194,9 @@ function wrap() {
             destCol = pos.row;
         } else if (pos.dir === SOUTH) {
             destFace = TWO;
-            destDir = WEST;
-            destRow = pos.col;
-            destCol = MAX;
+            destDir = SOUTH;
+            destRow = START;
+            destCol = pos.col;
         } else if (pos.dir === WEST) {
             destFace = ONE;
             destDir = SOUTH;
@@ -217,9 +215,27 @@ function wrap() {
         pos.row = destRow;
         pos.col = destCol;
         pos.dir = destDir;
-        return true;
+        return false;
     }
-    return false;
+    return true;
+}
+
+function mapRelativePosToAbsolute() {
+    if (pos.face === ONE) {
+        pos.col += startCol;
+    } else if (pos.face === TWO) {
+        pos.col += (startCol + SIZE);
+    } else if (pos.face === THREE) {
+        pos.col += startCol;
+        pos.row += SIZE;
+    } else if (pos.face === FOUR) {
+        pos.row += (SIZE + SIZE);
+    } else if (pos.face === FIVE) {
+        pos.row += (SIZE + SIZE);
+        pos.col += startCol;
+    } else {
+        pos.row += (SIZE + SIZE + SIZE);
+    }
 }
 
 class Position {
@@ -235,88 +251,68 @@ class Position {
     }
 }
 
-let pos = new Position(0, grid[0].indexOf(FREE), EAST, 1);
-moves.forEach(x => {
+let pos = new Position(0, faces.get(ONE)[0].indexOf(FREE), EAST, 1);
+moves.forEach((x, index) => {
     if (x === CLOCK) {
         pos.dir = (pos.dir + 1) % 4;
     } else if (x === COUNTER) {
         pos.dir = pos.dir - 1 < 0 ? 3 : pos.dir - 1;
-    } else if (pos.dir === EAST) {
+    } else {
         for (let i = 0; i < x; i++) {
-            let tarCol = pos.col + 1 >= grid[pos.row].length ? 0 : pos.col + 1;
-            let next = grid[pos.row][tarCol];
-            if (next === VOID || next == null) {
-                pos.col = grid[pos.row].indexOf(FREE) < grid[pos.row].indexOf(WALL) ? grid[pos.row].indexOf(FREE) : pos.col;
-                if (grid[pos.row].indexOf(FREE) > grid[pos.row].indexOf(WALL)) {
-                    break;
-                }
-            } else if (next === WALL) {
-                break;
-            } else {
-                pos.col = tarCol;
-            }
-        }
-    } else if (pos.dir === SOUTH) {
-        for (let i = 0; i < x; i++) {
-            let tarRow = pos.row + 1 >= grid.length ? 0 : pos.row + 1;
-            let next = grid[(pos.row + 1) % grid.length][pos.col];
-            if (next === VOID || next == null) {
-                for (let j = 0; j < grid.length; j++) {
-                    if (grid[j][pos.col] === WALL) {
+            if (pos.dir === EAST) {
+                    let tarCol = pos.col + 1;
+                    if (tarCol > MAX) {
+                        let result = wrap();
+                        if (result) {
+                            break;
+                        }
+                    } else if (faces.get(pos.face)[pos.row][tarCol] === WALL) {
                         break;
-                    } else if (grid[j][pos.col] === FREE) {
-                        pos.row = j;
+                    } else {
+                        pos.col = tarCol;
+                    }
+            } else if (pos.dir === SOUTH) {
+                    let tarRow = pos.row + 1;
+                    if (tarRow > MAX) {
+                        let result = wrap();
+                        if (result) {
+                            break;
+                        }
+                    } else if (faces.get(pos.face)[tarRow][pos.col] === WALL) {
                         break;
+                    } else {
+                        pos.row = tarRow;
+                    }
+            } else if (pos.dir === WEST) {
+                    let tarCol = pos.col - 1;
+                    if (tarCol < START) {
+                        let result = wrap();
+                        if (result) {
+                            break;
+                        }
+                    } else if (faces.get(pos.face)[pos.row][tarCol] === WALL) {
+                        break;
+                    } else {
+                        pos.col = tarCol;
+                    }
+            } else if (pos.dir === NORTH) {
+                    let tarRow = pos.row - 1;
+                    if (tarRow < START) {
+                        let result = wrap();
+                        if (result) {
+                            break;
+                        }
+                    } else if (faces.get(pos.face)[tarRow][pos.col] === WALL) {
+                        break;
+                    } else {
+                        pos.row = tarRow;
                     }
                 }
-            } else if (next === WALL) {
-                break;
-            } else {
-                pos.row = tarRow;
-            }
         }
-    } else if (pos.dir === WEST) {
-        for (let i = 0; i < x; i++) {
-            let tarCol = pos.col - 1 >= 0 ? pos.col - 1 : grid[pos.row].length - 1;
-            let next = grid[pos.row][tarCol];
-            if (next === VOID || next == null) {
-                pos.col = grid[pos.row].lastIndexOf(FREE) > grid[pos.row].lastIndexOf(WALL) ? grid[pos.row].lastIndexOf(FREE) : pos.col;
-                if (grid[pos.row].lastIndexOf(FREE) <= grid[pos.row].lastIndexOf(WALL)) {
-                    break;
-                }
-            } else if (next === WALL) {
-                break;
-            } else {
-                pos.col = tarCol;
-            }
-        }
-    } else if (pos.dir === NORTH) {
-        for (let i = 0; i < x; i++) {
-            let tarRow = pos.row - 1 >= 0 ? pos.row - 1 : grid.length - 1;
-            let next = grid[tarRow][pos.col];
-            if (next === VOID || next == null) {
-                for (let j = grid.length - 1; j >= 0; j--) {
-                    if (grid[j][pos.col] === WALL) {
-                        break;
-                    } else if (grid[j][pos.col] === FREE) {
-                        pos.row = j;
-                        break;
-                    }
-                }
-            } else if (next === WALL) {
-                break;
-            } else {
-                pos.row = tarRow;
-            }
-        }
-    }
+    } 
 });
+
+mapRelativePosToAbsolute();
 
 let answer = (1000 * (pos.row + 1)) + (4 * (pos.col + 1)) + pos.dir;
 console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-
-
-//  12
-//  3
-// 45
-// 6
