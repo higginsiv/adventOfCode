239e1fc2f8b89574e5a478ce3a294275b6dc1e81
William Higgins <higginsiv@gmail.com>
2022-12-30 01:46:32 -0500

    2020 06 1 & 2 : complete

---
+++ 2020/js/Day06/problem1.js
@@ -0,0 +1,17 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const { EOL } = require('os');
+const [year, day, part] = ["2020","06","1"];
+const data = fr.getInput(year,day, EOL + EOL).map(x => {
+    x = x.replaceAll(EOL, '');
+    x = x.split('');
+    x = new Set(x);
+    return x;
+});
+
+let answer = data.reduce((total, curr) => {
+    return total + curr.size;
+}, 0);
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
