efbb598664416e20fdf7d77f1bfea535f8b8f117
William Higgins <higginsiv@gmail.com>
2022-12-30 01:10:32 -0500

    2020 05 1 & 2 : complete

---
+++ 2020/js/Day05/problem2.js
@@ -0,0 +1,52 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2020","05","2"];
+const data = fr.getInput(year,day).map(x => x.split(''));
+const [FRONT, BACK, LEFT, RIGHT] = ['F', 'B', 'L', 'R'];
+const ROWS = 127;
+const COLS = 7;
+const ROW_MULT = 8;
+
+let answer;
+// this should probably be a map instead of reduce but fuck it I love copy pasta
+let allIds = data.reduce((ids, curr) => {
+    let rowMin = 0;
+    let rowMax = ROWS;
+    let colMin = 0;
+    let colMax = COLS;
+
+    while (rowMin !== rowMax || colMin !== colMax) {
+        let midRow = rowMin + Math.floor((rowMax - rowMin) / 2);
+        let midCol = colMin + Math.floor((colMax - colMin) / 2);
+        let dir = curr.shift();
+        switch(dir) {
+            case FRONT:
+                rowMax = midRow;
+                break;
+            case BACK:
+                rowMin = midRow + 1;
+                break;
+            case LEFT:
+                colMax = midCol;
+                break;
+            case RIGHT:
+                colMin = midCol + 1;
+                break;
+        }
+    }
+
+    let id = (rowMin * ROW_MULT) + colMin;
+    ids.push(id)
+    return ids;
+}, []);
+
+allIds.sort((a, b) => a - b);
+
+for (let i = 0; i < allIds.length; i = i + 2) {
+    if (allIds[i + 1] - allIds[i] > 1) {
+        answer = allIds[i] + 1;
+        break;
+    }
+}
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
