44400119ce5a9b493efb4d05af1d7508d288d328
William Higgins <higginsiv@gmail.com>
2022-12-31 00:44:12 -0500

    2020 09 1 & 2 : complete

---
+++ 2020/js/Day09/problem1.js
@@ -0,0 +1,29 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2020","09","1"];
+const data = fr.getInput(year,day).map(x => parseInt(x));
+const CHUNK_SIZE = 25;
+
+let answer;
+
+for (let i = CHUNK_SIZE; i < data.length; i++) {
+    let allGood = false;
+    for (let j = i - CHUNK_SIZE; j < i; j++) {
+        for (let k = j + 1; k < i; k++) {
+            if (data[j] + data[k] === data[i] && data[j] !== data[k]) {
+                allGood = true;
+                break;
+            }
+        }
+        if (allGood) {
+            break;
+        }
+    }
+    if (!allGood) {
+        answer = data[i];
+        break;
+    }
+}
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
---
+++ 2020/js/Day09/problem2.js
@@ -0,0 +1,35 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2020","09","2"];
+const data = fr.getInput(year,day).map(x => parseInt(x));
+const CHUNK_SIZE = 25;
+// derived from part 1
+const GOAL = 133015568;
+
+let index = 0;
+let total = 0;
+let smallest = Infinity;
+let largest = -Infinity;
+let offset = 0;
+
+while (true) {
+    let current = data[index + offset];
+    total += current;
+    smallest = Math.min(smallest, current);
+    largest = Math.max(largest, current);
+
+    if (total > GOAL) {
+        smallest = Infinity;
+        largest = -Infinity;
+        total = 0;
+        offset++;
+        index = 0;
+    } else if (total === GOAL) {
+        break;
+    }
+    index++;
+}
+
+let answer = smallest + largest;
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
