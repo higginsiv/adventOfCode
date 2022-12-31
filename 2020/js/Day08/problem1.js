d8f4cef17885891275d5f35bfe671e83a40ceb7b
William Higgins <higginsiv@gmail.com>
2022-12-31 00:13:52 -0500

    2020 08 1 & 2 : complete

---
+++ 2020/js/Day08/problem1.js
@@ -0,0 +1,44 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2020","08","1"];
+const [ACCUMULATE, JUMP, NONE] = ['acc', 'jmp', 'nop'];
+
+class Instruction {
+    operation;
+    argument;
+    executed = false;
+    constructor(command) {
+        let [operation, argument] = command.split(' ');
+        this.operation = operation;
+        this.argument = parseInt(argument);
+    }
+}
+
+const data = fr.getInput(year,day).map(x => new Instruction(x));
+
+let accumulator = 0;
+let index = 0;
+
+while (true) {
+    let instruction = data[index];
+    if (instruction.executed) {
+        break;
+    }
+    instruction.executed = true;
+
+    switch (instruction.operation) {
+        case ACCUMULATE:
+            accumulator += instruction.argument;
+            index++;
+            break;
+        case JUMP:
+            index += instruction.argument;
+            break;
+        case NONE:
+            index++;
+            break;
+    }
+}
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + accumulator);
+console.timeEnd();
---
+++ 2020/js/Day08/problem2.js
@@ -0,0 +1,95 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2020","08","2"];
+const [ACCUMULATE, JUMP, NONE] = ['acc', 'jmp', 'nop'];
+
+class Instruction {
+    operation;
+    argument;
+    executed = false;
+    constructor(command) {
+        let [operation, argument] = command.split(' ');
+        this.operation = operation;
+        this.argument = parseInt(argument);
+    }
+}
+
+class State {
+    accumulator;
+    index;
+    instructions;
+}
+const data = fr.getInput(year,day).map(x => new Instruction(x));
+
+let accumulator = 0;
+let index = 0;
+let visited = [];
+
+let fixed = JSON.parse(JSON.stringify(data));
+
+
+// generate list of possible instructions that need fixing
+while (true) {
+    let instruction = fixed[index];
+    visited.push(index);
+    if (instruction.executed) {
+        break;
+    }
+    instruction.executed = true;
+
+    operate(instruction);
+}
+
+let ended = false;
+
+index = 0;
+accumulator = 0;
+// Attempt each potential fix and break when one terminates
+for (let i = 0; i < visited.length; i++) {
+    if (ended) {
+        break;
+    }
+    index = 0;
+    accumulator = 0;
+    fixed = JSON.parse(JSON.stringify(data));
+    if (fixed[[visited[i]]].operation === ACCUMULATE) {
+        continue;
+    } else {
+        fixed[visited[i]].operation = getOppositeOperation(fixed[visited[i]].operation);
+        while (true) {
+            let instruction = fixed[index];
+            if (instruction.executed) {
+                break;
+            }
+            instruction.executed = true;
+        
+            operate(instruction);
+            if (index >= fixed.length) {
+                ended = true;
+                break;
+            }
+        }
+    }
+}
+
+function operate(instruction) {
+    switch (instruction.operation) {
+        case ACCUMULATE:
+            accumulator += instruction.argument;
+            index++;
+            break;
+        case JUMP:
+            index += instruction.argument;
+            break;
+        case NONE:
+            index++;
+            break;
+    }
+}
+
+function getOppositeOperation(operation) {
+    return operation === JUMP ? NONE : JUMP;
+}
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + accumulator);
+console.timeEnd();
