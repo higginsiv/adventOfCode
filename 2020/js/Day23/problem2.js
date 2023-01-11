46eaceb443389f395f929b645537bd4edc1ec6b1
William Higgins <william.higgins@ncino.com>
2023-01-11 17:39:25 -0500

    2020 23 2 : in progress : began linked list implementation

--- 2020/js/Day23/problem2.js
+++ 2020/js/Day23/problem2.js
@@ -1,21 +1,75 @@
 console.time();
 const fr = require('../../../tools/fileReader');
 const [year, day, part] = ["2020","23","2"];
-let data = fr.getInput(year,day, '').map(x => parseInt(x));
-let big = [...Array(1000000).keys()]
-big.splice(0, 9);
-data = data.concat(big);
-// for (let i = 10; i < 1000000; i++) {
-//     data.push(i);
-// }
+const MAX = 9;
 const MOVES = 10000000;
 
-let currIndex = 0;
+class Cup {
+    label;
+    previous;
+    next;
+    constructor(label, previous, next) {
+        this.label = label;
+        this.previous = previous;
+        this.next = next;
+    }
+}
+
+let labelToCup = new Map();
+let data = fr.getInput(year,day, '').map(x => parseInt(x));
+let big = [...Array(MAX + 1).keys()]
+big.splice(0, data.length);
+data = data.concat(big);
+
+// TODO work on building Cups and adding them to map
+data.forEach((x, index) => {
+    let previousIndex = index - 1 < 0 ? data.length - 1 : index - 1;
+    let nextIndex = index + 1 >= data.length ? 0 : index + 1;
+    let c = new Cup(x, labelToCup.get(nextIndex))
+    labelToCup.set(x, c);
+})
+
+let currCup = labelToCup.get(data[0]);
 for (let i = 0; i < MOVES; i++) {
+    let destination;
+    let invalidLabels = [];
+    let startOfTrio = currCup.next;
+    invalidLabels.push(startOfTrio.label, startOfTrio.next.label, startOfTrio.next.next.label);
+    destination = getDestinationCup(currCup.label - 1, invalidLabels);
+
+    // set next of current
+    let cupAfterTrio = currCup.next.next.next.next;
+    currCup.next = cupAfterTrio;
+    // cupAfterTrio.previous = currCup;
+    // make start of trio point back to destination cup
+    // startOfTrio.previous = destination;
+    // make end of trio point to cup formerly after destination
+    startOfTrio.next.next.next = destination.next;
+    // make cup formerly after destination point back to end of trio
+    // destination.next.previous = startOfTrio.next.next
+    // make trio follow destination cup
+    destination.next = startOfTrio;
+}
+
+function getDestinationCup(label, invalidLabels) {
+    let destinationCup = null;
+    while (destinationCup == null) {
+        label--;
+        if (label === 0) {
+            label = MAX
+        }
+        if (!invalidLabels.includes(label)) {
+            destinationCup = labelToCup.get(label);
+        }
+    }
+    return destinationCup;
+}
+let currIndex = 0;
+for (let i = 0; i < 20; i++) {
     if (i % 100000 === 0) {
         console.log(i)
-
     }
+    printCups(data)
     let curr = data[currIndex];
     let removed = data.splice(currIndex + 1, 3);
 
@@ -24,7 +78,7 @@ for (let i = 0; i < MOVES; i++) {
     while (index === -1) {
         cupToFind -= 1;
         if (cupToFind === 0) {
-            cupToFind = 1000000;
+            cupToFind = MAX;
         }
         index = data.indexOf(cupToFind);
     }
@@ -49,7 +103,17 @@ for (let i = 0; i < MOVES; i++) {
 // while (data[0] !== 1) {
 //     data.push(data.shift());
 // }
-
+function printCups(grid, numOnSide = 9) {
+    let string = ''
+    for (let i = 0; i < numOnSide; i++) {
+        string += (grid[i] + ',')
+    }
+    // string += '...'
+    // for (let i = numOnSide; i > 0; i--) {
+    //     string += (grid[grid.length - i] + ',')
+    // }
+    console.log(string)
+}
 let answer = data[data.indexOf(1) + 1] * data[data.indexOf(1) + 2];
 console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
 console.timeEnd();
