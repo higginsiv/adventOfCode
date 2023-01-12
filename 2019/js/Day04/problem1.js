74f71d929cb867a07d5d91055325e7fcc02d9fc9
William Higgins <william.higgins@ncino.com>
2023-01-12 15:01:02 -0500

    2019 4 1 & 2 : complete

---
+++ 2019/js/Day04/problem1.js
@@ -0,0 +1,30 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2019","04","1"];
+const [low, high] = fr.getInput(year,day,'-').map(x => parseInt(x));
+
+let answer = 0;
+for (let i = low; i <= high; i++) {
+	let pass = i.toString().split('');
+	let ascDigs = true;
+	let twoAdj = false;
+
+	let lastDig;
+	while (pass.length > 0) {
+		let dig = parseInt(pass.shift());
+		if (lastDig && dig < lastDig) {
+			ascDigs = false;
+			break;
+		}
+		if (lastDig === dig) {
+			twoAdj = true;
+		}
+		lastDig = dig;
+	}
+	if (ascDigs && twoAdj) {
+		answer++;
+	}
+}
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
---
+++ 2019/js/Day04/problem2.js
@@ -0,0 +1,38 @@
+console.time();
+const fr = require('../../../tools/fileReader');
+const [year, day, part] = ["2019","04","2"];
+const [low, high] = fr.getInput(year,day,'-').map(x => parseInt(x));
+
+let answer = 0;
+for (let i = low; i <= high; i++) {
+	let pass = i.toString().split('');
+	let ascDigs = true;
+	let twoAdj = false;
+	let twoAdjLocked = false;
+
+	let grandPaDig;
+	let lastDig;
+	while (pass.length > 0) {
+		let dig = parseInt(pass.shift());
+		if (lastDig && dig < lastDig) {
+			ascDigs = false;
+			break;
+		}
+		if (lastDig === dig && !twoAdjLocked) {
+			twoAdj = grandPaDig !== dig;
+		} else {
+			if (twoAdj) {
+				twoAdjLocked = true
+			}
+		}
+
+		grandPaDig = lastDig;
+		lastDig = dig;
+	}
+	if (ascDigs && (twoAdj || twoAdjLocked)) {
+		answer++;
+	}
+}
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
