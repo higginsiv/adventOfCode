1a234f10ed1257f14d56bf9c6452bf787a5bb043
William Higgins <higginsiv@gmail.com>
2023-01-15 11:34:30 -0500

    2019 10 2 : limited cleaning 2019 readme added

--- 2019/js/Day10/problem2.js
+++ 2019/js/Day10/problem2.js
@@ -1,3 +1,5 @@
+// In order to maximize reuse of existing part 10 code you are going to see a LOT of messy code
+
 console.time();
 const fr = require('../../../tools/fileReader');
 const math = require('../../../tools/math.js');
@@ -7,30 +9,19 @@ const DELIM = '|';
 const STROIDS_TO_STROY = 200;
 const [INVALID, VALID] = [0, 1];
 
-class Slope {
-    x;
-    y;
-    key;
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.key = x + DELIM + y;
-    }
-}
 let slopes = new Set();
 let posSlopes = new Set();
 let data = fr.getInput(year, day).map((x) => x.split(''));
+
 for (let y = 0; y < data.length; y++) {
     for (let x = data[0].length - 1; x >= 0; x--) {
         if (x === 0 && y === 0) {
             continue;
         } else if (x === 0) {
             slopes.add(0 + DELIM + 1);
-            // slopes.add(new Slope(0, 1))
             posSlopes.add(0 + DELIM + 1);
         } else if (y === 0) {
             slopes.add(1 + DELIM + 0);
-            // slopes.add(new Slope(1, 0))
             posSlopes.add(1 + DELIM + 0);
         } else {
             let gcd = math.getGCD(x, y);
@@ -39,7 +30,6 @@ for (let y = 0; y < data.length; y++) {
             slopes.add(reducedX + DELIM + reducedY);
             slopes.add(-reducedX + DELIM + reducedY);
             posSlopes.add(reducedX + DELIM + reducedY);
-            // posSlopes.add(-reducedX + DELIM + reducedY);
         }
     }
 }
@@ -80,10 +70,8 @@ stroidToStroids.forEach((value, key) => {
     }
 });
 
-// This relies on the fact that spreading a Set in JS will do so in insertion order
 posSlopes = [...posSlopes];
 
-// TRY SORT
 posSlopes.sort((a, b) => {
     let [ax, ay] = a.split(DELIM).map((x) => parseInt(x));
     let [bx, by] = b.split(DELIM).map((x) => parseInt(x));
@@ -96,41 +84,14 @@ posSlopes.sort((a, b) => {
     }
     return bSlope - aSlope;
 });
-printSlopes(posSlopes);
-// process.exit()
 
-// END TRY SORT
-let vertSlope = posSlopes.indexOf(0 + DELIM + 1);
-// console.log(posSlopes)
-// posSlopes.sort((a, b) => {
-//     let ia = posSlopes.indexOf(a);
-//     let ib = posSlopes.indexOf(b);
 
-//     if (ia === vertSlope) {
-//         return -1;
-//     } else if (ib === vertSlope) {
-//         return 1;
-//     } else {
-//         return (ib - vertSlope) - (ia - vertSlope)
-//     }
-// })
-console.log(bestKey);
-// console.log(posSlopes)
 let [stationX, stationY] = bestKey.split(DELIM).map((x) => parseInt(x));
-console.log(stationX + ' ' + stationY);
 
 let stroidsStroyed = 0;
 let lastX;
 let lastY;
-//TODO rotate effectively
-// straight up
-// -y +x
-// horizontal
-// +y + x
-// neg vert
-// +y -x
-// neg hor
-// -y -x
+
 while (stroidsStroyed < STROIDS_TO_STROY) {
     // Straight up to horizontal
     for (let i = 0; i < posSlopes.length; i++) {
@@ -143,24 +104,10 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
             -slopeY
         );
         if (code === VALID) {
-            console.log(
-                'stroid: ' +
-                    (stroidsStroyed + 1) +
-                    ' slope x: ' +
-                    slopeX +
-                    ' slopeY: ' +
-                    slopeY +
-                    ' x: ' +
-                    x +
-                    ' y: ' +
-                    y
-            );
-
             stroidsStroyed++;
             lastX = x;
             lastY = y;
             data[y][x] = EMPTY;
-            // break;
         }
         if (stroidsStroyed === STROIDS_TO_STROY) {
             break;
@@ -183,24 +130,10 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
             slopeY
         );
         if (code === VALID) {
-            console.log(
-                'stroid: ' +
-                    (stroidsStroyed + 1) +
-                    ' slope x: ' +
-                    slopeX +
-                    ' slopeY: ' +
-                    slopeY +
-                    ' x: ' +
-                    x +
-                    ' y: ' +
-                    y
-            );
-
             stroidsStroyed++;
             lastX = x;
             lastY = y;
             data[y][x] = EMPTY;
-            // break;
         }
         if (stroidsStroyed === STROIDS_TO_STROY) {
             break;
@@ -223,24 +156,10 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
             slopeY
         );
         if (code === VALID) {
-            console.log(
-                'stroid: ' +
-                    (stroidsStroyed + 1) +
-                    ' slope x: ' +
-                    slopeX +
-                    ' slopeY: ' +
-                    slopeY +
-                    ' x: ' +
-                    x +
-                    ' y: ' +
-                    y
-            );
-
             stroidsStroyed++;
             lastX = x;
             lastY = y;
             data[y][x] = EMPTY;
-            // break;
         }
         if (stroidsStroyed === STROIDS_TO_STROY) {
             break;
@@ -250,7 +169,7 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
         break;
     }
 
-        // Straight Left to Straight up
+    // Straight Left to Straight up
 
     // Subtract 2 to skip over the horizontal slope so we don't hit it twice in a row. Stop at 1 so we don't hit the vertical slope twice
     for (let i = posSlopes.length - 2; i >= 1; i--) {
@@ -263,24 +182,10 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
             -slopeY
         );
         if (code === VALID) {
-            console.log(
-                'stroid: ' +
-                    (stroidsStroyed + 1) +
-                    ' slope x: ' +
-                    slopeX +
-                    ' slopeY: ' +
-                    slopeY +
-                    ' x: ' +
-                    x +
-                    ' y: ' +
-                    y
-            );
-
             stroidsStroyed++;
             lastX = x;
             lastY = y;
             data[y][x] = EMPTY;
-            // break;
         }
         if (stroidsStroyed === STROIDS_TO_STROY) {
             break;
@@ -291,147 +196,6 @@ while (stroidsStroyed < STROIDS_TO_STROY) {
     }
 }
 
-// while (stroidsStroyed < STROIDS_TO_STROY) {
-//     // Straight up
-//     let [slopeX, slopeY] = [0, -1];
-//     let [code, x, y] = findFirstOnSlope2(stationX + slopeX, stationY + slopeY, slopeX, slopeY);
-//     if (code === VALID) {
-//         console.log('x: ' + x + ' y: ' + y)
-//         stroidsStroyed++;
-//         lastX = x;
-//         lastY = y;
-//         data[y][x] = EMPTY
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-//     // Negative Y, Plus X
-//     for (let i = posSlopes.length - 1; i >= 0; i--) {
-//         let slope = posSlopes[i];
-//         let [slopeX, slopeY] = slope.split(DELIM).map(p => parseInt(p));
-//         let [code, x, y] = findFirstOnSlope2(stationX + slopeX, stationY - slopeY, slopeX, -slopeY);
-//         if (code === VALID) {
-//         console.log('slope x: ' + slopeX + ' slopeY: ' + slopeY + ' x: ' + x + ' y: ' + y)
-
-//             stroidsStroyed++;
-//             lastX = x;
-//             lastY = y;
-//             data[y][x] = EMPTY
-//             // break;
-//         }
-//         if (stroidsStroyed === STROIDS_TO_STROY) {
-//             break;
-//         }
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-//     // Horizontal
-//     [slopeX, slopeY] = [1, 0];
-//     [code, x, y] = findFirstOnSlope2(stationX + slopeX, stationY + slopeY, slopeX, slopeY);
-//     if (code === VALID) {
-//         console.log('x: ' + x + ' y: ' + y)
-
-//         stroidsStroyed++;
-//         lastX = x;
-//         lastY = y;
-//         data[y][x] = EMPTY
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-//     // Plus Y, Plus X
-//     for (let i = 0; i < posSlopes.length; i++) {
-//         let slope = posSlopes[i];
-//         let [slopeX, slopeY] = slope.split(DELIM).map(p => parseInt(p));
-//         let [code, x, y] = findFirstOnSlope2(stationX + slopeX, stationY + slopeY, slopeX, slopeY);
-//         if (code === VALID) {
-//             console.log('x: ' + x + ' y: ' + y)
-
-//             stroidsStroyed++;
-//             lastX = x;
-//             lastY = y;
-//             data[y][x] = EMPTY
-//         }
-//         if (stroidsStroyed === STROIDS_TO_STROY) {
-//             break;
-//         }
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-
-//     // Straight down
-//     [slopeX, slopeY] = [0, 1];
-//     [code, x, y] = findFirstOnSlope2(stationX + slopeX, stationY + slopeY, slopeX, slopeY);
-//     if (code === VALID) {
-//         console.log('x: ' + x + ' y: ' + y)
-
-//         stroidsStroyed++;
-//         lastX = x;
-//         lastY = y;
-//         data[y][x] = EMPTY
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-//     // Plus Y, Negative X
-//     for (let i = posSlopes.length - 1; i >= 0; i--) {
-//         let slope = posSlopes[i];
-//         let [slopeX, slopeY] = slope.split(DELIM).map(p => parseInt(p));
-//         let [code, x, y] = findFirstOnSlope2(stationX - slopeX, stationY + slopeY, -slopeX, slopeY);
-//         if (code === VALID) {
-//         console.log('x: ' + x + ' y: ' + y)
-
-//             stroidsStroyed++;
-//             lastX = x;
-//             lastY = y;
-//             data[y][x] = EMPTY
-
-//         }
-//         if (stroidsStroyed === STROIDS_TO_STROY) {
-//             break;
-//         }
-//     }
-
-//     // Straight left
-//     [slopeX, slopeY] = [-1, 0];
-//     [code, x, y] = findFirstOnSlope2(stationX - slopeX, stationY + slopeY, slopeX, slopeY);
-//     if (code === VALID) {
-//         console.log('x: ' + x + ' y: ' + y)
-
-//         stroidsStroyed++;
-//         lastX = x;
-//         lastY = y;
-//         data[y][x] = EMPTY
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-
-// // Negative Y, Negative X
-// for (let i = 0; i < posSlopes.length; i++) {
-//     let slope = posSlopes[i];
-//     let [slopeX, slopeY] = slope.split(DELIM).map(p => parseInt(p));
-//     let [code, x, y] = findFirstOnSlope2(stationX - slopeX, stationY - slopeY, -slopeX, -slopeY);
-//     if (code === VALID) {
-//     console.log('x: ' + x + ' y: ' + y)
-
-//         stroidsStroyed++;
-//         lastX = x;
-//         lastY = y;
-//         data[y][x] = EMPTY
-
-//     }
-//     if (stroidsStroyed === STROIDS_TO_STROY) {
-//         break;
-//     }
-// }
-// if (stroidsStroyed === STROIDS_TO_STROY) {
-//     break;
-// }
-
-// }
 
 function findFirstOnSlope(x, y, slopeX, slopeY) {
     if (y >= data.length || x >= data[0].length || y < 0 || x < 0) {
@@ -443,6 +207,8 @@ function findFirstOnSlope(x, y, slopeX, slopeY) {
     }
 }
 
+// I didn't feel like refactoring the first findFirstOnSlope to account for this use case
+// because it was very later. Now that the problem is done I feel even less like refactoring
 function findFirstOnSlope2(x, y, slopeX, slopeY) {
     if (y >= data.length || x >= data[0].length || y < 0 || x < 0) {
         return [INVALID];
@@ -452,10 +218,7 @@ function findFirstOnSlope2(x, y, slopeX, slopeY) {
         return findFirstOnSlope2(x + slopeX, y + slopeY, slopeX, slopeY);
     }
 }
+
 let answer = lastX * 100 + lastY;
 console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
 console.timeEnd();
-
-function printSlopes(slopes) {
-    slopes.forEach((x) => console.log(x));
-}
