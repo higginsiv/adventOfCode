b329a3eb5104a14074686446521c5403960d4a79
William Higgins <higginsiv@gmail.com>
2023-02-05 20:19:57 -0500

    m

--- 2019/js/Day15/problem1.js
+++ 2019/js/Day15/problem1.js
@@ -6,6 +6,7 @@ const ic = require('../common/IntCode.js');
 const [year, day, part] = ["2019","15","1"];
 const [NORTH, SOUTH, WEST, EAST] = [1n, 2n, 3n, 4n];
 const DIRECTIONS = [NORTH, SOUTH, WEST, EAST];
+const NO_MOVES = 'nahbro'
 const [WALL, EMPTY, GOAL] = [0n, 1n, 2n];
 const DELIM = '|';
 const EVENT_OUTPUT = 'output';
@@ -24,7 +25,8 @@ let memory = fr.getInput(year,day,',').map(x => BigInt(x));
 
 let grid = new Map();
 let locToSteps = new Map();
-let dirTriedFromPos = new Map(createKey(0,0), [1n]);
+locToSteps.set(createKey(0,0), 0)
+let stepsToGoal = Infinity;
 
 let location = {
     x: 0,
@@ -35,44 +37,63 @@ let stepsTaken = [1n];
 let input = [1n]; 
 let output = [];
 ic.runAsync(memory, 0n, input, output, 0n, eventEmitter).then(answer => {
-    let blocks = [...grid.values()].reduce((total, curr) => {
-        if (curr === BLOCK) {
-            total++;
-        }
-        return total;
-    }, 0)
-    console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + blocks);
-    console.log('Expected: ')
-    console.timeEnd();
+    console.log('dead')
 })
 
 function receiveInput() {
     let status = output.shift();
-
+    console.log('status: ' + status)
+    let nextMove;
+    let lastDirectionTraveled;
     switch (status) {
         case WALL:
-            // increment to position that is not LAST input and is not in dirTried list
-            // update map with details
+            lastDirectionTraveled = stepsTaken[stepsTaken.length - 1];
+            updatePosition(lastDirectionTraveled);
+            grid.set(createKey(location.x, location.y), WALL);
+            updatePosition(getOpposite(lastDirectionTraveled));
+            stepsTaken.pop();
             break;
         case EMPTY:
-            // check if steps to reach is lower than previous
-                // if no, prune branch and backtrack
-                // if yes, continue
-            // update location
-            let lastDirectionTraveled = stepsTaken[stepsTaken.length - 1];
+            lastDirectionTraveled = stepsTaken[stepsTaken.length - 1];
             updatePosition(lastDirectionTraveled);
-            // reset dirTried list
-            // update map with details
+            locToSteps.set(createKey(location.x, location.y), stepsTaken.length);
+  
             grid.set(createKey(location.x, location.y), EMPTY);
-            stepsTaken.push(lastDirectionTraveled);
+            // console.log(location)
+
             break;
         case GOAL:
-            // update location
-            // backtrack to position with choices
-            // update dirTried list
+            lastDirectionTraveled = stepsTaken[stepsTaken.length - 1];
+            updatePosition(lastDirectionTraveled);
+            stepsToGoal = stepsToGoal > stepsTaken.length ? stepsTaken.length : stepsToGoal;
+            locToSteps.set(createKey(location.x, location.y), stepsTaken.length);
+            grid.set(createKey(location.x, location.y), GOAL);
+
+            updatePosition(getOpposite(lastDirectionTraveled));
+            stepsTaken.pop();
+            console.log('g: ' + stepsToGoal)
             break;
+        default:
+            console.log('Invalid Status');
     }
-    // make move
+
+    nextMove = pickNextMove();
+    // console.log('next: ' + nextMove)
+    // todo bail out if total steps > goal steps
+    if (nextMove === NO_MOVES) {
+        console.log(stepsTaken.length)
+        if (stepsTaken.length === 0) {
+            console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + blocks);
+            console.log('Expected: ')
+            console.timeEnd();
+            process.exit();
+        } else {
+            input.push(getOpposite(stepsTaken.pop()));
+        }
+    } else {
+        stepsTaken.push(nextMove)
+        input.push(nextMove);
+    }
 }
 
 function createKey(row, col) {
@@ -89,11 +110,13 @@ function getOpposite(key) {
             return WEST;
         case WEST:
             return EAST;
+        default:
+            console.log('Invalid Opposite: ' + key)
     }
 }
 
 function updatePosition(dir) {
-    switch (key) {
+    switch (dir) {
         case NORTH:
             location.x--;
             break;
@@ -106,5 +129,31 @@ function updatePosition(dir) {
         case WEST:
             location.y--;
             break;
+        default:
+            console.log('Invalid update: ' + dir)
     }
+    // console.log(location)
+}
+
+function pickNextMove() {
+    let northKey = createKey(location.x - 1, location.y);
+    let southKey = createKey(location.x + 1, location.y);
+    let westKey = createKey(location.x, location.y - 1);
+    let eastKey = createKey(location.x, location.y + 1);
+
+    let keys = [northKey, southKey, westKey, eastKey];
+
+    for (let i = 0; i < keys.length; i++) {
+        let tile = grid.get(keys[i]);
+        // console.log('tile next move: ' + tile)
+        if (tile === WALL) {
+            continue;
+        }
+        if (locToSteps.get(keys[i]) <= stepsTaken.length + 1) {
+            continue
+        }
+        return BigInt(i + 1);
+    } 
+
+    return NO_MOVES;
 }
