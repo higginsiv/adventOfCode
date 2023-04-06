4768984b8ab082f1a77c879263159a99f47c958c
William Higgins <william.higgins@ncino.com>
2023-04-06 14:15:50 -0400

    2015 17 2 : complete

--- 2015/js/Day17/problem2.js
+++ 2015/js/Day17/problem2.js
@@ -1,19 +1,26 @@
 console.time();
 const fr = require('../../../tools/fileReader');
-const [year, day, part] = ["2015","01","2"];
+const [year, day, part] = ["2015","17","2"];
+const data = fr.getInput(year,day).map(x => parseInt(x)).sort((a, b) => b-a);
 
-let floor = 0;
-let answer;
-const data = fr.getInput(year,day, '');
+const MAX = 150;
+let numContainersToFrequency = new Map();
+getValidOptions(0, data.slice(), 0);
+let answer = numContainersToFrequency.get([...numContainersToFrequency.keys()].sort((a,b) => a-b)[0]);
 
+function getValidOptions(currentVolume, containers, containersUsed) {
+	let localValid = 0;
 
-for(let i = 0; i < data.length; i++) {
-	let dir = data[i];
-	floor = dir === '(' ? floor + 1 : floor - 1;
-	if (floor === -1) {
-		answer = i + 1;
-		break;
-	}
+	containers.forEach((container, index) => {
+		if (currentVolume + container === MAX) {
+			localValid++;
+			numContainersToFrequency.set(containersUsed, numContainersToFrequency.get(containersUsed) == null ? 1 : numContainersToFrequency.get(containersUsed) + 1);
+		} else if (currentVolume + container < MAX) {
+			localValid += getValidOptions(currentVolume + container, containers.slice(index + 1), containersUsed + 1);
+		}
+	});
+
+	return localValid;
 }
 
 console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
