39a5215c8f0a67a3f4025fa499b8b167c77a1c31
William Higgins <higginsiv@gmail.com>
2023-09-06 22:10:06 -0400

    2015 Day13 1&2

---
+++ 2015/js/Day13/problem1.js
@@ -0,0 +1,43 @@
+console.time();
+const { map } = require('async');
+const fr = require('../../../tools/fileReader');
+const MATH = require('../../../tools/math');
+
+const [year, day, part] = ["2015","13","1"];
+
+let people = new Map();
+fr.getInput(year,day).forEach(x => {
+    x = x.replace(' would ', ' ');
+    x = x.replace(' happiness units by sitting next to ', ' ');
+    x = x.replace('.', '');
+
+    const [NAME, DIRECTION, NUMBER, GUEST] = x.split(' ');
+ 
+    let person = people.get(NAME);
+    if (person == null) {
+        person = new Map();
+        people.set(NAME, person);
+    }
+
+    person.set(GUEST, DIRECTION == 'gain' ? parseInt(NUMBER) : -parseInt(NUMBER));
+});
+
+let perms = MATH.permute([...people.keys()]);
+
+let answer = perms.reduce((total, curr, index) => {
+    let happiness = 0;
+    for (let i = 0; i < curr.length; i++) {
+        let partnerAIndex = i - 1 < 0 ? curr.length - 1 : i - 1;
+        let partnerBIndex = i + 1 >= curr.length ? 0 : i + 1;
+
+        let partnerAHappiness = people.get(curr[i]).get(curr[partnerAIndex]) == null ? 0 : people.get(curr[i]).get(curr[partnerAIndex]);
+        let partnerBHappiness = people.get(curr[i]).get(curr[partnerBIndex]) == null ? 0 : people.get(curr[i]).get(curr[partnerBIndex]);
+
+        happiness += (partnerAHappiness + partnerBHappiness);
+    }
+
+    return happiness > total ? happiness : total;
+}, -Infinity);
+
+console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+console.timeEnd();
