e2f12ec5251c04051e84e674fc37e0801393ab02
William Higgins <higginsiv@gmail.com>
2023-12-02 00:32:05 -0500

    2023 Day 2 complete

--- 2023/js/Day02/problem1.js
+++ 2023/js/Day02/problem1.js
@@ -1,6 +1,38 @@
 const fr = require('../../../tools/fileReader');
 const [YEAR, DAY, PART] = ["2023","02","1"];
-const DATA = fr.getInput(YEAR,DAY);
 
-let answer;
+const [RED, GREEN, BLUE] = [12, 13, 14];
+
+let colorMaxes = new Map([
+    ['red', RED],
+    ['green', GREEN],
+    ['blue', BLUE]      
+]);
+
+let idSum = 0;
+fr.getInput(YEAR,DAY).forEach(x => {
+    x = x.replace('Game ','');
+    x = x.split(': ');
+
+    let draws = x[1].split('; ');
+    draws = draws.map(y => {
+        y = y.split(', ');
+        return y;
+    });
+    
+    let valid = true;
+    draws.forEach(draw => {
+        draw.forEach(color => {
+            color = color.split(' ');
+            if (colorMaxes.get(color[1]) < parseInt(color[0])) {
+                valid = false;
+            }
+        })
+    })
+    if (valid) {
+        idSum += parseInt(x[0]);
+    }
+});
+
+let answer = idSum;
 console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
