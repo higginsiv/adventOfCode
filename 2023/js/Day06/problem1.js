21eab9612694e35fde06a8819e787dece9af960b
William Higgins <higginsiv@gmail.com>
2023-12-06 00:55:21 -0500

    2023 Day 6 Part 1 optimized solution

--- 2023/js/Day06/problem1.js
+++ 2023/js/Day06/problem1.js
@@ -3,14 +3,12 @@ const [YEAR, DAY, PART] = ["2023","06","1"];
 const [TIMES, DISTANCES] = fr.getInput(YEAR,DAY).map(x => x.match(/\d+/g).map(x => parseInt(x)));
 
 let answer = TIMES.reduce((total, curr, index) => {
-    const maxTime = curr;
-    let wins = 0;
-    for (let i = 0; i <= maxTime; i++) {
-        const distance = (i * (maxTime - i));
-        if (distance >= DISTANCES[index]) {
-            wins++;
-        }
-    }
+    const TIME = curr;
+    const DISTANCE = DISTANCES[index];
+    const x1 = (TIME + Math.sqrt(TIME * TIME - 4 * DISTANCE)) / 2;
+    const x2 = (TIME - Math.sqrt(TIME * TIME - 4 * DISTANCE)) / 2;
+    const wins = Math.floor(x1) - Math.floor(x2);
+
     return total * wins;
 }, 1);
 
