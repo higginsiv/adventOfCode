008a01878a4f49f226b055224a96f810c0c6700f
William Higgins <higginsiv@gmail.com>
2023-12-19 23:51:04 -0500

    Math.js added a bunch of functions I've used regularly

--- tools/math.js
+++ tools/math.js
@@ -5,7 +5,12 @@ module.exports = {
     getGCD: getGCD,
 	permute: permute,
     getCombinations: getCombinations,
-    getCombinationsQueue: getCombinationsQueue
+    getCombinationsQueue: getCombinationsQueue,
+    manhattanDistance: manhattanDistance,
+    manhattanDistance3d: manhattanDistance3d,
+    solveQuadratic: solveQuadratic,
+    shoelace: shoelace,
+    picksTheorem: picksTheorem
 };
 
 function getGCD(a, b) {
@@ -62,60 +67,36 @@ function getCombinationsQueue(symbols, length) {
     return allCombinations;
 }
 
-// function getCombinationsQueue(symbols, minLength, maxLength) {
-//     let allCombinations = [];
+function manhattanDistance(start, end) {
+    return Math.abs(start.x - end.x) + Math.abs(start.y - end.y);
+}
 
-//     if (minLength === 0) {
-//         return allCombinations;
-//     }
+function manhattanDistance3d(start, end) {
+    return Math.abs(start.x - end.x) + Math.abs(start.y - end.y) + Math.abs(start.z - end.z);
+}
 
-//     if (maxLength == null) {
-//         maxLength = minLength;
-//     }
-//     for (let length = minLength; length <= maxLength; length++) {
-//         if (length === 1) {
-//             allCombinations.push(...symbols);
-//         } else {
-//             let combinations = [];
-//             let smallerCombinations = getCombinations(symbols, length - 1, length - 1);
+function solveQuadratic(a, b, c) {
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
+    if (discriminant < 0) {
+        return null;
+    }
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [x1, x2];
+}
 
-//             for (let symbol of symbols) {
-//                 for (let smallerCombination of smallerCombinations) {
-//                     combinations.push(symbol + smallerCombination);
-//                 }
-//             }
+// Calculate area of polygon defined by vertices
+function shoelace(vertices) {
+    let area = 0;
+    for (let i = 0; i < vertices.length; i++) {
+        let j = (i + 1) % vertices.length;
+        area += vertices[i][0] * vertices[j][1];
+        area -= vertices[j][0] * vertices[i][1];
+    }
+    return Math.abs(area / 2);
+}
 
-//             allCombinations.push(...combinations);
-//         }
-//     }
-
-//     return allCombinations;
-// }
-
-// function getCombinations(symbols, minLength, maxLength) {
-//     let allCombinations = [];
-//     let queue = symbols.map(symbol => [symbol]);
-
-//     if (minLength === 0) {
-//         return allCombinations;
-//     }
-//     if (maxLength == null) {
-//         maxLength = minLength;
-//     }
-//     while (queue.length > 0) {
-//         let combination = queue.shift();
-//         let length = combination.length;
-
-//         if (length >= minLength) {
-//             allCombinations.push(combination.join(''));
-//         }
-
-//         if (length < maxLength) {
-//             for (let symbol of symbols) {
-//                 queue.push([...combination, symbol]);
-//             }
-//         }
-//     }
-
-//     return allCombinations;
-// }
+// Calculates area of polygon while counting it's vertices as part of the polygon
+function picksTheorem(vertices) {
+    return shoelace(vertices) + 2 - vertices.length;
+}
