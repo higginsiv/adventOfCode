e9e4f03e39b3d4faf1fc45e65d822cf9f36515fc
William Higgins <higginsiv@gmail.com>
2023-12-19 23:44:15 -0500

    2023 Day 20 create shell moved insertIntoSortedQueue into iteration tools

---
+++ tools/iteration.js
@@ -0,0 +1,26 @@
+/**
+ * Inserts a given state object into the correct place in an array of states sorted by weight ascending.
+ * @param {*} queue 
+ * @param {*} state 
+ * @param {*} weightVariableName 
+ */
+function insertIntoSortedQueue(queue, state, weightVariableName) {
+    let low = 0;
+    let high = queue.length;
+
+    while (low < high) {
+        let mid = (low + high) >>> 1;
+
+        if (queue[mid][weightVariableName] < state[weightVariableName]) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    queue.splice(low, 0, state);
+}
+
+module.exports = {
+    insertIntoSortedQueue: insertIntoSortedQueue
+};
