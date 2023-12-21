276b9c39633e5c9231328e10f6f849acb0449aa2
William Higgins <higginsiv@gmail.com>
2023-12-20 23:56:12 -0500

    update output.js to have file support

--- tools/output.js
+++ tools/output.js
@@ -1,11 +1,16 @@
+const fs = require('node:fs');
+
 module.exports = {
-    output: output
+    output: output,
 };
 
 function output(year, day, part, answer, strategy='console') {
     switch (strategy) {
         case 'console':
             console.log(`Year ${year} Day ${day} Puzzle ${part}: ${answer}`);
+            break;
+        case 'file':
+            fs.writeFileSync(`${year}/js/Day${day}/output-${part}.txt`, String(answer));
             break;
     }
 }
