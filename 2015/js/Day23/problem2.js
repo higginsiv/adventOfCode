11073da097eb80aaa944f85a2f77d183976167d9
William Higgins <higginsiv@gmail.com>
2023-12-27 01:05:30 -0500

    2015 Day 23 Solutions

---
+++ 2015/js/Day23/problem2.js
@@ -0,0 +1,60 @@
+const fr = require('../../../tools/fileReader');
+const OUTPUT = require('../../../tools/output');
+const [YEAR, DAY, PART] = ["2015","23","2"];
+const DATA = fr.getInput(YEAR,DAY).map(line => {
+    const [instruction, register, offset] = line.split(' ');
+
+    if (instruction === 'jmp') {
+        return {
+            instruction,
+            offset: parseInt(register)
+        };
+    }
+    
+    return {
+        instruction,
+        register: register.replace(',',''),
+        offset: parseInt(offset)
+    };
+});
+
+let pointer = 0;
+let registers = {a: 1, b: 0};
+
+while (pointer < DATA.length && pointer >= 0) {
+    const {instruction, register, offset} = DATA[pointer];
+    switch (instruction) {
+        case 'hlf':
+            registers[register] /= 2;
+            pointer++;
+            break;
+        case 'tpl':
+            registers[register] *= 3;
+            pointer++;
+            break;
+        case 'inc':
+            registers[register]++;
+            pointer++;
+            break;
+        case 'jmp':
+            pointer += offset;
+            break;
+        case 'jie':
+            if (registers[register] % 2 === 0) {
+                pointer += offset;
+            } else {
+                pointer++;
+            }
+            break;
+        case 'jio':
+            if (registers[register] === 1) {
+                pointer += offset;
+            } else {
+                pointer++;
+            }
+            break;
+    }
+}
+
+let answer = registers['b'];
+OUTPUT.output(YEAR, DAY, PART, answer);
