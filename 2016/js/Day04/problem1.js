e6c333bbaa57eea951a01fc6f665901c76e7a081
William Higgins <higginsiv@gmail.com>
2024-01-03 00:51:18 -0500

    2016 Day 04 complete

---
+++ 2016/js/Day04/problem1.js
@@ -0,0 +1,38 @@
+const fr = require('../../../tools/fileReader');
+const OUTPUT = require('../../../tools/output');
+const [YEAR, DAY, PART] = ['2016', '04', '1'];
+let answer = fr
+    .getInput(YEAR, DAY)
+    .map((x) => {
+        const [letters, sector, checksum] = x
+            .match(/([a-z-]+)-(\d+)\[([a-z]+)\]/)
+            .slice(1);
+        return { letters, sector: parseInt(sector), checksum };
+    })
+    .reduce((total, current) => {
+        const letters = current.letters.replace(/-/g, '').split('');
+        const counts = letters.reduce((total, current) => {
+            if (total[current] == null) {
+                total[current] = 0;
+            }
+
+            total[current]++;
+            return total;
+        }, {});
+
+        const sorted = Object.keys(counts).sort((a, b) => {
+            if (counts[a] === counts[b]) {
+                return a.localeCompare(b);
+            }
+            return counts[b] - counts[a];
+        });
+
+        const checksum = sorted.slice(0, 5).join('');
+        if (checksum === current.checksum) {
+            total += current.sector;
+        }
+
+        return total;
+    }, 0);
+
+OUTPUT.output(YEAR, DAY, PART, answer);
---
+++ 2016/js/Day04/problem2.js
@@ -0,0 +1,58 @@
+const fr = require('../../../tools/fileReader');
+const OUTPUT = require('../../../tools/output');
+const [YEAR, DAY, PART] = ['2016', '04', '2'];
+const Z = 'z'.charCodeAt(0);
+const ALPHA_LENGTH = 26;
+
+const DATA = fr
+    .getInput(YEAR, DAY)
+    .map((x) => {
+        const [letters, sector, checksum] = x
+            .match(/([a-z-]+)-(\d+)\[([a-z]+)\]/)
+            .slice(1);
+        return { letters, sector: parseInt(sector), checksum };
+    });
+
+let answer;
+for (room of DATA)    {
+    const letters = room.letters.replace(/-/g, '').split('');
+    const counts = letters.reduce((total, current) => {
+        if (total[current] == null) {
+            total[current] = 0;
+        }
+
+        total[current]++;
+        return total;
+    }, {});
+
+    const sorted = Object.keys(counts).sort((a, b) => {
+        if (counts[a] === counts[b]) {
+            return a.localeCompare(b);
+        }
+        return counts[b] - counts[a];
+    });
+
+    const checksum = sorted.slice(0, 5).join('');
+    if (checksum === room.checksum) {
+        const shift = room.sector % ALPHA_LENGTH;
+        const letters = room.letters.split('');
+        const decrypted = letters.map((x) => {
+            if (x === '-') {
+                return ' ';
+            }
+
+            const code = x.charCodeAt(0) + shift;
+            if (code > Z) {
+                return String.fromCharCode(code - ALPHA_LENGTH);
+            }
+            return String.fromCharCode(code);
+        });
+
+        if (decrypted.join('').includes('north')) {
+            answer = room.sector;
+            break;
+        }
+    }
+};
+
+OUTPUT.output(YEAR, DAY, PART, answer);
