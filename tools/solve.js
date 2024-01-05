91bda93c3f01dc82ba21fa7d7c848866d4e3a832
William Higgins <higginsiv@gmail.com>
2024-01-05 16:41:25 -0500

    added solve to npm scripts moved solve into tools

--- tools/solve.js
+++ tools/solve.js
@@ -1,8 +1,13 @@
 // Get year, day, and part from arguments
 const [YEAR, DAY, PART] = process.argv.slice(2);
 
+if (!YEAR || !DAY || !PART) {
+    console.error('Usage: node solve.js <year> <day> <part>');
+    process.exit(1);
+}
+
 // Get the path to the solution file
-const path = `./${YEAR}/js/Day${DAY}/problem${PART}.js`;
+const path = `../${YEAR}/js/Day${DAY}/problem${PART}.js`;
 
 // run file at the path
 console.time('Solve!');
