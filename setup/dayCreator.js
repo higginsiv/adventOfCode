dbde7f7fe68c17bf90a8536689f1419f3ea2d8ee
William Higgins <higginsiv@gmail.com>
2024-01-05 16:32:16 -0500

    Update DayCreator to be npm script

--- setup/dayCreator.js
+++ setup/dayCreator.js
@@ -2,16 +2,25 @@ const fs = require('node:fs');
 
 const [YEAR, DAY] = process.argv.slice(2);
 
-console.log(`Creating advent directory for ${YEAR} Day ${DAY}`);
-
-if (!fs.existsSync(`${YEAR}/js/Day${DAY}`)){
-    fs.mkdirSync(`${YEAR}/js/Day${DAY}`, { recursive: true });
+if (!YEAR || !DAY) {
+    console.error('Usage: node dayCreators.js <year> <day>');
+    process.exit(1);
 }
 
-fs.writeFileSync(`${YEAR}/js/Day${DAY}/problem1.js`, getTemplate(1));
-fs.writeFileSync(`${YEAR}/js/Day${DAY}/problem2.js`, getTemplate(2));
-fs.writeFileSync(`${YEAR}/js/Day${DAY}/input.txt`, '');
-fs.writeFileSync(`${YEAR}/js/Day${DAY}/README.md`, '');
+
+if (!fs.existsSync(`${YEAR}/js/Day${DAY}`)){
+    console.log(`Creating advent directory for ${YEAR} Day ${DAY}`);
+
+    fs.mkdirSync(`${YEAR}/js/Day${DAY}`, { recursive: true });
+
+    fs.writeFileSync(`${YEAR}/js/Day${DAY}/problem1.js`, getTemplate(1));
+    fs.writeFileSync(`${YEAR}/js/Day${DAY}/problem2.js`, getTemplate(2));
+    fs.writeFileSync(`${YEAR}/js/Day${DAY}/input.txt`, '');
+    fs.writeFileSync(`${YEAR}/js/Day${DAY}/README.md`, '');
+} else {
+    console.error(`Directory ${YEAR}/js/Day${DAY} already exists`);
+    process.exit(1);
+}
 
 function getTemplate(part) {
     return `const fr = require('../../../tools/fileReader');
