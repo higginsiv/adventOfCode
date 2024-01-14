384c6576d4a6d1479254fe3a1b8b6d4d9da14d39
William Higgins <higginsiv@gmail.com>
2024-01-14 00:26:56 -0500

    2016 day 14 complete removed example test cases because runtime is long

--- 2016/js/Day14/problem2.js
+++ 2016/js/Day14/problem2.js
@@ -1,118 +1,52 @@
-module.exports = {solve: solve};
+module.exports = { solve: solve };
 
-function solve({lines, rawData}) {
+function solve({ lines, rawData }) {
     const CREATE_HASH = require('node:crypto').createHash;
 
-    const LOOPS = 2016;
+    const LOOPS = 2017;
 
-    let allHashes = {};
     let index = 0;
-    let numKeys = 0;
+    let keyIndices = [];
 
+    let last1000 = Array(1000).fill(null);
 
-    let initialStream = {
-        key: 0,
-        hashes: [],
-        nextStream: null
-    }
-
-    let streams = [initialStream];
-
-    while (numKeys < 64) {
-        if (index % 1000 === 0)console.log(index)
-
-        let expandedHash = getExpandedHashRecursive(`${rawData}${index}`, 2017)[2016];
-
-        let matches = expandedHash.match(/(.)\1\1/);
-        if (matches && matches[1]) {
-            let char = matches[1];
-            let regex = new RegExp(`${char}{5}`);
-
-            for (let i = 1; i <= 1000; i++) {
-                let hash2 = getExpandedHashRecursive(`${rawData}${index+i}`, 2017)[2016];
-
-                if (hash2.match(regex)) {
-                    console.log('found')
-                    numKeys++;
-                    break;
-                }
-            }
+    while (keyIndices.length < 64) {
+        let hash = `${rawData}${index}`;
+        for (let i = 0; i < LOOPS; i++) {
+            hash = getHash(hash);
         }
+
+        let fiveMatches = hash.match(/(.)\1\1\1\1/g);
+        fiveMatches = fiveMatches ? fiveMatches.map((match) => match[0]) : null;
+
+        let firstThree = hash.match(/(.)\1\1/);
+
+        let localIndex = index % 1000;
+        if (fiveMatches) {
+            last1000.forEach((item) => {
+                if (
+                    item &&
+                    item.firstThree &&
+                    fiveMatches.includes(item.firstThree)
+                ) {
+                    keyIndices.push(item.trueIndex);
+                }
+            });
+        }
+        last1000[localIndex] = {
+            hash: hash,
+            firstThree: firstThree ? firstThree[1] : null,
+            fiveMatches: fiveMatches,
+            trueIndex: index,
+        };
+
         index++;
     }
 
-    // function getExpandedHash(str) {
-    //     let hash = str;
-    //     for (let i = 0; i <= LOOPS; i++) {
-    //         hash = getHash(hash);
-    //     }
-    //     return hash;
-    // }
-
     function getHash(str) {
-        if (!allHashes[str]) {
-            const hash = CREATE_HASH('md5').update(str).digest('hex');
-            allHashes[str] = hash;
-        } 
-        return allHashes[str];
+        return CREATE_HASH('md5').update(str).digest('hex');
     }
 
-    function getExpandedHashRecursive(str, n) {
-        // console.log(n)
-        if (n <= 0) {
-            return [];
-        }
-
-        let hashes = getHash(str);
-        let numHashes = hashes.length;
-        let hashToSend = hashes[numHashes - 1];
-
-        let downStreamHashes = getExpandedHashRecursive(hashToSend, n-numHashes);
-        allHashes[str] = allHashes[str].concat(downStreamHashes);
-        return [...hashes, ...downStreamHashes];
-    }
-
-    // while (numKeys < 64) {
-    //     if (index % 1000 === 0)console.log(index)
-    //     let hash = hash(`${rawData}${index}`);
-
-    //     if (!hashes[hash]) {
-    //         hashes[hash] = [];
-    //     }
-
-    //     let hashesToSkip = hashes[hash].length;
-    //     if (hashesToSkip > 0) {
-    //         let hash = hashes[hash][hashesToSkip - 1];
-    //     }
-
-    //     for (let i = hashesToSkip; i < LOOPS; i++) {
-    //         hash = hash(hash);
-    //         hashes[hash].push(hash);
-    //     }
-
-    //     let matches = HASH.match(/(.)\1\1/);
-    //     if (matches && matches[1]) {
-    //         let char = matches[1];
-    //         let regex = new RegExp(`${char}{5}`);
-
-    //         for (let i = 1; i <= 1000; i++) {
-    //             let HASH2 = hashes[index+i];
-    //             if (!HASH2) {
-    //                 HASH2 = CREATE_HASH('md5').update(`${rawData}${index+i}`).digest('hex');
-    //                 hashes[index+i] = HASH2;
-    //             }
-
-    //             if (HASH2.match(regex)) {
-    //                 console.log('found')
-    //                 numKeys++;
-    //                 break;
-    //             }
-    //         }
-    //     }
-
-    //     index++;
-    // }
-
-    let answer = index - 1;
-    return {value: answer};
+    let answer = keyIndices.sort((a, b) => a - b)[63];
+    return { value: answer };
 }
--- 2016/js/Day14/tests/test.js
+++ 2016/js/Day14/tests/test.js
@@ -5,19 +5,11 @@ const { solve: part2 } = require(`../problem2.js`);
 const data = fr.getInputForFunction('2016', '14');
 
 describe(`2016 Day 14`, () => {
-    it('Part 1 - Example', () => {
-        expect(part1({rawData: 'abc'}).value).toBe(22728);
-    });
-
     it('Part 1', () => {
         expect(part1(data).value).toBe(25427);
     });
 
-    it('Part 2 - Example', () => {
-        expect(part2({rawData: 'abc'}).value).toBe();
-    });
-    
     it('Part 2', () => {
-        expect(part2(data).value).toBe();
+        expect(part2(data).value).toBe(22045);
     });
 });
