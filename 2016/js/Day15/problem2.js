34c25fe8d62262db99ad039b3cfe1b97c30bcf4f
William Higgins <higginsiv@gmail.com>
2024-01-14 20:52:53 -0500

    2016 Day 15 Solutions

---
+++ 2016/js/Day15/problem2.js
@@ -0,0 +1,21 @@
+module.exports = {solve: solve};
+
+function solve({lines, rawData}) {
+    lines = lines.map((line, i) => {
+        let [discNumber, positions, time, start] = line.match(/\d+/g).map(Number);
+        return {discNumber, positions, start};
+    });
+    lines.push({discNumber: lines.length + 1, positions: 11, start: 0});
+
+    let time = 0;
+
+    while (true) {
+        if (lines.every(line => (time + line.discNumber + line.start) % line.positions === 0)) {
+            break;
+        }
+        time++;
+    }
+
+    let answer = time;
+    return {value: answer};
+}
---
+++ 2016/js/Day15/tests/test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2016', '15');
+
+describe(`2016 Day 15`, () => {
+    it('Part 1', () => {
+        expect(part1(data).value).toBe(376777);
+    });
+
+    it('Part 2', () => {
+        expect(part2(data).value).toBe(3903937);
+    });
+});
