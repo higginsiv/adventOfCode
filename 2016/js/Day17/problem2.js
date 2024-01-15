10b9f31a66e45f373691a90f59deb2c1b6c999bf
William Higgins <higginsiv@gmail.com>
2024-01-14 22:35:10 -0500

    2016 Day 17 p1

---
+++ 2016/js/Day17/problem2.js
@@ -0,0 +1,49 @@
+module.exports = {solve: solve};
+
+function solve({lines, rawData}) {
+    const { insertIntoSortedQueue } = require('../../../tools/iteration.js');
+    const crypto = require('crypto');
+    const OPEN = ['b', 'c', 'd', 'e', 'f'];
+    const MAX = 3;
+
+    let queue = [{x: 0, y: 0, path: '', pathLength: 0}];
+    let answer;
+
+    while (queue.length > 0) {
+        let state = queue.shift();
+
+        if (state.x === 3 && state.y === 3) {
+            answer = Math.max(answer || 0, state.pathLength);
+        }
+
+        queue = queue.concat(getNeighbors(state));
+    }
+
+    function getHash(path) {
+        return crypto.createHash('md5').update(rawData + path).digest("hex");
+    }
+
+    function getNeighbors(state) {
+        let hash = getHash(state.path);
+        let neighbors = [];
+        if (OPEN.includes(hash[0]) && state.y > 0) {
+            neighbors.push({x: state.x, y: state.y - 1, path: state.path + 'U', pathLength: state.pathLength + 1});
+        }
+
+        if (OPEN.includes(hash[1]) && state.y < MAX) {
+            neighbors.push({x: state.x, y: state.y + 1, path: state.path + 'D', pathLength: state.pathLength + 1});
+        }
+
+        if (OPEN.includes(hash[2]) && state.x > 0) {
+            neighbors.push({x: state.x - 1, y: state.y, path: state.path + 'L', pathLength: state.pathLength + 1});
+        }
+
+        if (OPEN.includes(hash[3]) && state.x < MAX) {
+            neighbors.push({x: state.x + 1, y: state.y, path: state.path + 'R', pathLength: state.pathLength + 1});
+        }
+
+        return neighbors;
+    }
+
+    return {value: answer};
+}
