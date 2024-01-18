f2ad23a66202b121f0f6882bb6bd36a32572ed72
William Higgins <higginsiv@gmail.com>
2024-01-17 22:05:44 -0500

    Tools - updated solve.js to not require a part

--- tools/solve.js
+++ tools/solve.js
@@ -4,13 +4,10 @@ const OUTPUT = require('./output');
 // Get year, day, and part from arguments
 let [YEAR, DAY, PART] = process.argv.slice(2);
 
-// const YEAR = process.env.npm_config_year;
-// const DAY = process.env.npm_config_dayNum;
-// const PART = process.env.npm_config_part;
 const LEGACY = process.env.npm_config_legacy;
 
-if (!YEAR || !DAY || !PART) {
-    console.error('Usage: node solve.js <year> <day> <part>');
+if (!YEAR || !DAY) {
+    console.error('Usage: node solve.js <year> <day> [<part>]');
     process.exit(1);
 }
 
@@ -18,26 +15,36 @@ if (DAY.length === 1) {
     DAY = DAY.padStart(2, '0');
 }
 
-if (LEGACY) {
-    // Get the path to the solution file
-    const path = `../${YEAR}/js/Day${DAY}/problem${PART}.js`;
-
-    // run file at the path
-    console.time('Solve!');
-    require(path);
-    console.timeEnd('Solve!');
+let parts;
+if (!PART) {
+    parts = ['1', '2'];
 } else {
-    let data = fr.getInputForFunction(YEAR, DAY);
-
-    // Get the path to the solution file
-    const path = `../${YEAR}/js/Day${DAY}/problem${PART}.js`;
-    const { solve } = require(path);
-
-    // run file at the path
-    const timerKey = `Solve! ${YEAR} Day ${DAY} Part ${PART}`;
-    console.time(timerKey);
-    let answer = solve(data);
-    console.timeEnd(timerKey);
-
-    OUTPUT.output(YEAR, DAY, PART, answer.value, answer.strategy);
+    parts = [PART];
 }
+
+parts.forEach((part) => {
+    if (LEGACY) {
+        // Get the path to the solution file
+        const path = `../${YEAR}/js/Day${DAY}/problem${part}.js`;
+    
+        // run file at the path
+        console.time('Solve!');
+        require(path);
+        console.timeEnd('Solve!');
+    } else {
+        let data = fr.getInputForFunction(YEAR, DAY);
+    
+        // Get the path to the solution file
+        const path = `../${YEAR}/js/Day${DAY}/problem${part}.js`;
+        const { solve } = require(path);
+    
+        // run file at the path
+        const timerKey = `${YEAR} Day ${DAY} Part ${part}`;
+        console.time(timerKey);
+        let answer = solve(data);
+        console.timeEnd(timerKey);
+    
+        OUTPUT.output(YEAR, DAY, PART, answer.value, answer.strategy);
+    }
+});
+
