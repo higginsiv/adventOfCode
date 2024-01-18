eba74cedad3263599fa7c8faa4508ac1e3eebf29
William Higgins <higginsiv@gmail.com>
2024-01-17 22:00:28 -0500

    2016 22 part 1

---
+++ 2016/js/Day22/problem1.js
@@ -0,0 +1,33 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    lines.splice(0, 2);
+
+    lines = lines.map((line) => {
+        let matches = line.match(/\d+/g);
+        return {
+            used: parseInt(matches[3]),
+            avail: parseInt(matches[4]),
+        };
+    });
+
+    let viablePairs = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+        for (let j = 0; j < lines.length; j++) {
+            if (i === j) {
+                continue;
+            }
+
+            if (lines[i].used === 0) {
+                continue;
+            }
+
+            if (lines[i].used <= lines[j].avail) {
+                viablePairs++;
+            }
+        }
+    }
+
+    return { value: viablePairs };
+}
---
+++ 2016/js/Day22/tests/2016-22-test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2016', '22');
+
+describe(`2016 Day 22`, () => {
+    it('Part 1', () => {
+        expect(part1(data).value).toBe(967);
+    });
+
+    it('Part 2', () => {
+        expect(part2(data).value).toBe();
+    });
+});
