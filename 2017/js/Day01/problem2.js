2727beb8dbc51fa3513650e8040535d3bbf28b50
William Higgins <higginsiv@gmail.com>
2024-01-22 21:27:41 -0500

    2017 Day 1 complete

---
+++ 2017/js/Day01/problem2.js
@@ -0,0 +1,15 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    let answer = 0;
+    let digits = rawData.split('').map(char => parseInt(char));
+    const half = digits.length / 2;
+
+    for (let i = 0; i < half; i++) {
+        if (digits[i] === digits[i + half]) {
+            answer += digits[i] * 2;
+        }
+    }
+
+    return { value: answer };
+}
