359a67b24c8b8322be6358cfc2084a79660fee8a
William Higgins <higginsiv@gmail.com>
2024-01-22 21:35:43 -0500

    2017 Day 2 complete

---
+++ 2017/js/Day02/problem1.js
@@ -0,0 +1,16 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    let answer = lines
+        .map((line) => {
+            return line
+                .match(/\d+/g)
+                .map((value) => parseInt(value))
+                .sort((a, b) => a - b);
+        })
+        .reduce((sum, curr) => {
+            return sum + (curr[curr.length - 1] - curr[0]);
+        }, 0);
+
+    return { value: answer };
+}
