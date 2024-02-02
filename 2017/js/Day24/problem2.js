35f9e23b5515caa0092b0b4c659850de61a0db0f
William Higgins <higginsiv@gmail.com>
2024-02-02 00:56:49 -0500

    2017 24 complete

---
+++ 2017/js/Day24/problem2.js
@@ -0,0 +1,48 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    lines = lines.map((line, index) => {
+        line = line.split('/').map(Number);
+        return { id: index, ports: line };
+    });
+
+    const bridges = [];
+    const buildBridge = (bridge, lines, ids) => {
+        const last = bridge[bridge.length - 1];
+        const next = lines.filter((line) => line.ports.includes(last) && !ids.includes(line.id));
+        if (next.length === 0) {
+            bridges.push(bridge);
+            return;
+        }
+
+        next.forEach((line) => {
+            const newBridge = bridge.slice();
+            let port = line.ports.filter((port) => port !== last);
+            let toPush = port[0] === undefined ? last : port[0];
+            newBridge.push(last, toPush);
+            const newIds = ids.slice();
+            newIds.push(line.id);
+            buildBridge(newBridge, lines, newIds);
+        });
+    };
+
+    buildBridge([0], lines, []);
+    const answer = bridges
+        .sort((a, b) => {
+            return b.length - a.length;
+        })
+        .filter((bridge, index, bridges) => {
+            return bridge.length === bridges[0].length;
+        })
+        .reduce((max, bridge) => {
+            const strength = bridge.reduce((strength, curr) => {
+                if (curr === undefined) {
+                    return strength;
+                }
+                return strength + curr;
+            }, 0);
+            return max > strength ? max : strength;
+        }, 0);
+
+    return { value: answer };
+}
---
+++ 2017/js/Day24/tests/2017-24.test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2017', '24');
+
+describe(`2017 Day 24`, () => {
+    it('Part 1', () => {
+        expect(part1(data).value).toBe(1859);
+    });
+
+    it('Part 2', () => {
+        expect(part2(data).value).toBe(1799);
+    });
+});
