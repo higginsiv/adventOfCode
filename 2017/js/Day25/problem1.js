da6bb028bc70817efb9c11845835b1a8b8c4f78d
William Higgins <higginsiv@gmail.com>
2024-02-02 21:06:14 -0500

    2017 25 complete

---
+++ 2017/js/Day25/problem1.js
@@ -0,0 +1,43 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    function getKey(position, tape, state) {
+        return `${position}-${tape}-${state}`;
+    }
+    let state = lines[0].charAt(lines[0].length - 2);
+    const steps = Number(lines[1].match(/\d+/)[0]);
+
+    const rules = {};
+
+    for (let i = 3; i < lines.length; i += 10) {
+        let rule = {};
+        let state = lines[i].charAt(lines[i].length - 2);
+        let rule0 = {
+            value: Number(lines[i + 2].match(/\d+/)[0]),
+            move: lines[i + 3].includes('right') ? 1 : -1,
+            next: lines[i + 4].charAt(lines[i + 4].length - 2),
+        };
+        let rule1 = {
+            value: Number(lines[i + 6].match(/\d+/)[0]),
+            move: lines[i + 7].includes('right') ? 1 : -1,
+            next: lines[i + 8].charAt(lines[i + 8].length - 2),
+        };
+        rule[0] = rule0;
+        rule[1] = rule1;
+        rules[state] = rule;
+    }
+
+    let tape = { 0: 0 };
+    let position = 0;
+
+    for (let i = 0; i < steps; i++) {
+        let value = tape[position] || 0;
+        let rule = rules[state][value];
+        tape[position] = rule.value;
+        position += rule.move;
+        state = rule.next;
+    }
+
+    const answer = Object.values(tape).filter((v) => v === 1).length;
+    return { value: answer };
+}
---
+++ 2017/js/Day25/tests/2017-25.test.js
@@ -0,0 +1,11 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2017', '25');
+
+describe(`2017 Day 25`, () => {
+    it('Part 1', () => {
+        expect(part1(data).value).toBe(3362);
+    });
+});
