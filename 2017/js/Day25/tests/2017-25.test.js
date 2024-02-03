feab8749c364980211bffb518babd4a269c941b3
William Higgins <higginsiv@gmail.com>
2024-02-02 21:09:15 -0500

    2017 remove bad test include

--- 2017/js/Day25/tests/2017-25.test.js
+++ 2017/js/Day25/tests/2017-25.test.js
@@ -1,6 +1,5 @@
 const fr = require('../../../../tools/fileReader');
 const { solve: part1 } = require(`../problem1.js`);
-const { solve: part2 } = require(`../problem2.js`);
 
 const data = fr.getInputForFunction('2017', '25');
 
