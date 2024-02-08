becb8f2e3c77d10717d94dbf64395180768a685a
William Higgins <higginsiv@gmail.com>
2024-02-07 21:29:51 -0500

    2018 8 p1 complete

---
+++ 2018/js/Day08/problem1.js
@@ -0,0 +1,21 @@
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    function sumMetadata(elements, numChildren, numMetadata) {
+        let sum = 0;
+
+        for (let i = 0; i < numChildren; i++) {
+            sum += sumMetadata(elements, elements.pop(), elements.pop());
+        }
+
+        for (let i = 0; i < numMetadata; i++) {
+            sum += elements.pop();
+        }
+
+        return sum;
+    }
+
+    let numbers = rawData.match(/\d+/g).map(Number).reverse();
+
+    return { value: sumMetadata(numbers, numbers.pop(), numbers.pop()) };
+}
