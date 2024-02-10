959d59370f0dda3fb46ab029c930e441d9b8e011
William Higgins <higginsiv@gmail.com>
2024-02-10 00:48:55 -0500

    2018 day 11 p2 performance improvement (left comments with kadane algorithm that is unfinished)

--- 2018/js/Day11/problem2.js
+++ 2018/js/Day11/problem2.js
@@ -3,37 +3,61 @@ module.exports = { solve: solve };
 function solve({ lines, rawData }) {
     const { floor } = Math;
 
+    // function kadane2D(matrix) {
+    //     let maxSum = -Infinity;
+    //     let left, right, top, bottom;
+
+    //     for (let leftCol = 0; leftCol < matrix[0].length; leftCol++) {
+    //         let temp = new Array(matrix.length).fill(0);
+
+    //         for (let rightCol = leftCol; rightCol < matrix[0].length; rightCol++) {
+    //             for (let i = 0; i < matrix.length; i++) {
+    //                 temp[i] += matrix[i][rightCol];
+    //             }
+
+    //             let {
+    //                 maxSum: currentMaxSum,
+    //                 startIndex: currentStart,
+    //                 endIndex: currentEnd,
+    //             } = kadane(temp);
+
+    //             // Check that the width and height are equal
+    //             if (currentMaxSum > maxSum && currentEnd - currentStart == rightCol - leftCol) {
+    //                 maxSum = currentMaxSum;
+    //                 left = leftCol;
+    //                 right = rightCol;
+    //                 top = currentStart;
+    //                 bottom = currentEnd;
+    //             }
+    //         }
+    //     }
+
+    //     return { maxSum, left, right, top, bottom };
+    // }
+
     // function kadane(arr) {
-    //     let localMax = 0;
-    //     let globalMax = -Infinity;
+    //     let localMax = arr[0];
+    //     let globalMax = arr[0];
     //     let localStart = 0;
     //     let globalStart = 0;
+    //     let globalEnd = 0;
 
-    //     for (let i = 0; i < arr.length; i++) {
-    //         if (arr[i] > arr[i] + localMax) {
+    //     for (let i = 1; i < arr.length; i++) {
+    //         if (arr[i] > localMax + arr[i]) {
     //             localMax = arr[i];
     //             localStart = i;
     //         } else {
-    //             localMax = arr[i] + localMax;
+    //             localMax = localMax + arr[i];
     //         }
 
     //         if (localMax > globalMax) {
     //             globalMax = localMax;
     //             globalStart = localStart;
+    //             globalEnd = i;
     //         }
     //     }
 
-    //     return { maxSum: globalMax, startIndex: globalStart };
-    // }
-
-    // console.log(kadane([1, 2, -3, -4, 1, 8, -30]));
-    // process.exit(0);
-    // function getHighestFactor(n) {
-    //     for (let i = n - 1; i > 0; i--) {
-    //         if (n % i === 0) {
-    //             return i;
-    //         }
-    //     }
+    //     return { maxSum: globalMax, startIndex: globalStart, endIndex: globalEnd };
     // }
 
     function getPowerLevel(x, y, serialNumber) {
@@ -45,11 +69,6 @@ function solve({ lines, rawData }) {
         powerLevel -= 5;
         return powerLevel;
     }
-
-    // let highestFactors = new Map();
-    // for (let i = 1; i <= 300; i++) {
-    //     highestFactors.set(i, getHighestFactor(i));
-    // }
 
     const serialNumber = Number(rawData);
     let grid = new Array(300).fill(0).map(() => new Array(300).fill(0));
@@ -63,29 +82,20 @@ function solve({ lines, rawData }) {
         }
     }
 
+    // let ans = kadane2D(grid);
+    // console.log(ans);
+
     let maxx = { powerLevel: -Infinity, x: 0, y: 0, size: 0 };
 
-    // x
-    // x + highestFactor
-    // x + 2 * highestFactor
     for (let i = 2; i <= 300; i++) {
-        // const highestFactor = highestFactors.get(i);
-        // const multiple = i / highestFactor;
         for (let x = 0; x < 300 - i; x++) {
             for (let y = 0; y < 300 - i; y++) {
-                // try 2
-                // for (let j = 0; j < multiple; j++) {
-                //     for (let k = 0; k < multiple; k++) {
-                //         grid[x][y][i] += grid[x + j * highestFactor][y + k * highestFactor][highestFactor];
-                //     }
-                // }
-
                 let gridPowerLevel = gridMax[x][y];
                 for (let j = 0; j < i; j++) {
                     gridPowerLevel += grid[x + i - 1][y + j] + grid[x + j][y + i - 1];
                 }
                 gridMax[x][y] = gridPowerLevel;
-                // const gridPowerLevel = grid[x][y][i];
+
                 if (gridPowerLevel > maxx.powerLevel) {
                     maxx.powerLevel = gridPowerLevel;
                     maxx.x = x + 1;
