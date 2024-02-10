b6a82b6438dc7c4cfe5015bb296aa50ca7e582f8
William Higgins <higginsiv@gmail.com>
2024-02-10 00:27:58 -0500

    2018 day 11 p2 time improvements (7s to 2s)

--- 2018/js/Day11/problem2.js
+++ 2018/js/Day11/problem2.js
@@ -2,6 +2,40 @@ module.exports = { solve: solve };
 
 function solve({ lines, rawData }) {
     const { floor } = Math;
+
+    // function kadane(arr) {
+    //     let localMax = 0;
+    //     let globalMax = -Infinity;
+    //     let localStart = 0;
+    //     let globalStart = 0;
+
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr[i] > arr[i] + localMax) {
+    //             localMax = arr[i];
+    //             localStart = i;
+    //         } else {
+    //             localMax = arr[i] + localMax;
+    //         }
+
+    //         if (localMax > globalMax) {
+    //             globalMax = localMax;
+    //             globalStart = localStart;
+    //         }
+    //     }
+
+    //     return { maxSum: globalMax, startIndex: globalStart };
+    // }
+
+    // console.log(kadane([1, 2, -3, -4, 1, 8, -30]));
+    // process.exit(0);
+    // function getHighestFactor(n) {
+    //     for (let i = n - 1; i > 0; i--) {
+    //         if (n % i === 0) {
+    //             return i;
+    //         }
+    //     }
+    // }
+
     function getPowerLevel(x, y, serialNumber) {
         const rackId = x + 10;
         let powerLevel = rackId * y;
@@ -12,43 +46,55 @@ function solve({ lines, rawData }) {
         return powerLevel;
     }
 
+    // let highestFactors = new Map();
+    // for (let i = 1; i <= 300; i++) {
+    //     highestFactors.set(i, getHighestFactor(i));
+    // }
+
     const serialNumber = Number(rawData);
-    let grid = new Array(300)
-        .fill(0)
-        .map(() => new Array(300).fill(0).map(() => new Array(300).fill(0)));
+    let grid = new Array(300).fill(0).map(() => new Array(300).fill(0));
+
+    let gridMax = new Array(300).fill(0).map(() => new Array(300).fill(0));
 
     for (let x = 1; x <= 300; x++) {
         for (let y = 1; y <= 300; y++) {
-            grid[x - 1][y - 1][0] = getPowerLevel(x, y, serialNumber);
-            grid[x - 1][y - 1][1] = getPowerLevel(x, y, serialNumber);
+            grid[x - 1][y - 1] = getPowerLevel(x, y, serialNumber);
+            gridMax[x - 1][y - 1] = grid[x - 1][y - 1];
         }
     }
 
-    let maxPowerLevel = -Infinity;
-    let maxPowerLevelX;
-    let maxPowerLevelY;
-    let maxPowerLevelSize;
+    let maxx = { powerLevel: -Infinity, x: 0, y: 0, size: 0 };
 
-    for (let i = 1; i <= 300; i++) {
+    // x
+    // x + highestFactor
+    // x + 2 * highestFactor
+    for (let i = 2; i <= 300; i++) {
+        // const highestFactor = highestFactors.get(i);
+        // const multiple = i / highestFactor;
         for (let x = 0; x < 300 - i; x++) {
             for (let y = 0; y < 300 - i; y++) {
-                let previous = grid[x][y][i - 1];
-                let gridPowerLevel = previous;
-                for (let j = 0; j < i; j++) {
-                    gridPowerLevel += grid[x + i - 1][y + j][1];
-                    gridPowerLevel += grid[x + j][y + i - 1][1];
-                }
-                grid[x][y][i] = gridPowerLevel;
+                // try 2
+                // for (let j = 0; j < multiple; j++) {
+                //     for (let k = 0; k < multiple; k++) {
+                //         grid[x][y][i] += grid[x + j * highestFactor][y + k * highestFactor][highestFactor];
+                //     }
+                // }
 
-                if (gridPowerLevel > maxPowerLevel) {
-                    maxPowerLevel = gridPowerLevel;
-                    maxPowerLevelX = x + 1;
-                    maxPowerLevelY = y + 1;
-                    maxPowerLevelSize = i;
+                let gridPowerLevel = gridMax[x][y];
+                for (let j = 0; j < i; j++) {
+                    gridPowerLevel += grid[x + i - 1][y + j] + grid[x + j][y + i - 1];
+                }
+                gridMax[x][y] = gridPowerLevel;
+                // const gridPowerLevel = grid[x][y][i];
+                if (gridPowerLevel > maxx.powerLevel) {
+                    maxx.powerLevel = gridPowerLevel;
+                    maxx.x = x + 1;
+                    maxx.y = y + 1;
+                    maxx.size = i;
                 }
             }
         }
     }
 
-    return { value: `${maxPowerLevelX},${maxPowerLevelY},${maxPowerLevelSize}` };
+    return { value: `${maxx.x},${maxx.y},${maxx.size}` };
 }
