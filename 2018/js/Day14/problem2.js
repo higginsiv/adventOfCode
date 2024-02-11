486365ce0c82bf374ec1055b9e5d4fe580a2bd26
William Higgins <higginsiv@gmail.com>
2024-02-11 18:01:02 -0500

    2018 14 removed unneeded array

--- 2018/js/Day14/problem2.js
+++ 2018/js/Day14/problem2.js
@@ -1,6 +1,19 @@
 module.exports = { solve: solve };
 
 function solve({ lines, rawData }) {
+    function push(value) {
+        recipes.push(value);
+        if (value === recipeSequence[spotInSequence]) {
+            spotInSequence++;
+            if (spotInSequence === recipeSequence.length) {
+                return true;
+            }
+        } else {
+            spotInSequence = 0;
+        }
+        return false;
+    }
+
     const recipeSequence = rawData.split('').map(Number);
 
     let recipes = [3, 7];
@@ -13,23 +26,15 @@ function solve({ lines, rawData }) {
     while (spotInSequence < recipeSequence.length) {
         const sum = recipes[elf1] + recipes[elf2];
         const toPush = [];
+        let finished = false;
         if (sum >= 10) {
-            toPush.push(1);
-            toPush.push(sum - 10);
-        } else {
-            toPush.push(sum);
-        }
-
-        for (let i = 0; i < toPush.length; i++) {
-            recipes.push(toPush[i]);
-            if (toPush[i] === recipeSequence[spotInSequence]) {
-                spotInSequence++;
-                if (spotInSequence === recipeSequence.length) {
-                    break;
-                }
-            } else {
-                spotInSequence = 0;
+            finished = push(1);
+            if (finished) {
+                break;
             }
+            finished = push(sum - 10);
+        } else {
+            finished = push(sum);
         }
 
         elf1 = (elf1 + recipes[elf1] + 1) % recipes.length;
