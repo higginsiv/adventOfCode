b1cf42715143e0a075382f0f0859bc0053f81f64
William Higgins <higginsiv@gmail.com>
2024-02-17 21:25:03 -0500

    2018 20 p1 progress

--- 2018/js/Day20/problem1.js
+++ 2018/js/Day20/problem1.js
@@ -1,27 +1,48 @@
 module.exports = {solve: solve};
 
 function solve({lines, rawData}) {
+    function getOpposite(direction) {
+        if (direction === 'N') {
+            return 'S';
+        } else if (direction === 'E') {
+            return 'W';
+        } else if (direction === 'S') {
+            return 'N';
+        } else if (direction === 'W') {
+            return 'E';
+        }
+    }
+
     function getKey(x, y) {
         return `${x},${y}`;
     }
 
     let directions = rawData.substring(1, rawData.length - 1).split('');
 
-    let current = {x: 0, y: 0};
+    // TODO have array of locations
+    // TODO have stack of possible moves
+    let current = {x: 0, y: 0, doors: new Set()};
+
+    let endPoints = [current];
     let locations = new Map()
+    locations.set(getKey(0, 0), current);
     let stack = [];
     let index = 0;
     while (index < directions.length) {
         let direction = directions[index];
         if (direction === '(') {
-            stack.push({x: current.x, y: current.y});
+            stack.push({x: current.x, y: current.y, doors: current.doors});
         } else if (direction === ')') {
             // TODO not sure if this pop makes sense
             current = stack.pop();
         } else if (direction === '|') {
+            // todo record new endpoint
             current = stack[stack.length - 1];
         } else {
             // TODO set up doors
+            let oppositeDirection = getOpposite(direction);
+            console.log(`current: ${current.x},${current.y} direction: ${direction} doors: ${current.doors}`);
+            current.doors.add(direction);
             let x = current.x;
             let y = current.y;
             if (direction === 'N') {
@@ -37,6 +58,7 @@ function solve({lines, rawData}) {
 
             if (!locations.has(locationkey)) {
                 locations.set(locationkey, {
+                    key: locationkey,
                     x: x,
                     y: y,
                     doors: new Set()
@@ -44,12 +66,13 @@ function solve({lines, rawData}) {
             }
 
             let location = locations.get(locationkey);
-            location.doors.add(direction);
+            location.doors.add(oppositeDirection);
 
-            current = {x: x, y: y};
+            current = {x: x, y: y, doors: location.doors};
         }
         index++;
     }
+    console.log(locations);
     const answer = null;
     return {value: answer};
 }
