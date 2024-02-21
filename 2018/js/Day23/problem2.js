b6f5cd2a4902f7e22ca6c2347fe09a0454f29a54
William Higgins <higginsiv@gmail.com>
2024-02-21 01:44:37 -0500

    2018 23 p2: solved but slow as fuck

--- 2018/js/Day23/problem2.js
+++ 2018/js/Day23/problem2.js
@@ -1,7 +1,66 @@
-module.exports = {solve: solve};
+module.exports = { solve: solve };
 
-function solve({lines, rawData}) {
+function solve({ lines, rawData }) {
+    const { abs } = Math;
+    function pointsWithinManhattanDistance(p1, distance) {
+        let points = [];
+        for (let dx = 0; dx <= distance; dx++) {
+            for (let dy = 0; dy <= distance - dx; dy++) {
+                for (let dz = 0; dz <= distance - dx - dy; dz++) {
+                    points.push({ x: p1.x + dx, y: p1.y + dy, z: p1.z + dz });
+                    points.push({ x: p1.x + dx, y: p1.y + dy, z: p1.z - dz });
+                    points.push({ x: p1.x + dx, y: p1.y - dy, z: p1.z + dz });
+                    points.push({ x: p1.x + dx, y: p1.y - dy, z: p1.z - dz });
+                    points.push({ x: p1.x - dx, y: p1.y + dy, z: p1.z + dz });
+                    points.push({ x: p1.x - dx, y: p1.y + dy, z: p1.z - dz });
+                    points.push({ x: p1.x - dx, y: p1.y - dy, z: p1.z + dz });
+                    points.push({ x: p1.x - dx, y: p1.y - dy, z: p1.z - dz });
+                }
+            }
+        }
+        return points;
+    }
 
-    const answer = null;
-    return {value: answer};
+    function getKey(x, y, z) {
+        return `${x},${y},${z}`;
+    }
+
+    function getDistance(p1, p2) {
+        return abs(p1 - p2);
+    }
+
+    const nanobots = lines.map((line) => {
+        let [x, y, z, range] = line.match(/-?\d+/g).map(Number);
+        return { x, y, z, range };
+    });
+
+    let pointToBot = new Map();
+    nanobots.forEach((bot) => {
+        bot.points = pointsWithinManhattanDistance(bot, bot.range);
+        bot.points.forEach((point) => {
+            const key = getKey(point.x, point.y, point.z);
+            if (!pointToBot.has(key)) {
+                pointToBot.set(key, 0);
+            }
+            pointToBot.set(key, pointToBot.get(key) + 1);
+        });
+    });
+
+    let max = 0;
+    let bestPoints = [];
+    for (let [key, value] of pointToBot) {
+        if (value > max) {
+            max = value;
+            bestPoints = [{ key, value }];
+        } else if (value === max) {
+            bestPoints.push({ key, value });
+        }
+    }
+
+    const answer = bestPoints.reduce((acc, point) => {
+        const [x, y, z] = point.key.split(',').map(Number);
+        return acc + getDistance(0, x) + getDistance(0, y) + getDistance(0, z);
+    }, 0);
+
+    return { value: answer };
 }
