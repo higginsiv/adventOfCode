b4fd7014bd08e9137fcbf6a6bf97add80ab10522
William Higgins <higginsiv@gmail.com>
2024-02-23 00:50:28 -0500

    2018 23 2 complete but slow af

--- 2018/js/Day23/problem2.js
+++ 2018/js/Day23/problem2.js
@@ -1,7 +1,8 @@
 module.exports = { solve: solve };
 
 function solve({ lines, rawData }) {
-    const { abs, min, max } = Math;
+    const { abs, min, max, floor } = Math;
+    const PriorityQueue = require('../../../tools/queue.js');
     const OctreeNode = require('../../../tools/octree.js');
 
     function getKey(x, y, z) {
@@ -12,8 +13,14 @@ function solve({ lines, rawData }) {
         return abs(p1 - p2);
     }
 
+    function compare(a, b) {
+        return b.count - a.count;
+    }
+
+    let minRange = Infinity;
     const nanobots = lines.map((line) => {
         let [x, y, z, range] = line.match(/-?\d+/g).map(Number);
+        minRange = min(minRange, range);
         return { x, y, z, range };
     });
 
@@ -35,26 +42,54 @@ function solve({ lines, rawData }) {
     console.log({ minX, minY, minZ, maxX, maxY, maxZ });
     console.log({ x: maxX - minX, y: maxY - minY, z: maxZ - minZ });
     let root = new OctreeNode(minX, minY, minZ, maxX, maxY, maxZ);
-    root.split();
-    let queue = [...root.children];
+    root.count = 0;
 
-    while (queue.length > 0) {
-        let node = queue.shift();
-        // if node contains less bots than max, skip
-        // if node contains only one coordinate, save max bots and skip
-        let midX = (node.boundingBox.minX + node.boundingBox.maxX) / 2;
-        let midY = (node.boundingBox.minY + node.boundingBox.maxY) / 2;
-        let midZ = (node.boundingBox.minZ + node.boundingBox.maxZ) / 2;
-        node.split();
-        for (let bot of nanobots) {
-            // check if bot overlaps with node
-            // increment count if it does
+    let queue = new PriorityQueue([root], compare);
+    let maxBots = 0;
+    let shortestManhattan = Infinity;
+    let bestCoord;
+
+    let i = 0;
+    while (queue.isNotEmpty()) {
+        // i++;
+        // if (i > 50000) {
+        //     break;
+        // }
+        let node = queue.next();
+        // console.log(node, maxBots)
+
+        if (node.boundingBox.minX === node.boundingBox.maxX && node.boundingBox.minY === node.boundingBox.maxY && node.boundingBox.minZ === node.boundingBox.maxZ) {
+            maxBots = max(maxBots, node.count);
+            if (node.count === maxBots) {
+                shortestManhattan = min(shortestManhattan, getDistance(node.boundingBox.minX, 0) + getDistance(node.boundingBox.minY, 0) + getDistance(node.boundingBox.minZ, 0));
+                bestCoord = { x: node.boundingBox.minX, y: node.boundingBox.minY, z: node.boundingBox.minZ };
+            }
+            continue;
         }
 
-        // take node with highest number of bots and split
-        // add new children to queue and sort by number of bots
+        if (node.boundingBox.maxX - node.boundingBox.minX <= minRange && node.boundingBox.maxY - node.boundingBox.minY <= minRange && node.boundingBox.maxZ - node.boundingBox.minZ <= minRange && node.count < maxBots) {
+            continue;
+        }
+
+        node.split();
+        for (let child of node.children) {
+            let midX = floor((child.boundingBox.minX + child.boundingBox.maxX) / 2);
+            let midY = floor((child.boundingBox.minY + child.boundingBox.maxY) / 2);
+            let midZ = floor((child.boundingBox.minZ + child.boundingBox.maxZ) / 2);
+            child.count = 0;
+            for (let bot of nanobots) {
+                if (getDistance(bot.x, midX) + getDistance(bot.y, midY) + getDistance(bot.z, midZ) <= bot.range) {
+                    child.count++;
+                }
+            }
+
+            if ((child.boundingBox.maxX - child.boundingBox.minX > minRange || child.boundingBox.maxY - child.boundingBox.minY > minRange || child.boundingBox.maxZ - child.boundingBox.minZ > minRange) || child.count >= maxBots) {
+                queue.insert(child);
+            }
+            
+        }
     }
-    
-    const answer = 0;
+    console.log(i, maxBots, shortestManhattan, bestCoord)
+    const answer = shortestManhattan;
     return { value: answer };
 }
--- tools/octree.js
+++ tools/octree.js
@@ -1,3 +1,5 @@
+const { floor } = Math;
+
 class OctreeNode {
     constructor(minX, minY, minZ, maxX, maxY, maxZ) {
         this.boundingBox = { minX, minY, minZ, maxX, maxY, maxZ };
@@ -5,19 +7,40 @@ class OctreeNode {
     }
 
     split() {
-        let midX = (this.boundingBox.minX + this.boundingBox.maxX) / 2;
-        let midY = (this.boundingBox.minY + this.boundingBox.maxY) / 2;
-        let midZ = (this.boundingBox.minZ + this.boundingBox.maxZ) / 2;
+        let midX = floor((this.boundingBox.minX + this.boundingBox.maxX) / 2);
+        let midY = floor((this.boundingBox.minY + this.boundingBox.maxY) / 2);
+        let midZ = floor((this.boundingBox.minZ + this.boundingBox.maxZ) / 2);
 
         for (let x = 0; x < 2; x++) {
+            let minX;
+            let maxX;
+            if (this.boundingBox.minX === midX) {
+                minX = midX + x;
+                maxX = midX + x;
+            } else {
+                minX = x === 0 ? this.boundingBox.minX : midX;
+                maxX = x === 0 ? midX : this.boundingBox.maxX;
+            }
             for (let y = 0; y < 2; y++) {
+                let minY;
+                let maxY;
+                if (this.boundingBox.minY === midY) {
+                    minY = midY + y;
+                    maxY = midY + y;
+                } else {
+                    minY = y === 0 ? this.boundingBox.minY : midY;
+                    maxY = y === 0 ? midY : this.boundingBox.maxY;
+                }
                 for (let z = 0; z < 2; z++) {
-                    let minX = x === 0 ? this.boundingBox.minX : midX;
-                    let maxX = x === 0 ? midX : this.boundingBox.maxX;
-                    let minY = y === 0 ? this.boundingBox.minY : midY;
-                    let maxY = y === 0 ? midY : this.boundingBox.maxY;
-                    let minZ = z === 0 ? this.boundingBox.minZ : midZ;
-                    let maxZ = z === 0 ? midZ : this.boundingBox.maxZ;
+                    let minZ;
+                    let maxZ;
+                    if (this.boundingBox.minZ === midZ) {
+                        minZ = midZ + z;
+                        maxZ = midZ + z;
+                    } else {
+                        minZ = z === 0 ? this.boundingBox.minZ : midZ;
+                        maxZ = z === 0 ? midZ : this.boundingBox.maxZ;
+                    }
 
                     this.children.push(new OctreeNode(minX, minY, minZ, maxX, maxY, maxZ));
                 }
