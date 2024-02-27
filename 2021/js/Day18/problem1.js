71b6b5befe130a96f6d2835748beb3690986ff99
William Higgins <higginsiv@gmail.com>
2024-02-26 22:31:20 -0500

    2021 18 1 updated to binary tree approach. Got explode finished

--- 2021/js/Day18/problem1.js
+++ 2021/js/Day18/problem1.js
@@ -1,25 +1,91 @@
 module.exports = {solve: solve};
-const EXPLODED = 0;
-class Pair {
+const EXPLOSION_RESIDUE = 0;
+class Node {
     left;
     right;
     parent;
-    constructor(left, right, parent) {
+    value;
+    constructor(left, right) {
         this.left = left;
         this.right = right;
-        this.parent = parent;
     }
 }
 
 function solve({lines, rawData}) {
-    lines = lines.map(line => JSON.parse(line));
-    console.log(lines[0])
+    lines = lines.map(line => buildTree(JSON.parse(line)));
+    lines.forEach(line => {
+        printTree(line);
+        explode(findFirstNodeAtDepth(line,4));
+        printTree(line);
+        console.log('-------------------');
+    });
+
     const answer = null;
     return {value: answer};
 }
 
+// TODO clean up
+function buildTree(snailNumber, parent = null) {
+    let left = snailNumber[0];
+    let right = snailNumber[1];
+    let node = new Node(left, right);
+    node.parent = parent;
+    if (isNaN(left)) {
+        node.left = buildTree(left, node);
+    } else {
+        node.left = new Node();
+        node.left.parent = node;
+        node.left.value = left;
+    }
+    if (isNaN(right)) {
+        node.right = buildTree(right, node);
+    } else {
+        node.right = new Node();
+        node.right.parent = node;
+        node.right.value = right;
+    }
+
+    return node;
+}
+
+function printTree(node) {
+    function buildTreeString(node) {
+        let treeString = "";
+        if (node == null) {
+            return;
+        }
+
+        if (node.value != null) {
+            treeString += node.value;
+            return treeString;
+        }
+        if (node.value != null) {
+            treeString += node.value;
+        } else {
+            treeString += "(";
+            treeString += buildTreeString(node.left);
+            treeString += ")";
+        }
+    
+        if (node.value != null) {
+            treeString += (',' + node.value);
+        } else {
+            treeString += "(";
+            treeString += buildTreeString(node.right);
+            treeString += ")";
+        }
+        return treeString;
+    }
+
+    console.log(buildTreeString(node));
+}
+
+
 function add(a, b) {
-    let pair = formPair(a, b);
+    const newRoot = new Node(a, b);
+    newRoot.left.parent = newRoot;
+    newRoot.right.parent = newRoot;
+    return newRoot;
 }
 
 function formPair(a, b) {
@@ -38,39 +104,101 @@ function reduce(snailNumber) {
 
 }
 
-function searchAndExplode(snailNumber, depth = 0) {
-    if (depth === 4) {
-        return snailNumber;
+function findFirstNodeAtDepth(root, depth) {
+    if (root == null || root.value != null) {
+        return null;
     }
-    let left = snailNumber[0];
-    let right = snailNumber[1];
-    const leftIsNumber = !isNaN(left);
-    const rightIsNumber = !isNaN(right);
-    // TODO work out these conditions
-    let exploded;
-    if (leftIsNumber && rightIsNumber) {
-        // Not too deep and no reason to explode
+    if (depth == 0) {
+        return root;
+    }
+    let node = findFirstNodeAtDepth(root.left, depth - 1);
+    if (node != null) {
+        return node;
+    }
+    node = findFirstNodeAtDepth(root.right, depth - 1);
+    return node;
+}
+
+function searchAndExplode(root, depth = 0) {
+    let nodeToExplode = findFirstNodeAtDepth(root, 4);
+    if (nodeToExplode == null) {
         return;
-    } else if (leftIsNumber) {
-        // Right is not a number
-        exploded = searchAndExplode(right, depth + 1);
-    } else if (rightIsNumber) {
-        // Left is not a number
-        exploded = searchAndExplode(left, depth + 1);
+    }
+
+}
+
+function explode(node) {
+    let parent = node.parent;
+    const numberToAddLeft = node.left.value;
+    const numberToAddRight = node.right.value;
+
+    let nextLeft = findNextLeft(node);
+    if (nextLeft != null) {
+        nextLeft.value += numberToAddLeft;
+    }
+
+    let nextRight = findNextRight(node);
+    if (nextRight != null) {
+        nextRight.value += numberToAddRight;
+    }
+    // TODO make this part of the constructor or something
+    if (parent.left == node) {
+        parent.left = new Node();
+        parent.left.parent = parent;
+        parent.left.value = EXPLOSION_RESIDUE;
     } else {
-        // Both are not numbers
-        exploded = searchAndExplode(left, depth + 1);
-        // Only explode right if left didn't explode
-        if (!exploded) {
-            exploded = searchAndExplode(right, depth + 1);
+        parent.right = new Node();
+        parent.right.parent = parent;
+        parent.right.value = EXPLOSION_RESIDUE;
+    }
+}
+
+function findNextLeft(node) {
+    let searchingNode = node;
+    let lastNode = null;
+    while (searchingNode.parent != null) {
+        lastNode = searchingNode;
+        searchingNode = searchingNode.parent;
+        if (searchingNode.left != lastNode) {
+            break;
         }
     }
-    if (exploded === EXPLODED) {
-        return EXPLODED;
-    } else if (exploded != null) {
-        // TODO explode
-        // TODO set exploded number to 0
-        let temp = snailNumber;
+    if (searchingNode.parent == null && searchingNode.left == lastNode) {
+        return null;
+    } else {
+        searchingNode = searchingNode.left;
+        if (searchingNode.value != null) {
+            return searchingNode;
+        }
+        while (searchingNode.right != null) {
+            searchingNode = searchingNode.right;
+        }
+        return searchingNode;
+    }
+}
+
+function findNextRight(node) {
+    let searchingNode = node;
+    let lastNode = null;
+    while (searchingNode.parent != null) {
+        lastNode = searchingNode;
+        searchingNode = searchingNode.parent;
+        if (searchingNode.right != lastNode) {
+            break;
+        }
+    }
+    
+    if (searchingNode.parent == null && searchingNode.right == lastNode) {
+        return null;
+    } else {
+        searchingNode = searchingNode.right;
+        if (searchingNode.value != null) {
+            return searchingNode;
+        }
+        while (searchingNode.left != null) {
+            searchingNode = searchingNode.left;
+        }
+        return searchingNode;
     }
 }
 
