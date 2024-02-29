808402f7e5f9ab4014e507d466a32f194efc7f6c
William Higgins <higginsiv@gmail.com>
2024-02-28 23:31:35 -0500

    2021 19 part 1 more work

--- 2021/js/Day19/problem1.js
+++ 2021/js/Day19/problem1.js
@@ -1,45 +1,50 @@
 module.exports = { solve: solve };
 const { manhattanDistance3d } = require('../../../tools/math');
 const MIN_MATCHES = 12;
+const SCANNER = 's';
+const BEACON = 'b';
+
 let grid = new Map();
 const EOL = require('os').EOL;
 function solve({ lines, rawData }) {
-
     let scanners = [];
     let scannerToRanges = new Map();
 
     rawData.split(EOL + EOL).forEach((scanner, index) => {
         let lines = scanner.split(EOL);
         lines.shift();
-        beacons = lines.map((beacon) => {
+        let beacons = lines.map((beacon, index) => {
             beacon = beacon.match(/-?\d+/g).map(Number);
             // return { x: beacon[0], y: 0, z: 0 };
             // TODO undo this comment when adding more dimensions
-            return {init: {x: beacon[0], y: beacon[1], z: beacon[2]}};
+            return { key: index, init: { x: beacon[0], y: beacon[1], z: beacon[2] } };
         });
-        scanners.push({ beacons });
+        scanners.push({ beacons, shared: new Map(), key: index });
+    });
+
+    grid.set('0,0,0', SCANNER);
+    scanners[0].beacons.forEach((beacon, index) => {
+        grid.set(getKey(beacon.init), BEACON);
     });
 
     const totalBeaconSightings = scanners.reduce((acc, scanner) => acc + scanner.beacons.length, 0);
     console.log('totalBeaconSightings', totalBeaconSightings);
-    let totalBeacons = totalBeaconSightings;
-    let total2 = totalBeaconSightings;
     getAllDistances(scanners);
+
 
     for (let i = 0; i < scanners.length; i++) {
         for (let j = i + 1; j < scanners.length; j++) {
             let sharedBeaconCount = findSharedBeaconCount(scanners[i], scanners[j]);
-            // if (sharedBeaconCount >= MIN_MATCHES) {
-                totalBeacons -= sharedBeaconCount;
-            // }
+            if (sharedBeaconCount >= MIN_MATCHES) {
+                console.log('scanner ', i, 'and scanner', j, 'share', sharedBeaconCount, 'beacons');
+                console.log('---', scanners[i].shared.get(scanners[j].key));
+            }
         }
     }
-    for (let i = 0; i < scanners.length; i++) {
-        for (let j = scanners[i].beacons.length - 1; j >= 0; j--) {
-            total2 -= scanners[i].beacons[j].shared ? 1 : 0;
-        }
-    }
-    console.log(total2)
+
+
+
+    console.log(total2);
     // printScanners(scanners);
     const answer = totalBeacons;
     return { value: answer };
@@ -60,29 +65,43 @@ function getAllDistances(scanners) {
 
 function findSharedBeaconCount(scanner1, scanner2) {
     let sharedBeaconCount = 0;
+    let sharedBeacons1 = [];
+    let sharedBeacons2 = [];
     scanner1.beacons.forEach((beacon1, index) => {
         scanner2.beacons.forEach((beacon2, index) => {
             let overlappingDistances = 0;
             for (let i = 0; i < beacon1.distances.length; i++) {
                 if (beacon1.distances.includes(beacon2.distances[i])) {
-                    beacon2.tentativeReal = {x: beacon1.init.x, y: beacon1.init.y, z: beacon1.init.z};
                     overlappingDistances++;
                 }
             }
             if (overlappingDistances >= MIN_MATCHES - 1) {
-                // beacon1.shared = true;
-                // beacon2.shared = true;
                 sharedBeaconCount++;
-                beacon2.real = {x: beacon2.tentativeReal.x, y: beacon2.tenativeReal.y, z: beacon2.tentativeReal.z};
+                sharedBeacons1.push(beacon1.key);
+                sharedBeacons2.push(beacon2.key);
             }
         });
     });
+    if (sharedBeaconCount >= MIN_MATCHES) {
+        scanner1.shared.set(scanner2.key, sharedBeacons1);
+        scanner2.shared.set(scanner1.key, sharedBeacons2);
+    }
     return sharedBeaconCount;
 }
 
-function alignScanners(scanner1, scanner2, grid) {
+function mapToGrid(scanners, grid) {
+    let queue = [];
+    while (queue.length > 0) {
+        let point = queue.shift();
 
+    }
 }
+
+function getKey(point) {
+    return `${point.x},${point.y},${point.z}`;
+}
+
+function alignScanners(scanner1, scanner2, grid) {}
 function printScanners(scanners) {
     scanners.forEach((scanner, index) => {
         console.log('scanner', index);
