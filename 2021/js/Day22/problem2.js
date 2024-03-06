e835c399dabfb79c7414a709f28e0e73a8bfdc35
William Higgins <higginsiv@gmail.com>
2024-03-05 21:18:27 -0500

    2022 22 p2 ripped out so wouldn't run for long time

--- 2021/js/Day22/problem2.js
+++ 2021/js/Day22/problem2.js
@@ -8,30 +8,7 @@ function solve({lines, rawData}) {
     const instructions = parseData(lines);
     let on = new Set();
     instructions.forEach(instruction => {
-        const [move, x1, x2, y1, y2, z1, z2] = instruction;
-        if (move === ON) {
-            for (let x = MIN; x <= MAX; x++) {
-                // if (!isBetween(x1, x2, x)) continue;
-                for (let y = MIN; y <= MAX; y++) {
-                    // if (!isBetween(y1, y2, y)) continue;
-                    for (let z = MIN; z <= MAX; z++) {
-                        // if (!isBetween(z1, z2, z)) continue;
-                        on.add(`${x},${y},${z}`);
-                    }
-                }
-            }
-        } else {
-            for (let x = MIN; x <= MAX; x++) {
-                // if (!isBetween(x1, x2, x)) continue;
-                for (let y = MIN; y <= MAX; y++) {
-                    // if (!isBetween(y1, y2, y)) continue;
-                    for (let z = MIN; z <= MAX; z++) {
-                        // if (!isBetween(z1, z2, z)) continue;
-                        on.delete(`${x},${y},${z}`);
-                    }
-                }
-            }
-        }
+        
     });
 
     const answer = on.size;
