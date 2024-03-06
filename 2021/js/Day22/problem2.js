c655802d00f6897e92125c45215e4188c8c3ceca
William Higgins <higginsiv@gmail.com>
2024-03-05 21:40:17 -0500

    2022 22 p2 started breaking cuboids up in one dimension

--- 2021/js/Day22/problem2.js
+++ 2021/js/Day22/problem2.js
@@ -23,3 +23,46 @@ function parseData(lines) {
         return line;
     });
 }
+
+function breakCuboidsUp(cube1, cube2) {
+    // No overlap
+    if (cube1.x2 < cube2.x1 || cube1.x1 > cube2.x2) {
+        return [cube1, cube2];
+    }
+
+    // cube1 is inside cube2
+    if (cube1.x1 >= cube2.x1 && cube1.x2 <= cube2.x2) {
+        // TODO - label cubes as on or off
+        return [
+            {x1: cube2.x1, x2: cube1.x1 - 1},
+            {x1: cube1.x2 + 1, x2: cube2.x2},
+            {x1: cube1.x1, x2: cube1.x2}
+        ];
+    }
+
+    // cube2 is inside cube1
+    if (cube2.x1 >= cube1.x1 && cube2.x2 <= cube1.x2) {
+        // TODO - label cubes as on or off
+        return [
+            {x1: cube1.x1, x2: cube2.x1 - 1},
+            {x1: cube2.x2 + 1, x2: cube1.x2},
+            {x1: cube2.x1, x2: cube2.x2}
+        ];
+    }
+
+    // cube1 overlaps on the left
+    if (cube1.x1 < cube2.x1) {
+        return [
+            {x1: cube1.x1, x2: cube2.x1 - 1},
+            {x1: cube2.x1, x2: cube1.x2},
+            {x1: cube1.x2 + 1, x2: cube2.x2}
+        ];
+    }
+
+    // cube1 overlaps on the right
+    return [
+        {x1: cube2.x1, x2: cube1.x1 - 1},
+        {x1: cube1.x1, x2: cube2.x2},
+        {x1: cube1.x1, x2: cube1.x2}
+    ];
+}
