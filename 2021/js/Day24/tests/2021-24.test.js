9b7745cfd3228a452c6c1e3aa39719cce78f21b2
William Higgins <higginsiv@gmail.com>
2024-03-17 22:43:07 -0400

    2021 24 tests fixed to be strings

--- 2021/js/Day24/tests/2021-24.test.js
+++ 2021/js/Day24/tests/2021-24.test.js
@@ -6,10 +6,10 @@ const data = fr.getInputForFunction('2021', '24');
 
 describe(`2021 Day 24`, () => {
     test('Part 1', () => {
-        expect(part1(data).value).toBe(49917929934999);
+        expect(part1(data).value).toBe('49917929934999');
     });
 
     test('Part 2', () => {
-        expect(part2(data).value).toBe(11911316711816);
+        expect(part2(data).value).toBe('11911316711816');
     });
 });
