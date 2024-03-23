550e4f1293caf02206f42baf39562ea7162b09bb
William Higgins <higginsiv@gmail.com>
2024-03-23 00:23:52 -0400

    2019 16 2 sped up

--- 2019/js/Day16/problem2.js
+++ 2019/js/Day16/problem2.js
@@ -1,27 +1,26 @@
 module.exports = { solve: solve };
 
 function solve({ lines, rawData }) {
+    const REPEATS = 10000;
+
     let input = rawData.split('').map(Number);
     const offset = Number(input.slice(0, 7).join(''));
     const inputLength = input.length;
     const realLength = inputLength * 10000;
     const PHASES = 100;
 
-    let output = new Map();
+    let real = [];
+
+    for (let i = 0; i < REPEATS; i++) {
+        real.push(...input);
+    }
+
     for (let phase = 0; phase < PHASES; phase++) {
         for (let i = realLength - 1; i >= offset; i--) {
-            let previousValue = output.get(i + 1) || 0;
-            let nextValue;
-            if (phase === 0) {
-                nextValue = input[i % inputLength];
-            } else {
-                nextValue = output.get(i);
-            }
-
-            output.set(i, (nextValue + previousValue) % 10);
+            real[i] = (real[i] + (real[i + 1] || 0)) % 10;
         }
     }
 
-    const answer = Array.from(output.values()).slice(-8).reverse().join('');
+    const answer = real.slice(offset, offset + 8).join('');
     return { value: answer };
 }
