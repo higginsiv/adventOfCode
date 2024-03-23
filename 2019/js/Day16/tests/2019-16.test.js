fc41176cf8225363378941edc5025014ca9be053
William Higgins <higginsiv@gmail.com>
2024-03-23 00:09:19 -0400

    2019 16 fixed tests

--- 2019/js/Day16/tests/2019-16.test.js
+++ 2019/js/Day16/tests/2019-16.test.js
@@ -6,10 +6,10 @@ const data = fr.getInputForFunction('2019', '16');
 
 describe(`2019 Day 16`, () => {
     test('Part 1', () => {
-        expect(part1(data).value).toBe(23135243);
+        expect(part1(data).value).toBe('23135243');
     });
 
     test('Part 2', () => {
-        expect(part2(data).value).toBe(21130597);
+        expect(part2(data).value).toBe('21130597');
     });
 });
