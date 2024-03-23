a151ce13cd0229cc369ea356ef7d10a4d04ebd55
William Higgins <higginsiv@gmail.com>
2024-03-23 00:53:43 -0400

    2019 17 1 complete

---
+++ 2019/js/Day17/problem1.js
@@ -0,0 +1,61 @@
+module.exports = { solve: solve };
+const { IntCode } = require('../common/IntCode.js');
+const [SCAFFOLD, SPACE, ROBOT_UP, ROBOT_DOWN, ROBOT_LEFT, ROBOT_RIGHT] = [
+    '#',
+    '.',
+    '^',
+    'v',
+    '<',
+    '>',
+];
+
+function solve({ lines, rawData }) {
+    let ic = new IntCode(rawData, null, 0, [], []);
+
+    let { output } = ic.run();
+
+    let grid = [];
+    let row = [];
+    while (output.length > 0) {
+        const value = output.shift();
+        if (value === 10) {
+            grid.push(row);
+            row = [];
+        } else {
+            row.push(String.fromCharCode(value));
+        }
+    }
+
+    let alignmentSum = 0;
+    for (let i = 1; i < grid.length - 1; i++) {
+        for (let j = 1; j < grid[i].length - 1; j++) {
+            let current = grid[i][j];
+            let left = grid[i][j - 1];
+            let right = grid[i][j + 1];
+            let up = grid[i - 1][j];
+            let down = grid[i + 1][j];
+
+            if (
+                isRobotOrScaffold(current) &&
+                isRobotOrScaffold(left) &&
+                isRobotOrScaffold(right) &&
+                isRobotOrScaffold(up) &&
+                isRobotOrScaffold(down)
+            ) {
+                alignmentSum += i * j;
+            }
+        }
+    }
+    const answer = alignmentSum;
+    return { value: answer };
+}
+
+function isRobotOrScaffold(value) {
+    return (
+        value === SCAFFOLD ||
+        value === ROBOT_UP ||
+        value === ROBOT_DOWN ||
+        value === ROBOT_LEFT ||
+        value === ROBOT_RIGHT
+    );
+}
