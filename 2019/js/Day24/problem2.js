a78cc935e68f82dd763bdf47cd2dbf57f1af3da2
William Higgins <higginsiv@gmail.com>
2024-04-03 00:51:14 -0400

    2019 24 2 started

--- 2019/js/Day24/problem2.js
+++ 2019/js/Day24/problem2.js
@@ -1,7 +1,119 @@
-module.exports = {solve: solve};
+module.exports = { solve: solve };
 
-function solve({lines, rawData}) {
+function solve({ lines, rawData }) {
+    const [BUG, EMPTY] = [1, 0];
+    const MINUTES = 10;
+    let grids = new Map();
+    let grid = lines.map((line) => line.split('').map((char) => (char === '#' ? 1 : 0)));
+    grids.set(0, grid);
 
-    const answer = null;
-    return {value: answer};
+    let deltas = [
+        [0, -1],
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+    ];
+    let answer;
+    let lowestLevel = 0;
+    let highestLevel = 0;
+    for (let i = 0; i < MINUTES; i++) {
+        let newGrids = new Map();
+        for (let [level, grid] of grids) {
+            newGrids.set(
+                level,
+                grid.map((row) => row.slice()),
+            );
+        }
+
+        lowestLevel--;
+        highestLevel++;
+        for (let level = lowestLevel; level <= highestLevel; level++) {
+            if (!newGrids.has(level)) {
+                newGrids.set(
+                    level,
+                    Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => 0)),
+                );
+            }
+
+            let newGrid = newGrids.get(level).map((row) => row.slice());
+            for (let y = 0; y < grid.length; y++) {
+                for (let x = 0; x < grid[y].length; x++) {
+                    let bugNeighbors = 0;
+                    let neighbors = getNeighbors(level, x, y);
+                    neighbors.forEach(([level, x, y]) => {
+                        if (!newGrids.has(level)) {
+                            newGrids.set(
+                                level,
+                                Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => 0)),
+                            );
+                        } else if (level < lowestLevel || level > highestLevel) {
+                            return;
+                        } else {
+                            const grid = grids.get(level);
+                            // console.log(level, x, y)
+                            // console.log(grid)
+                            const value = grid != null ? grid[y][x] : 0;
+                            bugNeighbors += value;
+                        }
+                    });
+                    
+                    if (grid[y][x] === BUG && bugNeighbors !== 1) {
+                        newGrid[y][x] = EMPTY;
+                    } else if (grid[y][x] === EMPTY && (bugNeighbors === 1 || bugNeighbors === 2)) {
+                        newGrid[y][x] = BUG;
+                    }
+                }
+            }
+
+            newGrids.set(level, newGrid);
+        }
+        grids = newGrids;
+    }
+
+    function getNeighbors(level, x, y) {
+        let neighbors = [];
+        for (let [dx, dy] of deltas) {
+            const newX = x + dx;
+            const newY = y + dy;
+            if (newX === 2 && newY === 2) {
+                if (dx === 0 && dy === -1) {
+                    for (let i = 0; i < 5; i++) {
+                        neighbors.push([level + 1, i, 4]);
+                    }
+                } else if (dx === 1 && dy === 0) {
+                    for (let i = 0; i < 5; i++) {
+                        neighbors.push([level + 1, 0, i]);
+                    }
+                } else if (dx === 0 && dy === 1) {
+                    for (let i = 0; i < 5; i++) {
+                        neighbors.push([level + 1, i, 0]);
+                    }
+                } else if (dx === -1 && dy === 0) {
+                    for (let i = 0; i < 5; i++) {
+                        neighbors.push([level + 1, 4, i]);
+                    }
+                }
+            } else if (newX >= 0 && newX < 5 && newY >= 0 && newY < 5) {
+                neighbors.push([level, newX, newY]);
+            } else {
+                if (newX < 0) {
+                    neighbors.push([level - 1, 1, 2]);
+                } else if (newX >= 5) {
+                    neighbors.push([level - 1, 3, 2]);
+                } else if (newY < 0) {
+                    neighbors.push([level - 1, 2, 1]);
+                } else if (newY >= 5) {
+                    neighbors.push([level - 1, 2, 3]);
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    answer = [...grids.values()].reduce((acc, grid) => {
+        return acc + grid.flat().filter((val) => val === BUG).length;
+    }, 0);
+    return { value: answer };
 }
+
+// 1109 too low
