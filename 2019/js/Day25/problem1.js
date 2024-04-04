799c08e2d64b4116271a6192e0645088451285db
William Higgins <higginsiv@gmail.com>
2024-04-03 22:02:55 -0400

    2019 25 AND ALL OF AOC COMPLETE

---
+++ 2019/js/Day25/problem1.js
@@ -0,0 +1,53 @@
+module.exports = { solve: solve };
+const readline = require('readline');
+const { IntCode } = require('../common/IntCode.js');
+
+// Input Options
+// north, east, south, west
+// take <item>
+// drop <item>
+// inv
+async function solve({ lines, rawData }) {
+    const QUIT = 'q';
+
+    let ic = new IntCode(rawData, null, 0, [], []);
+
+    // TODO export memory after each step so that I can rapidly try all combinations at the door
+    while (true) {
+        let { output, pointer } = ic.run();
+        console.log(output.map((x) => String.fromCharCode(x)).join(''));
+        let input = await getInput();
+
+        if (input === QUIT) {
+            break;
+        }
+
+        ic.addAsciiInput([input]);
+        ic.setState({ memory: ic.memory, pointer: pointer });
+    }
+
+    const answer = null;
+    return { value: answer };
+}
+
+function getInput() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise((resolve) => {
+        rl.question('Enter input: ', (answer) => {
+            resolve(answer);
+            rl.close();
+        });
+    });
+}
+
+// NOTES
+// BAD: giant electromagnet, infinite loop, photons, escape pod, molten lava
+// GOOD: candy cane, prime number, mutex, dehydrated water, coin, manifold, fuel cell, cake
+// Candy cane alone is too heavy
+// Fuel Cell and Dehydrated Water are too heavy together. Dehydrated water is too light with all others
+
+// WORKING: mutex, fuel cell, cake, coin
---
+++ 2019/js/Day25/tests/2019-25.test.js
@@ -0,0 +1,10 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+        
+const data = fr.getInputForFunction('2019', '25');
+        
+describe(`2019 Day 25`, () => {
+    test('Part 1', () => {
+        expect('play the game').toBe('play the game');
+    });
+});
