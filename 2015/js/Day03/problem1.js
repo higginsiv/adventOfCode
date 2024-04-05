edca4cd9a8828155eefad1045f6e959719a2944f
William Higgins <higginsiv@gmail.com>
2024-04-04 20:37:46 -0400

    2015 3 refactor

--- 2015/js/Day03/problem1.js
+++ 2015/js/Day03/problem1.js
@@ -1,42 +1,41 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '03', '1'];
+module.exports = { solve: solve };
 
-const [NORTH, EAST, SOUTH, WEST] = ['^', '>', 'v', '<'];
+function solve({ lines, rawData }) {
+    const [NORTH, EAST, SOUTH, WEST] = ['^', '>', 'v', '<'];
 
-let curX = 0;
-let curY = 0;
-let locations = new Map();
-locations.set(makeKey(curX, curY), 1);
+    let curX = 0;
+    let curY = 0;
+    let locations = new Map();
+    locations.set(makeKey(curX, curY), 1);
 
-fr.getInput(year, day, '').forEach((x) => {
-    switch (x) {
-        case NORTH:
-            curY++;
-            break;
-        case EAST:
-            curX++;
-            break;
-        case SOUTH:
-            curY--;
-            break;
-        case WEST:
-            curX--;
-            break;
-        default:
-            console.log('f');
-    }
+    rawData.split('').forEach((x) => {
+        switch (x) {
+            case NORTH:
+                curY++;
+                break;
+            case EAST:
+                curX++;
+                break;
+            case SOUTH:
+                curY--;
+                break;
+            case WEST:
+                curX--;
+                break;
+            default:
+                console.log('f');
+        }
 
-    let key = makeKey(curX, curY);
-    let presentCount = locations.get(key);
-    presentCount = presentCount == null ? 1 : presentCount + 1;
-    locations.set(key, presentCount);
-});
+        let key = makeKey(curX, curY);
+        let presentCount = locations.get(key);
+        presentCount = presentCount == null ? 1 : presentCount + 1;
+        locations.set(key, presentCount);
+    });
 
-let answer = locations.size;
+    const answer = locations.size;
+    return { value: answer };
+}
 
 function makeKey(x, y) {
     return x + '.' + y;
 }
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
--- 2015/js/Day03/problem2.js
+++ 2015/js/Day03/problem2.js
@@ -1,51 +1,50 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '03', '2'];
+module.exports = { solve: solve };
 
-const [NORTH, EAST, SOUTH, WEST] = ['^', '>', 'v', '<'];
+function solve({ lines, rawData }) {
+    const [NORTH, EAST, SOUTH, WEST] = ['^', '>', 'v', '<'];
 
-let curX = 0;
-let curY = 0;
-let roboX = 0;
-let roboY = 0;
+    let curX = 0;
+    let curY = 0;
+    let roboX = 0;
+    let roboY = 0;
 
-let locations = new Map();
-locations.set(makeKey(curX, curY), 2);
+    let locations = new Map();
+    locations.set(makeKey(curX, curY), 2);
 
-fr.getInput(year, day, '').forEach((x, index) => {
-    const roboTurn = index % 2 == 1;
-    switch (x) {
-        case NORTH:
-            roboTurn ? roboY++ : curY++;
-            break;
-        case EAST:
-            roboTurn ? roboX++ : curX++;
-            break;
-        case SOUTH:
-            roboTurn ? roboY-- : curY--;
-            break;
-        case WEST:
-            roboTurn ? roboX-- : curX--;
-            break;
-        default:
-            console.log('f');
-    }
+    rawData.split('').forEach((x, index) => {
+        const roboTurn = index % 2 == 1;
+        switch (x) {
+            case NORTH:
+                roboTurn ? roboY++ : curY++;
+                break;
+            case EAST:
+                roboTurn ? roboX++ : curX++;
+                break;
+            case SOUTH:
+                roboTurn ? roboY-- : curY--;
+                break;
+            case WEST:
+                roboTurn ? roboX-- : curX--;
+                break;
+            default:
+                console.log('f');
+        }
 
-    let key = makeKey(curX, curY);
-    let presentCount = locations.get(key);
-    presentCount = presentCount == null ? 1 : presentCount + 1;
-    locations.set(key, presentCount);
+        let key = makeKey(curX, curY);
+        let presentCount = locations.get(key);
+        presentCount = presentCount == null ? 1 : presentCount + 1;
+        locations.set(key, presentCount);
 
-    let roboKey = makeKey(roboX, roboY);
-    let roboPresentCount = locations.get(roboKey);
-    roboPresentCount = roboPresentCount == null ? 1 : roboPresentCount + 1;
-    locations.set(roboKey, roboPresentCount);
-});
+        let roboKey = makeKey(roboX, roboY);
+        let roboPresentCount = locations.get(roboKey);
+        roboPresentCount = roboPresentCount == null ? 1 : roboPresentCount + 1;
+        locations.set(roboKey, roboPresentCount);
+    });
 
-let answer = locations.size;
+    const answer = locations.size;
+    return { value: answer };
+}
 
 function makeKey(x, y) {
     return x + '.' + y;
 }
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
