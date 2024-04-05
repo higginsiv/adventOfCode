492ef318d00d7322620f7ad165ebf041ea536592
William Higgins <higginsiv@gmail.com>
2024-04-04 20:32:37 -0400

    2015 2 refactor

--- 2015/js/Day02/problem2.js
+++ 2015/js/Day02/problem2.js
@@ -1,19 +1,17 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '02', '1'];
-const answer = fr
-    .getInput(year, day)
-    .map((x) => {
-        return x.split('x').map((v) => parseInt(v));
-    })
-    .reduce((total, curr) => {
-        let perimeters = [
-            2 * curr[0] + 2 * curr[1],
-            2 * curr[0] + 2 * curr[2],
-            2 * curr[1] + 2 * curr[2],
-        ].sort((a, b) => a - b);
-        return total + perimeters[0] + curr[0] * curr[1] * curr[2];
-    }, 0);
+module.exports = { solve: solve };
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+function solve({ lines, rawData }) {
+    const answer = lines
+        .map((x) => {
+            return x.split('x').map((v) => parseInt(v));
+        })
+        .reduce((total, curr) => {
+            let perimeters = [
+                2 * curr[0] + 2 * curr[1],
+                2 * curr[0] + 2 * curr[2],
+                2 * curr[1] + 2 * curr[2],
+            ].sort((a, b) => a - b);
+            return total + perimeters[0] + curr[0] * curr[1] * curr[2];
+        }, 0);
+    return { value: answer };
+}
