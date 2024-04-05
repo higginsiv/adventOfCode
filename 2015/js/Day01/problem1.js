5c56e34da5b4bfece767af1ba8ebcc55c32a87fc
William Higgins <higginsiv@gmail.com>
2024-04-04 20:29:43 -0400

    2015 1 refactor

--- 2015/js/Day01/problem1.js
+++ 2015/js/Day01/problem1.js
@@ -1,9 +1,8 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '01', '1'];
-const answer = fr.getInput(year, day, '').reduce((total, curr) => {
-    return curr === '(' ? total + 1 : total - 1;
-}, 0);
+module.exports = { solve: solve };
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+function solve({ lines, rawData }) {
+    const answer = rawData.split('').reduce((total, curr) => {
+        return curr === '(' ? total + 1 : total - 1;
+    }, 0);
+    return { value: answer };
+}
--- 2015/js/Day01/problem2.js
+++ 2015/js/Day01/problem2.js
@@ -1,19 +1,17 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '01', '2'];
+module.exports = { solve: solve };
 
-let floor = 0;
-let answer;
-const data = fr.getInput(year, day, '');
-
-for (let i = 0; i < data.length; i++) {
-    let dir = data[i];
-    floor = dir === '(' ? floor + 1 : floor - 1;
-    if (floor === -1) {
-        answer = i + 1;
-        break;
+function solve({ lines, rawData }) {
+    let floor = 0;
+    let answer;
+    let data = rawData.split('');
+    for (let i = 0; i < data.length; i++) {
+        let dir = data[i];
+        floor = dir === '(' ? floor + 1 : floor - 1;
+        if (floor === -1) {
+            answer = i + 1;
+            break;
+        }
     }
-}
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+    return { value: answer };
+}
