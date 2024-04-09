e1bb3c20dfb00b4804bb07b0208d306a1dff39af
William Higgins <higginsiv@gmail.com>
2024-04-09 19:59:55 -0400

    2015 10 refactor to testable

--- 2015/js/Day10/problem1.js
+++ 2015/js/Day10/problem1.js
@@ -1,25 +1,16 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '10', '1'];
-
-const ITERATIONS = 40;
-let current = fr.getInput(year, day, '').map((x) => parseInt(x));
-
-for (let i = 0; i < ITERATIONS; i++) {
-    let next = [];
-    while (current.length > 0) {
-        let char = current.shift();
-        let numChar = 1;
-        while (current[0] == char) {
-            current.shift();
-            numChar++;
-        }
-        next.push(numChar);
-        next.push(char);
+module.exports = {solve: solve};
+function solve({lines, rawData}) {
+    const ITERATIONS = 40;
+    let data = rawData;
+    
+    for (let i = 0; i < ITERATIONS; i++) {
+        data = data
+            .match(/(\d)\1{0,}/g)
+            .map((x) => x.length + x[0])
+            .join('');
     }
-    current = next;
-}
+    
+    let answer = data.length;
 
-let answer = current.length;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+    return {value: answer};
+}
--- 2015/js/Day10/problem2.js
+++ 2015/js/Day10/problem2.js
@@ -1,17 +1,16 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '10', '2'];
+module.exports = {solve: solve};
 
-const ITERATIONS = 50;
-let data = fr.getInput(year, day)[0];
-
-for (let i = 0; i < ITERATIONS; i++) {
-    data = data
-        .match(/(\d)\1{0,}/g)
-        .map((x) => x.length + x[0])
-        .join('');
+function solve({lines, rawData}) {
+    const ITERATIONS = 50;
+    let data = rawData;
+    
+    for (let i = 0; i < ITERATIONS; i++) {
+        data = data
+            .match(/(\d)\1{0,}/g)
+            .map((x) => x.length + x[0])
+            .join('');
+    }
+    
+    let answer = data.length;
+    return {value: answer};
 }
-
-let answer = data.length;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
