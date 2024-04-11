ecbb3f1ea6c5aac5387ab8270830dcea7034d29d
William Higgins <higginsiv@gmail.com>
2024-04-10 22:10:23 -0400

    2015 11 refactor

--- 2015/js/Day11/problem1.js
+++ 2015/js/Day11/problem1.js
@@ -1,65 +1,66 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2015', '11', '1'];
-const [A, Z, I, O, L] = [
-    'a'.charCodeAt(0),
-    'z'.charCodeAt(0),
-    'i'.charCodeAt(0),
-    'o'.charCodeAt(0),
-    'l'.charCodeAt(0),
-];
+module.exports = { solve: solve };
 
-let password = fr.getInput(year, day)[0].split('');
+function solve({ lines, rawData }) {
+    const [A, Z, I, O, L] = [
+        'a'.charCodeAt(0),
+        'z'.charCodeAt(0),
+        'i'.charCodeAt(0),
+        'o'.charCodeAt(0),
+        'l'.charCodeAt(0),
+    ];
 
-do {
-    password = increment(password);
-} while (!isValid(password));
+    let password = rawData.split('');
 
-function increment(password) {
-    let index = password.length - 1;
-    let doneIncrementing = false;
+    do {
+        password = increment(password);
+    } while (!isValid(password));
 
-    while (!doneIncrementing) {
-        if (password[index] == 'z') {
-            password[index] = 'a';
-            if (index == 0) {
-                password.unshift('a');
-                doneIncrementing = true;
+    function increment(password) {
+        let index = password.length - 1;
+        let doneIncrementing = false;
+
+        while (!doneIncrementing) {
+            if (password[index] == 'z') {
+                password[index] = 'a';
+                if (index == 0) {
+                    password.unshift('a');
+                    doneIncrementing = true;
+                } else {
+                    index--;
+                }
             } else {
-                index--;
+                let newCharCode = password[index].charCodeAt(0) + 1;
+                if (newCharCode == I || newCharCode == O || newCharCode == L) {
+                    newCharCode++;
+                }
+                password[index] = String.fromCharCode(newCharCode);
+                doneIncrementing = true;
             }
-        } else {
-            let newCharCode = password[index].charCodeAt(0) + 1;
-            if (newCharCode == I || newCharCode == O || newCharCode == L) {
-                newCharCode++;
+        }
+
+        return password;
+    }
+
+    function isValid(password) {
+        let passString = password.join('');
+        let matches = passString.match(/([a-z])\1{1}/g);
+        if (matches == null || matches.length < 2) {
+            return false;
+        }
+
+        let hasSequence = false;
+        for (let i = 0; i < password.length - 2; i++) {
+            if (
+                password[i].charCodeAt(0) + 1 == password[i + 1].charCodeAt(0) &&
+                password[i].charCodeAt(0) + 2 == password[i + 2].charCodeAt(0)
+            ) {
+                hasSequence = true;
+                break;
             }
-            password[index] = String.fromCharCode(newCharCode);
-            doneIncrementing = true;
         }
+        return hasSequence;
     }
 
-    return password;
+    const answer = password.join('');
+    return { value: answer };
 }
-
-function isValid(password) {
-    let passString = password.join('');
-    let matches = passString.match(/([a-z])\1{1}/g);
-    if (matches == null || matches.length < 2) {
-        return false;
-    }
-
-    let hasSequence = false;
-    for (let i = 0; i < password.length - 2; i++) {
-        if (
-            password[i].charCodeAt(0) + 1 == password[i + 1].charCodeAt(0) &&
-            password[i].charCodeAt(0) + 2 == password[i + 2].charCodeAt(0)
-        ) {
-            hasSequence = true;
-            break;
-        }
-    }
-    return hasSequence;
-}
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + password.join(''));
-console.timeEnd();
