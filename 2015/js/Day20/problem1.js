265f10af0734b64c9a8c77074bce38a4066e681b
William Higgins <higginsiv@gmail.com>
2024-04-15 21:56:42 -0400

    2015 20 refactored

--- 2015/js/Day20/problem1.js
+++ 2015/js/Day20/problem1.js
@@ -1,34 +1,33 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [YEAR, DAY, PART] = ['2015', '20', '1'];
-const DATA = fr.getInput(YEAR, DAY);
+module.exports = {solve: solve};
 
-const GOAL = parseInt(DATA[0]);
-
-let house = 1;
-
-while (true) {
-    let factors = getFactors(house);
-    let sum = 10 * factors.reduce((total, curr) => total + curr, 0);
-    if (sum >= GOAL) {
-        break;
+function solve({lines, rawData}) {
+    const GOAL = parseInt(rawData);
+    
+    let house = 1;
+    
+    while (true) {
+        let factors = getFactors(house);
+        let sum = 10 * factors.reduce((total, curr) => total + curr, 0);
+        if (sum >= GOAL) {
+            break;
+        }
+        house++;
     }
-    house++;
-}
-
-function getFactors(num) {
-    let factors = [];
-    for (let i = 1; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            factors.push(i);
-            if (num / i !== i) {
-                // check if the pair factor has already been included
-                factors.push(num / i);
+    
+    function getFactors(num) {
+        let factors = [];
+        for (let i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                factors.push(i);
+                if (num / i !== i) {
+                    // check if the pair factor has already been included
+                    factors.push(num / i);
+                }
             }
         }
+        return factors.sort((a, b) => a - b); // sort factors in ascending order
     }
-    return factors.sort((a, b) => a - b); // sort factors in ascending order
+    let answer = house;
+    
+    return {value: answer};
 }
-let answer = house;
-console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
-console.timeEnd();
