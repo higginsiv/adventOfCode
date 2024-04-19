2365ed9650330d8a22b2b0aa2df1a52f7069d56a
William Higgins <higginsiv@gmail.com>
2024-04-18 23:09:52 -0400

    2016 3 refactor for testing

--- 2016/js/Day03/problem1.js
+++ 2016/js/Day03/problem1.js
@@ -1,16 +1,15 @@
-const fr = require('../../../tools/fileReader');
-const OUTPUT = require('../../../tools/output');
-const [YEAR, DAY, PART] = ['2016', '03', '1'];
+module.exports = { solve: solve };
 
-let answer = fr
-    .getInput(YEAR, DAY)
-    .map((x) => x.match(/\d+/g).map(Number))
-    .reduce((total, current) => {
-        let [a, b, c] = current;
-        if (a + b > c && a + c > b && b + c > a) {
-            total++;
-        }
-        return total;
-    }, 0);
+function solve({ lines, rawData }) {
+    const answer = lines
+        .map((x) => x.match(/\d+/g).map(Number))
+        .reduce((total, current) => {
+            let [a, b, c] = current;
+            if (a + b > c && a + c > b && b + c > a) {
+                total++;
+            }
+            return total;
+        }, 0);
 
-OUTPUT.output(YEAR, DAY, PART, answer);
+    return { value: answer };
+}
--- 2016/js/Day03/problem2.js
+++ 2016/js/Day03/problem2.js
@@ -1,17 +1,16 @@
-const fr = require('../../../tools/fileReader');
-const OUTPUT = require('../../../tools/output');
-const [YEAR, DAY, PART] = ['2016', '03', '2'];
+module.exports = { solve: solve };
 
-const DATA = fr.getInput(YEAR, DAY).map((x) => x.match(/\d+/g).map(Number));
+function solve({ lines, rawData }) {
+    const DATA = lines.map((x) => x.match(/\d+/g).map(Number));
 
-let answer = 0;
-for (let i = 0; i < DATA.length; i += 3) {
-    for (let j = 0; j < 3; j++) {
-        let [a, b, c] = [DATA[i][j], DATA[i + 1][j], DATA[i + 2][j]];
-        if (a + b > c && a + c > b && b + c > a) {
-            answer++;
+    let answer = 0;
+    for (let i = 0; i < DATA.length; i += 3) {
+        for (let j = 0; j < 3; j++) {
+            let [a, b, c] = [DATA[i][j], DATA[i + 1][j], DATA[i + 2][j]];
+            if (a + b > c && a + c > b && b + c > a) {
+                answer++;
+            }
         }
     }
+    return { value: answer };
 }
-
-OUTPUT.output(YEAR, DAY, PART, answer);
