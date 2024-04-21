8d658fd51d9aff492d7e981b8ed4570bb3a078cb
William Higgins <higginsiv@gmail.com>
2024-04-21 17:31:37 -0400

    2019 14 refactor for tests

--- 2019/js/Day14/problem2.js
+++ 2019/js/Day14/problem2.js
@@ -1,73 +1,72 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2019', '14', '2'];
-const ORE = 'ORE';
-const FUEL = 'FUEL';
-let oreAvailable = 1000000000000;
-let keyToIngredients = new Map();
-let keyToNumberOutput = new Map();
-let ingredientsOnHand = new Map();
-let oreReliant = [];
+module.exports = { solve: solve };
 
-const data = fr.getInput(year, day).forEach((line) => {
-    let [inputs, output] = line.split(' => ');
+function solve({ lines, rawData }) {
+    const ORE = 'ORE';
+    const FUEL = 'FUEL';
+    let oreAvailable = 1000000000000;
+    let keyToIngredients = new Map();
+    let keyToNumberOutput = new Map();
+    let ingredientsOnHand = new Map();
+    let oreReliant = [];
 
-    output = output.split(' ');
-    output[0] = parseInt(output[0]);
+    const data = lines.forEach((line) => {
+        let [inputs, output] = line.split(' => ');
 
-    inputs = inputs.split(', ').map((ingredient) => {
-        ingredient = ingredient.split(' ');
-        ingredient[0] = parseInt(ingredient[0]);
-        if (ingredient[1] === ORE) {
-            oreReliant.push(output[1]);
-        }
-        return ingredient;
+        output = output.split(' ');
+        output[0] = parseInt(output[0]);
+
+        inputs = inputs.split(', ').map((ingredient) => {
+            ingredient = ingredient.split(' ');
+            ingredient[0] = parseInt(ingredient[0]);
+            if (ingredient[1] === ORE) {
+                oreReliant.push(output[1]);
+            }
+            return ingredient;
+        });
+
+        keyToIngredients.set(output[1], inputs);
+        keyToNumberOutput.set(output[1], output[0]);
     });
 
-    keyToIngredients.set(output[1], inputs);
-    keyToNumberOutput.set(output[1], output[0]);
-});
+    function calculateOre(key, amount) {
+        let onHand = ingredientsOnHand.get(key) ?? 0;
+        if (onHand >= amount) {
+            ingredientsOnHand.set(key, onHand - amount);
+            return 0;
+        } else {
+            amount -= onHand;
+            ingredientsOnHand.set(key, 0);
+        }
 
-function calculateOre(key, amount) {
-    let onHand = ingredientsOnHand.get(key) ?? 0;
-    if (onHand >= amount) {
-        ingredientsOnHand.set(key, onHand - amount);
-        return 0;
-    } else {
-        amount -= onHand;
-        ingredientsOnHand.set(key, 0);
+        let ingredients = keyToIngredients.get(key);
+        let numProduced = keyToNumberOutput.get(key);
+
+        let mult = Math.ceil(amount / numProduced);
+        let actualOutput = mult * numProduced;
+
+        if (actualOutput > amount) {
+            let currOnHand = ingredientsOnHand.get(key);
+            currOnHand += actualOutput - amount;
+            ingredientsOnHand.set(key, currOnHand);
+        }
+
+        if (ingredients[0][1] === ORE) {
+            return ingredients[0][0] * mult;
+        } else {
+            return ingredients.reduce((total, curr) => {
+                return total + calculateOre(curr[1], mult * curr[0]);
+            }, 0);
+        }
     }
 
-    let ingredients = keyToIngredients.get(key);
-    let numProduced = keyToNumberOutput.get(key);
-
-    let mult = Math.ceil(amount / numProduced);
-    let actualOutput = mult * numProduced;
-
-    if (actualOutput > amount) {
-        let currOnHand = ingredientsOnHand.get(key);
-        currOnHand += actualOutput - amount;
-        ingredientsOnHand.set(key, currOnHand);
+    let fuel = 0;
+    while (oreAvailable > 0) {
+        oreAvailable -= calculateOre(FUEL, 1);
+        fuel++;
     }
 
-    if (ingredients[0][1] === ORE) {
-        return ingredients[0][0] * mult;
-    } else {
-        return ingredients.reduce((total, curr) => {
-            return total + calculateOre(curr[1], mult * curr[0]);
-        }, 0);
+    if (oreAvailable !== 0) {
+        fuel--;
     }
+    return { value: fuel };
 }
-
-let fuel = 0;
-while (oreAvailable > 0) {
-    oreAvailable -= calculateOre(FUEL, 1);
-    fuel++;
-}
-
-if (oreAvailable !== 0) {
-    fuel--;
-}
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + fuel);
-console.timeEnd();
