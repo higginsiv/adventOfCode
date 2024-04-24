6e85af25d81e416d2528138977a34558590e7546
William Higgins <higginsiv@gmail.com>
2024-04-23 21:23:14 -0400

    2020 2 refactor for testable

--- 2020/js/Day02/problem2.js
+++ 2020/js/Day02/problem2.js
@@ -1,22 +1,24 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '02', '2'];
-const data = fr.getInput(year, day).map((x) => {
-    x = x.replace('-', ' ');
-    x = x.replace(':', '');
-    x = x.split(' ');
-    x[0] = parseInt(x[0]);
-    x[1] = parseInt(x[1]);
-    x[3] = x[3].split('');
-    return x;
-});
+module.exports = { solve: solve };
 
-let answer = data.reduce((total, curr) => {
-    const [pos1, pos2, letter, string] = curr;
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => {
+        x = x.replace('-', ' ');
+        x = x.replace(':', '');
+        x = x.split(' ');
+        x[0] = parseInt(x[0]);
+        x[1] = parseInt(x[1]);
+        x[3] = x[3].split('');
+        return x;
+    });
 
-    let charAtPos = string[pos1 - 1] === letter;
-    let charAtPos2 = string[pos2 - 1] === letter;
+    const answer = data.reduce((total, curr) => {
+        const [pos1, pos2, letter, string] = curr;
 
-    return (charAtPos && !charAtPos2) || (charAtPos2 && !charAtPos) ? total + 1 : total;
-}, 0);
+        let charAtPos = string[pos1 - 1] === letter;
+        let charAtPos2 = string[pos2 - 1] === letter;
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+        return (charAtPos && !charAtPos2) || (charAtPos2 && !charAtPos) ? total + 1 : total;
+    }, 0);
+
+    return { value: answer };
+}
