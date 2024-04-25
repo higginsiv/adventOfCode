590d5b682f66eb2b695194e48c3d61328ef3735d
William Higgins <higginsiv@gmail.com>
2024-04-24 21:59:02 -0400

    2020 3 refactor for testing

--- 2020/js/Day03/problem1.js
+++ 2020/js/Day03/problem1.js
@@ -1,22 +1,21 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '03', '1'];
-const data = fr.getInput(year, day).map((x) => x.split(''));
-const COLS = data[0].length;
-const ROWS = data.length;
-const TREE = '#';
+module.exports = { solve: solve };
 
-let row = 0;
-let col = 0;
-let collisions = 0;
-while (row < ROWS) {
-    if (data[row][col] === TREE) {
-        collisions++;
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => x.split(''));
+    const COLS = data[0].length;
+    const ROWS = data.length;
+    const TREE = '#';
+
+    let row = 0;
+    let col = 0;
+    let collisions = 0;
+    while (row < ROWS) {
+        if (data[row][col] === TREE) {
+            collisions++;
+        }
+        row = row + 1;
+        col = (col + 3) % COLS;
     }
-    row = row + 1;
-    col = (col + 3) % COLS;
+    const answer = collisions;
+    return { value: answer };
 }
-let answer = collisions;
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
