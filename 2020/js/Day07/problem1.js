93d7fd0c3beb8f59f2b5cb6d47b09c59001b351e
William Higgins <higginsiv@gmail.com>
2024-04-28 22:47:20 -0400

    2020 7 refactor for testing

--- 2020/js/Day07/problem1.js
+++ 2020/js/Day07/problem1.js
@@ -1,60 +1,60 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '07', '1'];
-const GOAL_BAG_TYPE = 'shiny gold';
+module.exports = { solve: solve };
 
-let bags = new Map();
-const data = fr.getInput(year, day).forEach((x) => {
-    x = x.replaceAll(' bags', '');
-    x = x.replaceAll(' bag', '');
-    let [bagType, childBagDetails] = x.split(' contain ');
-    childBagDetails = childBagDetails.replaceAll('.', '');
-    if (!childBagDetails.includes('no other')) {
-        childBagDetails = childBagDetails.split(', ').map((y) => {
-            y = y.replace(' ', '***');
-            y = y.split('***');
-            y[0] = parseInt(y[0]);
-            return y;
-        });
-        bags.set(bagType, childBagDetails);
-    }
-});
+function solve({ lines, rawData }) {
+    const GOAL_BAG_TYPE = 'shiny gold';
 
-let containsShinyGold = new Set();
-
-bags.forEach((value, bagType) => {
-    contains(GOAL_BAG_TYPE, bagType);
-});
-
-function contains(goalBagType, bagType) {
-    if (containsShinyGold.has(bagType)) {
-        return true;
-    }
-
-    let childrenBags = bags.get(bagType);
-    if (childrenBags == null) {
-        return false;
-    }
-
-    let childContainsGoalBagType = false;
-    childrenBags.forEach((childBagType) => {
-        if (childBagType[1] === GOAL_BAG_TYPE) {
-            childContainsGoalBagType = true;
-        } else {
-            let containsGoalBagType = contains(goalBagType, childBagType[1]);
-            if (containsGoalBagType) {
-                containsShinyGold.add(childBagType[1]);
-                childContainsGoalBagType = true;
-            }
+    let bags = new Map();
+    const data = lines.forEach((x) => {
+        x = x.replaceAll(' bags', '');
+        x = x.replaceAll(' bag', '');
+        let [bagType, childBagDetails] = x.split(' contain ');
+        childBagDetails = childBagDetails.replaceAll('.', '');
+        if (!childBagDetails.includes('no other')) {
+            childBagDetails = childBagDetails.split(', ').map((y) => {
+                y = y.replace(' ', '***');
+                y = y.split('***');
+                y[0] = parseInt(y[0]);
+                return y;
+            });
+            bags.set(bagType, childBagDetails);
         }
     });
 
-    if (childContainsGoalBagType) {
-        containsShinyGold.add(bagType);
-    }
-    return childContainsGoalBagType;
-}
+    let containsShinyGold = new Set();
 
-let answer = containsShinyGold.size;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+    bags.forEach((value, bagType) => {
+        contains(GOAL_BAG_TYPE, bagType);
+    });
+
+    function contains(goalBagType, bagType) {
+        if (containsShinyGold.has(bagType)) {
+            return true;
+        }
+
+        let childrenBags = bags.get(bagType);
+        if (childrenBags == null) {
+            return false;
+        }
+
+        let childContainsGoalBagType = false;
+        childrenBags.forEach((childBagType) => {
+            if (childBagType[1] === GOAL_BAG_TYPE) {
+                childContainsGoalBagType = true;
+            } else {
+                let containsGoalBagType = contains(goalBagType, childBagType[1]);
+                if (containsGoalBagType) {
+                    containsShinyGold.add(childBagType[1]);
+                    childContainsGoalBagType = true;
+                }
+            }
+        });
+
+        if (childContainsGoalBagType) {
+            containsShinyGold.add(bagType);
+        }
+        return childContainsGoalBagType;
+    }
+
+    const answer = containsShinyGold.size;
+    return { value: answer };
+}
--- 2020/js/Day07/problem2.js
+++ 2020/js/Day07/problem2.js
@@ -1,54 +1,52 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '07', '2'];
-const GOAL_BAG_TYPE = 'shiny gold';
+module.exports = { solve: solve };
 
-let bags = new Map();
-const data = fr.getInput(year, day).forEach((x) => {
-    x = x.replaceAll(' bags', '');
-    x = x.replaceAll(' bag', '');
-    let [bagType, childBagDetails] = x.split(' contain ');
-    childBagDetails = childBagDetails.replaceAll('.', '');
-    if (!childBagDetails.includes('no other')) {
-        childBagDetails = childBagDetails.split(', ').map((y) => {
-            y = y.replace(' ', '***');
-            y = y.split('***');
-            y[0] = parseInt(y[0]);
-            return y;
-        });
-        bags.set(bagType, childBagDetails);
-    }
-});
+function solve({ lines, rawData }) {
+    const GOAL_BAG_TYPE = 'shiny gold';
 
-let bagsToInnerBags = new Map();
-
-let answer = sumBags(GOAL_BAG_TYPE);
-
-function sumBags(bagType) {
-    if (bagsToInnerBags.get(bagType) != null) {
-        return bagsToInnerBags.get(bagType);
-    }
-
-    let childrenBags = bags.get(bagType);
-    if (childrenBags == null) {
-        return 0;
-    }
-
-    let childrenBagTotal = 0;
-    childrenBags.forEach((childBagType) => {
-        let childSum = sumBags(childBagType[1]);
-        let childTotal = childBagType[0] * childSum + childBagType[0];
-        if (bagsToInnerBags.get(childBagType) == null) {
-            bagsToInnerBags.set(childBagType, childSum);
+    let bags = new Map();
+    const data = lines.forEach((x) => {
+        x = x.replaceAll(' bags', '');
+        x = x.replaceAll(' bag', '');
+        let [bagType, childBagDetails] = x.split(' contain ');
+        childBagDetails = childBagDetails.replaceAll('.', '');
+        if (!childBagDetails.includes('no other')) {
+            childBagDetails = childBagDetails.split(', ').map((y) => {
+                y = y.replace(' ', '***');
+                y = y.split('***');
+                y[0] = parseInt(y[0]);
+                return y;
+            });
+            bags.set(bagType, childBagDetails);
         }
-        childrenBagTotal += childTotal;
     });
 
-    bagsToInnerBags.set(bagType, childrenBagTotal);
-    return childrenBagTotal;
+    let bagsToInnerBags = new Map();
+
+    const answer = sumBags(GOAL_BAG_TYPE);
+
+    function sumBags(bagType) {
+        if (bagsToInnerBags.get(bagType) != null) {
+            return bagsToInnerBags.get(bagType);
+        }
+
+        let childrenBags = bags.get(bagType);
+        if (childrenBags == null) {
+            return 0;
+        }
+
+        let childrenBagTotal = 0;
+        childrenBags.forEach((childBagType) => {
+            let childSum = sumBags(childBagType[1]);
+            let childTotal = childBagType[0] * childSum + childBagType[0];
+            if (bagsToInnerBags.get(childBagType) == null) {
+                bagsToInnerBags.set(childBagType, childSum);
+            }
+            childrenBagTotal += childTotal;
+        });
+
+        bagsToInnerBags.set(bagType, childrenBagTotal);
+        return childrenBagTotal;
+    }
+
+    return { value: answer };
 }
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
-
-// too high 23691
