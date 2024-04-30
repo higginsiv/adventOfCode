1c574eb478e260e0eca139caa9a5d86859c5405f
William Higgins <higginsiv@gmail.com>
2024-04-30 17:32:39 -0400

    2020 10 refactor test

--- 2020/js/Day10/problem1.js
+++ 2020/js/Day10/problem1.js
@@ -1,31 +1,28 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '10', '1'];
-const data = fr
-    .getInput(year, day)
-    .map((x) => parseInt(x))
-    .sort((a, b) => a - b);
+module.exports = { solve: solve };
 
-let ones = 0;
-let threes = 0;
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => parseInt(x)).sort((a, b) => a - b);
 
-for (let i = 0; i < data.length; i++) {
-    let diff;
-    if (i === 0) {
-        diff = data[i] - 0;
-    } else {
-        diff = data[i] - data[i - 1];
+    let ones = 0;
+    let threes = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        let diff;
+        if (i === 0) {
+            diff = data[i] - 0;
+        } else {
+            diff = data[i] - data[i - 1];
+        }
+
+        if (diff === 1) {
+            ones++;
+        } else if (diff === 3) {
+            threes++;
+        }
     }
 
-    if (diff === 1) {
-        ones++;
-    } else if (diff === 3) {
-        threes++;
-    }
+    threes++;
+
+    const answer = ones * threes;
+    return { value: answer };
 }
-
-threes++;
-
-let answer = ones * threes;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
--- 2020/js/Day10/problem2.js
+++ 2020/js/Day10/problem2.js
@@ -1,48 +1,45 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '10', '2'];
-const data = fr
-    .getInput(year, day)
-    .map((x) => parseInt(x))
-    .sort((a, b) => a - b);
+module.exports = { solve: solve };
 
-let combos = new Map();
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => parseInt(x)).sort((a, b) => a - b);
 
-function findCombos(curr) {
-    let ones = 0;
-    let twos = 0;
-    let threes = 0;
+    let combos = new Map();
 
-    if (data.includes(curr + 1)) {
-        if (combos.get(curr + 1) != null) {
-            ones += combos.get(curr + 1);
+    function findCombos(curr) {
+        let ones = 0;
+        let twos = 0;
+        let threes = 0;
+
+        if (data.includes(curr + 1)) {
+            if (combos.get(curr + 1) != null) {
+                ones += combos.get(curr + 1);
+            } else {
+                ones += findCombos(curr + 1);
+            }
+        }
+        if (data.includes(curr + 2)) {
+            if (combos.get(curr + 2) != null) {
+                twos += combos.get(curr + 2);
+            } else {
+                twos += findCombos(curr + 2);
+            }
+        }
+        if (data.includes(curr + 3)) {
+            if (combos.get(curr + 3) != null) {
+                threes += combos.get(curr + 3);
+            } else {
+                threes += findCombos(curr + 3);
+            }
+        }
+        if (data.indexOf(curr) === data.length - 1) {
+            combos.set(curr, 1);
+            return 1;
         } else {
-            ones += findCombos(curr + 1);
+            combos.set(curr, ones + twos + threes);
+            return ones + twos + threes;
         }
     }
-    if (data.includes(curr + 2)) {
-        if (combos.get(curr + 2) != null) {
-            twos += combos.get(curr + 2);
-        } else {
-            twos += findCombos(curr + 2);
-        }
-    }
-    if (data.includes(curr + 3)) {
-        if (combos.get(curr + 3) != null) {
-            threes += combos.get(curr + 3);
-        } else {
-            threes += findCombos(curr + 3);
-        }
-    }
-    if (data.indexOf(curr) === data.length - 1) {
-        combos.set(curr, 1);
-        return 1;
-    } else {
-        combos.set(curr, ones + twos + threes);
-        return ones + twos + threes;
-    }
+
+    const answer = findCombos(0);
+    return { value: answer };
 }
-
-let answer = findCombos(0);
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
