41ff99432cc981c9294054490d4299d55b07a6e9
William Higgins <higginsiv@gmail.com>
2024-05-03 18:06:46 -0400

    2020 16 refactor for testing

--- 2020/js/Day16/problem1.js
+++ 2020/js/Day16/problem1.js
@@ -1,66 +1,67 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const { EOL } = require('os');
-const { exit } = require('process');
-const [year, day, part] = ['2020', '16', '1'];
+module.exports = { solve: solve };
 
-class Range {
-    min;
-    max;
-    constructor(min, max) {
-        this.min = min;
-        this.max = max;
+function solve({ lines, rawData }) {
+    const { EOL } = require('os');
+
+    class Range {
+        min;
+        max;
+        constructor(min, max) {
+            this.min = min;
+            this.max = max;
+        }
     }
-}
 
-let [KEY_INFO, YOUR_TICKET, TICKETS] = fr.getInput(year, day, EOL + EOL);
+    let [KEY_INFO, YOUR_TICKET, TICKETS] = rawData.split(EOL + EOL);
 
-let keys = new Map();
-let keyNames = [];
-KEY_INFO.split(EOL).forEach((x) => {
-    let name = x.substring(0, x.indexOf(':'));
-    let rangesString = x.substring(x.indexOf(':') + 2);
-    let ranges = rangesString.split(' or ').map((r) => {
-        r = r.split('-').map((y) => parseInt(y));
-        return new Range(...r);
+    let keys = new Map();
+    let keyNames = [];
+    KEY_INFO.split(EOL).forEach((x) => {
+        let name = x.substring(0, x.indexOf(':'));
+        let rangesString = x.substring(x.indexOf(':') + 2);
+        let ranges = rangesString.split(' or ').map((r) => {
+            r = r.split('-').map((y) => parseInt(y));
+            return new Range(...r);
+        });
+        keys.set(name, ranges);
+        keyNames.push(name);
     });
-    keys.set(name, ranges);
-    keyNames.push(name);
-});
 
-let possibleKeys = new Array(keyNames.length);
-for (let i = 0; i < possibleKeys.length; i++) {
-    possibleKeys[i] = keyNames.slice();
-}
+    let possibleKeys = new Array(keyNames.length);
+    for (let i = 0; i < possibleKeys.length; i++) {
+        possibleKeys[i] = keyNames.slice();
+    }
 
-TICKETS = TICKETS.split(EOL).map((t) => {
-    t = t.split(',').map((x) => parseInt(x));
-    return t;
-});
-// REALLY lazy parsing (removing the 'nearby tickets:' line)
-TICKETS.shift();
-let invalidSum = 0;
+    TICKETS = TICKETS.split(EOL).map((t) => {
+        t = t.split(',').map((x) => parseInt(x));
+        return t;
+    });
+    // REALLY lazy parsing (removing the 'nearby tickets:' line)
+    TICKETS.shift();
+    let invalidSum = 0;
 
-TICKETS.forEach((ticket, u) => {
-    ticket.forEach((field, index) => {
-        let globalInRange = false;
-        for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
-            if (possibleKeys[index] != null) {
-                let ranges = keys.get(possibleKeys[index][i]);
-                for (let j = 0; j < ranges.length; j++) {
-                    let min = ranges[j].min;
-                    let max = ranges[j].max;
-                    if (field >= min && field <= max) {
-                        globalInRange = true;
+    TICKETS.forEach((ticket, u) => {
+        ticket.forEach((field, index) => {
+            let globalInRange = false;
+            for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
+                if (possibleKeys[index] != null) {
+                    let ranges = keys.get(possibleKeys[index][i]);
+                    for (let j = 0; j < ranges.length; j++) {
+                        let min = ranges[j].min;
+                        let max = ranges[j].max;
+                        if (field >= min && field <= max) {
+                            globalInRange = true;
+                        }
                     }
                 }
             }
-        }
-        if (!globalInRange) {
-            invalidSum += field;
-        }
+            if (!globalInRange) {
+                invalidSum += field;
+            }
+        });
     });
-});
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + invalidSum);
-console.timeEnd();
+    const answer = invalidSum;
+
+    return { value: answer };
+}
--- 2020/js/Day16/problem2.js
+++ 2020/js/Day16/problem2.js
@@ -1,107 +1,106 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const { EOL } = require('os');
-const [year, day, part] = ['2020', '16', '2'];
+module.exports = { solve: solve };
 
-class Range {
-    min;
-    max;
-    constructor(min, max) {
-        this.min = min;
-        this.max = max;
+function solve({ lines, rawData }) {
+    const { EOL } = require('os');
+
+    class Range {
+        min;
+        max;
+        constructor(min, max) {
+            this.min = min;
+            this.max = max;
+        }
     }
-}
 
-let [KEY_INFO, YOUR_TICKET, TICKETS] = fr.getInput(year, day, EOL + EOL);
+    let [KEY_INFO, YOUR_TICKET, TICKETS] = rawData.split(EOL + EOL);
 
-let keys = new Map();
-let keyNames = [];
-KEY_INFO.split(EOL).forEach((x) => {
-    let name = x.substring(0, x.indexOf(':'));
-    let rangesString = x.substring(x.indexOf(':') + 2);
-    let ranges = rangesString.split(' or ').map((r) => {
-        r = r.split('-').map((y) => parseInt(y));
-        return new Range(...r);
+    let keys = new Map();
+    let keyNames = [];
+    KEY_INFO.split(EOL).forEach((x) => {
+        let name = x.substring(0, x.indexOf(':'));
+        let rangesString = x.substring(x.indexOf(':') + 2);
+        let ranges = rangesString.split(' or ').map((r) => {
+            r = r.split('-').map((y) => parseInt(y));
+            return new Range(...r);
+        });
+        keys.set(name, ranges);
+        keyNames.push(name);
     });
-    keys.set(name, ranges);
-    keyNames.push(name);
-});
 
-let possibleKeys = new Array(keyNames.length);
-for (let i = 0; i < possibleKeys.length; i++) {
-    possibleKeys[i] = keyNames.slice();
-}
+    let possibleKeys = new Array(keyNames.length);
+    for (let i = 0; i < possibleKeys.length; i++) {
+        possibleKeys[i] = keyNames.slice();
+    }
 
-YOUR_TICKET = YOUR_TICKET.split(EOL).map((t) => {
-    t = t.split(',').map((x) => parseInt(x));
-    return t;
-});
-// REALLY lazy parsing (removing the 'your ticket:' line)
-YOUR_TICKET.shift();
+    YOUR_TICKET = YOUR_TICKET.split(EOL).map((t) => {
+        t = t.split(',').map((x) => parseInt(x));
+        return t;
+    });
+    // REALLY lazy parsing (removing the 'your ticket:' line)
+    YOUR_TICKET.shift();
 
-TICKETS = TICKETS.split(EOL).map((t) => {
-    t = t.split(',').map((x) => parseInt(x));
-    return t;
-});
-// REALLY lazy parsing (removing the 'nearby tickets:' line)
-TICKETS.shift();
+    TICKETS = TICKETS.split(EOL).map((t) => {
+        t = t.split(',').map((x) => parseInt(x));
+        return t;
+    });
+    // REALLY lazy parsing (removing the 'nearby tickets:' line)
+    TICKETS.shift();
 
-TICKETS.forEach((ticket, u) => {
-    ticket.forEach((field, index) => {
-        let globalInRange = false;
-        let badKeys = [];
+    TICKETS.forEach((ticket, u) => {
+        ticket.forEach((field, index) => {
+            let globalInRange = false;
+            let badKeys = [];
 
-        for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
-            let inRange = false;
-            if (possibleKeys[index] != null) {
-                let ranges = keys.get(possibleKeys[index][i]);
-                for (let j = 0; j < ranges.length; j++) {
-                    let min = ranges[j].min;
-                    let max = ranges[j].max;
-                    if (field >= min && field <= max) {
-                        inRange = true;
-                        globalInRange = true;
+            for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
+                let inRange = false;
+                if (possibleKeys[index] != null) {
+                    let ranges = keys.get(possibleKeys[index][i]);
+                    for (let j = 0; j < ranges.length; j++) {
+                        let min = ranges[j].min;
+                        let max = ranges[j].max;
+                        if (field >= min && field <= max) {
+                            inRange = true;
+                            globalInRange = true;
+                        }
+                    }
+                }
+                if (!inRange) {
+                    badKeys.push(i);
+                }
+            }
+            if (globalInRange) {
+                for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
+                    if (badKeys.includes(i)) {
+                        possibleKeys[index].splice(i, 1);
                     }
                 }
             }
-            if (!inRange) {
-                badKeys.push(i);
-            }
-        }
-        if (globalInRange) {
-            for (let i = possibleKeys[index].length - 1; i >= 0; i--) {
-                if (badKeys.includes(i)) {
-                    possibleKeys[index].splice(i, 1);
-                }
-            }
-        }
+        });
     });
-});
 
-let keyDeleted = true;
-while (keyDeleted) {
-    keyDeleted = false;
-    let keyToDelete;
-    for (let i = 0; i < possibleKeys.length; i++) {
-        let pks = possibleKeys[i];
-        if (pks.length === 1) {
-            keyToDelete = pks[0];
-            for (let j = 0; j < possibleKeys.length; j++) {
-                if (j !== i && possibleKeys[j].includes(keyToDelete)) {
-                    keyDeleted = true;
-                    possibleKeys[j].splice(possibleKeys[j].indexOf(keyToDelete), 1);
+    let keyDeleted = true;
+    while (keyDeleted) {
+        keyDeleted = false;
+        let keyToDelete;
+        for (let i = 0; i < possibleKeys.length; i++) {
+            let pks = possibleKeys[i];
+            if (pks.length === 1) {
+                keyToDelete = pks[0];
+                for (let j = 0; j < possibleKeys.length; j++) {
+                    if (j !== i && possibleKeys[j].includes(keyToDelete)) {
+                        keyDeleted = true;
+                        possibleKeys[j].splice(possibleKeys[j].indexOf(keyToDelete), 1);
+                    }
                 }
             }
         }
     }
+
+    const answer = possibleKeys.reduce((total, curr, index) => {
+        if (curr[0].indexOf('departure') != -1) {
+            total *= YOUR_TICKET[0][index];
+        }
+        return total;
+    }, 1);
+    return { value: answer };
 }
-
-let answer = possibleKeys.reduce((total, curr, index) => {
-    if (curr[0].indexOf('departure') != -1) {
-        total *= YOUR_TICKET[0][index];
-    }
-    return total;
-}, 1);
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
