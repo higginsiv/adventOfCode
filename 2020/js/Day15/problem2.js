201fa77d0255efaa6ebb419b9bbe935e66b6a04c
William Higgins <higginsiv@gmail.com>
2024-05-03 00:39:47 -0400

    2020 15 refactor for tests

--- 2020/js/Day15/problem2.js
+++ 2020/js/Day15/problem2.js
@@ -1,55 +1,52 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '15', '2'];
-const data = fr
-    .getInput(year, day)[0]
-    .split(',')
-    .map((x) => parseInt(x));
+module.exports = { solve: solve };
 
-const MAX = 30000000;
+function solve({ lines, rawData }) {
+    const data = rawData.split(',').map((x) => parseInt(x));
 
-class Digit {
-    saidAt;
-    amount;
-    constructor(saidAt, amount) {
-        this.saidAt = saidAt;
-        this.amount = amount;
-    }
-}
+    const MAX = 30000000;
 
-let digits = new Map();
-
-data.forEach((num, index) => {
-    digits.set(num, new Digit([index], 1));
-});
-
-let lastSaid = data[data.length - 1];
-
-for (let i = data.length; i < MAX; i++) {
-    let next;
-    let nextDigit;
-    let lastSaidDigit = digits.get(lastSaid);
-
-    if (lastSaidDigit.amount === 1) {
-        next = 0;
-    } else {
-        next = lastSaidDigit.saidAt[1] - lastSaidDigit.saidAt[0];
-    }
-
-    nextDigit = digits.get(next);
-    if (nextDigit != null) {
-        nextDigit.amount++;
-        nextDigit.saidAt.push(i);
-        if (nextDigit.saidAt.length > 2) {
-            nextDigit.saidAt.shift();
+    class Digit {
+        saidAt;
+        amount;
+        constructor(saidAt, amount) {
+            this.saidAt = saidAt;
+            this.amount = amount;
         }
-    } else {
-        digits.set(next, new Digit([i], 1));
     }
 
-    lastSaid = next;
-}
+    let digits = new Map();
 
-let answer = lastSaid;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
+    data.forEach((num, index) => {
+        digits.set(num, new Digit([index], 1));
+    });
+
+    let lastSaid = data[data.length - 1];
+
+    for (let i = data.length; i < MAX; i++) {
+        let next;
+        let nextDigit;
+        let lastSaidDigit = digits.get(lastSaid);
+
+        if (lastSaidDigit.amount === 1) {
+            next = 0;
+        } else {
+            next = lastSaidDigit.saidAt[1] - lastSaidDigit.saidAt[0];
+        }
+
+        nextDigit = digits.get(next);
+        if (nextDigit != null) {
+            nextDigit.amount++;
+            nextDigit.saidAt.push(i);
+            if (nextDigit.saidAt.length > 2) {
+                nextDigit.saidAt.shift();
+            }
+        } else {
+            digits.set(next, new Digit([i], 1));
+        }
+
+        lastSaid = next;
+    }
+
+    const answer = lastSaid;
+    return { value: answer };
+}
---
+++ 2020/js/Day15/tests/2020-15.test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2020', '15');
+
+describe(`2020 Day 15`, () => {
+    test('Part 1', () => {
+        expect(part1(data).value).toBe(758);
+    });
+
+    test('Part 2', () => {
+        expect(part2(data).value).toBe(814);
+    });
+});
