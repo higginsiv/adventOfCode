4aa92504974cc5d92d1fb327bc133778309e8946
William Higgins <higginsiv@gmail.com>
2024-05-04 22:11:16 -0400

    2020 17 refactor for testing

--- 2020/js/Day17/problem1.js
+++ 2020/js/Day17/problem1.js
@@ -1,90 +1,89 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '17', '1'];
+module.exports = { solve: solve };
 
-const [INACTIVE, ACTIVE] = ['.', '#'];
-const DELIM = '.';
-const Z_START = 0;
-const ROUNDS = 6;
+function solve({ lines, rawData }) {
+    const [INACTIVE, ACTIVE] = ['.', '#'];
+    const DELIM = '.';
+    const Z_START = 0;
+    const ROUNDS = 6;
 
-let cube = new Map();
-const data = fr.getInput(year, day).forEach((x, row) => {
-    x = x.split('');
-    x.forEach((val, col) => {
-        cube.set(generateKey(col, row, Z_START), val);
-        getNeighborKeys(col, row, Z_START).forEach((key) => {
-            if (cube.get(key) == null) {
-                cube.set(key, INACTIVE);
-            }
+    let cube = new Map();
+    lines.forEach((x, row) => {
+        x = x.split('');
+        x.forEach((val, col) => {
+            cube.set(generateKey(col, row, Z_START), val);
+            getNeighborKeys(col, row, Z_START).forEach((key) => {
+                if (cube.get(key) == null) {
+                    cube.set(key, INACTIVE);
+                }
+            });
         });
     });
-});
 
-for (let i = 0; i < ROUNDS; i++) {
-    let next = new Map();
-    cube.forEach((val, key) => {
-        let [x, y, z] = key.split(DELIM).map((n) => parseInt(n));
-        let neighbors = getNeighborKeys(x, y, z);
-        let activeNeighbors = neighbors.filter((k) => cube.get(k) === ACTIVE);
+    for (let i = 0; i < ROUNDS; i++) {
+        let next = new Map();
+        cube.forEach((val, key) => {
+            let [x, y, z] = key.split(DELIM).map((n) => parseInt(n));
+            let neighbors = getNeighborKeys(x, y, z);
+            let activeNeighbors = neighbors.filter((k) => cube.get(k) === ACTIVE);
 
-        if (val === ACTIVE && activeNeighbors.length !== 2 && activeNeighbors.length !== 3) {
-            next.set(generateKey(x, y, z), INACTIVE);
-        } else if (val === INACTIVE && activeNeighbors.length === 3) {
-            next.set(generateKey(x, y, z), ACTIVE);
-        } else {
-            next.set(generateKey(x, y, z), val);
-        }
-
-        neighbors.forEach((key) => {
-            if (next.get(key) == null) {
-                next.set(key, INACTIVE);
+            if (val === ACTIVE && activeNeighbors.length !== 2 && activeNeighbors.length !== 3) {
+                next.set(generateKey(x, y, z), INACTIVE);
+            } else if (val === INACTIVE && activeNeighbors.length === 3) {
+                next.set(generateKey(x, y, z), ACTIVE);
+            } else {
+                next.set(generateKey(x, y, z), val);
             }
+
+            neighbors.forEach((key) => {
+                if (next.get(key) == null) {
+                    next.set(key, INACTIVE);
+                }
+            });
         });
-    });
-    cube = next;
+        cube = next;
+    }
+
+    const answer = Array.from(cube.keys()).filter((key) => cube.get(key) === ACTIVE).length;
+
+    function generateKey(x, y, z) {
+        return x + DELIM + y + DELIM + z;
+    }
+
+    function getNeighborKeys(x, y, z) {
+        let neighborKeys = [];
+
+        // TODO change this to use generateKey
+        neighborKeys.push(x + 1 + DELIM + y + DELIM + z);
+        neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + z);
+        neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + (z + 1));
+        neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + z);
+        neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + (z - 1));
+        neighborKeys.push(x + 1 + DELIM + y + DELIM + (z + 1));
+        neighborKeys.push(x + 1 + DELIM + y + DELIM + (z - 1));
+        neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + (z - 1));
+        neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + (z + 1));
+
+        neighborKeys.push(x - 1 + DELIM + y + DELIM + z);
+        neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + z);
+        neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + (z + 1));
+        neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + z);
+        neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + (z - 1));
+        neighborKeys.push(x - 1 + DELIM + y + DELIM + (z + 1));
+        neighborKeys.push(x - 1 + DELIM + y + DELIM + (z - 1));
+        neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + (z - 1));
+        neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + (z + 1));
+
+        neighborKeys.push(x + DELIM + (y + 1) + DELIM + z);
+        neighborKeys.push(x + DELIM + (y + 1) + DELIM + (z + 1));
+        neighborKeys.push(x + DELIM + (y - 1) + DELIM + z);
+        neighborKeys.push(x + DELIM + (y - 1) + DELIM + (z - 1));
+        neighborKeys.push(x + DELIM + y + DELIM + (z + 1));
+        neighborKeys.push(x + DELIM + y + DELIM + (z - 1));
+        neighborKeys.push(x + DELIM + (y + 1) + DELIM + (z - 1));
+        neighborKeys.push(x + DELIM + (y - 1) + DELIM + (z + 1));
+
+        return neighborKeys;
+    }
+
+    return { value: answer };
 }
-
-let answer = Array.from(cube.keys()).filter((key) => cube.get(key) === ACTIVE).length;
-
-function generateKey(x, y, z) {
-    return x + DELIM + y + DELIM + z;
-}
-
-function getNeighborKeys(x, y, z) {
-    let neighborKeys = [];
-
-    // TODO change this to use generateKey
-    neighborKeys.push(x + 1 + DELIM + y + DELIM + z);
-    neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + z);
-    neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + (z + 1));
-    neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + z);
-    neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + (z - 1));
-    neighborKeys.push(x + 1 + DELIM + y + DELIM + (z + 1));
-    neighborKeys.push(x + 1 + DELIM + y + DELIM + (z - 1));
-    neighborKeys.push(x + 1 + DELIM + (y + 1) + DELIM + (z - 1));
-    neighborKeys.push(x + 1 + DELIM + (y - 1) + DELIM + (z + 1));
-
-    neighborKeys.push(x - 1 + DELIM + y + DELIM + z);
-    neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + z);
-    neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + (z + 1));
-    neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + z);
-    neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + (z - 1));
-    neighborKeys.push(x - 1 + DELIM + y + DELIM + (z + 1));
-    neighborKeys.push(x - 1 + DELIM + y + DELIM + (z - 1));
-    neighborKeys.push(x - 1 + DELIM + (y + 1) + DELIM + (z - 1));
-    neighborKeys.push(x - 1 + DELIM + (y - 1) + DELIM + (z + 1));
-
-    neighborKeys.push(x + DELIM + (y + 1) + DELIM + z);
-    neighborKeys.push(x + DELIM + (y + 1) + DELIM + (z + 1));
-    neighborKeys.push(x + DELIM + (y - 1) + DELIM + z);
-    neighborKeys.push(x + DELIM + (y - 1) + DELIM + (z - 1));
-    neighborKeys.push(x + DELIM + y + DELIM + (z + 1));
-    neighborKeys.push(x + DELIM + y + DELIM + (z - 1));
-    neighborKeys.push(x + DELIM + (y + 1) + DELIM + (z - 1));
-    neighborKeys.push(x + DELIM + (y - 1) + DELIM + (z + 1));
-
-    return neighborKeys;
-}
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
--- 2020/js/Day17/problem2.js
+++ 2020/js/Day17/problem2.js
@@ -1,74 +1,73 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '17', '2'];
+module.exports = { solve: solve };
 
-const [INACTIVE, ACTIVE] = ['.', '#'];
-const DELIM = '.';
-const Z_START = 0;
-const A_START = 0;
+function solve({ lines, rawData }) {
+    const [INACTIVE, ACTIVE] = ['.', '#'];
+    const DELIM = '.';
+    const Z_START = 0;
+    const A_START = 0;
 
-const ROUNDS = 6;
+    const ROUNDS = 6;
 
-let cube = new Map();
-const data = fr.getInput(year, day).forEach((x, row) => {
-    x = x.split('');
-    x.forEach((val, col) => {
-        cube.set(generateKey(col, row, Z_START, A_START), val);
-        getNeighborKeys(col, row, Z_START, A_START).forEach((key) => {
-            if (cube.get(key) == null) {
-                cube.set(key, INACTIVE);
-            }
+    let cube = new Map();
+    lines.forEach((x, row) => {
+        x = x.split('');
+        x.forEach((val, col) => {
+            cube.set(generateKey(col, row, Z_START, A_START), val);
+            getNeighborKeys(col, row, Z_START, A_START).forEach((key) => {
+                if (cube.get(key) == null) {
+                    cube.set(key, INACTIVE);
+                }
+            });
         });
     });
-});
 
-for (let i = 0; i < ROUNDS; i++) {
-    let next = new Map();
-    cube.forEach((val, key) => {
-        let [x, y, z, a] = key.split(DELIM).map((n) => parseInt(n));
-        let neighbors = getNeighborKeys(x, y, z, a);
-        let activeNeighbors = neighbors.filter((k) => cube.get(k) === ACTIVE);
+    for (let i = 0; i < ROUNDS; i++) {
+        let next = new Map();
+        cube.forEach((val, key) => {
+            let [x, y, z, a] = key.split(DELIM).map((n) => parseInt(n));
+            let neighbors = getNeighborKeys(x, y, z, a);
+            let activeNeighbors = neighbors.filter((k) => cube.get(k) === ACTIVE);
 
-        if (val === ACTIVE && activeNeighbors.length !== 2 && activeNeighbors.length !== 3) {
-            next.set(generateKey(x, y, z, a), INACTIVE);
-        } else if (val === INACTIVE && activeNeighbors.length === 3) {
-            next.set(generateKey(x, y, z, a), ACTIVE);
-        } else {
-            next.set(generateKey(x, y, z, a), val);
-        }
-
-        neighbors.forEach((key) => {
-            if (next.get(key) == null) {
-                next.set(key, INACTIVE);
+            if (val === ACTIVE && activeNeighbors.length !== 2 && activeNeighbors.length !== 3) {
+                next.set(generateKey(x, y, z, a), INACTIVE);
+            } else if (val === INACTIVE && activeNeighbors.length === 3) {
+                next.set(generateKey(x, y, z, a), ACTIVE);
+            } else {
+                next.set(generateKey(x, y, z, a), val);
             }
+
+            neighbors.forEach((key) => {
+                if (next.get(key) == null) {
+                    next.set(key, INACTIVE);
+                }
+            });
         });
-    });
-    cube = next;
-}
+        cube = next;
+    }
 
-let answer = Array.from(cube.keys()).filter((key) => cube.get(key) === ACTIVE).length;
+    const answer = Array.from(cube.keys()).filter((key) => cube.get(key) === ACTIVE).length;
 
-function generateKey(x, y, z, a) {
-    return x + DELIM + y + DELIM + z + DELIM + a;
-}
+    function generateKey(x, y, z, a) {
+        return x + DELIM + y + DELIM + z + DELIM + a;
+    }
 
-function getNeighborKeys(x, y, z, a) {
-    let neighborKeys = [];
+    function getNeighborKeys(x, y, z, a) {
+        let neighborKeys = [];
 
-    for (let i = x - 1; i <= x + 1; i++) {
-        for (let j = y - 1; j <= y + 1; j++) {
-            for (let k = z - 1; k <= z + 1; k++) {
-                for (let l = a - 1; l <= a + 1; l++) {
-                    if (!(i === x && j === y && k === z && l === a)) {
-                        neighborKeys.push(generateKey(i, j, k, l));
+        for (let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                for (let k = z - 1; k <= z + 1; k++) {
+                    for (let l = a - 1; l <= a + 1; l++) {
+                        if (!(i === x && j === y && k === z && l === a)) {
+                            neighborKeys.push(generateKey(i, j, k, l));
+                        }
                     }
                 }
             }
         }
+
+        return neighborKeys;
     }
 
-    return neighborKeys;
+    return { value: answer };
 }
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
