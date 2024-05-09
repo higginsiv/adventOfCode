04842934f837ea3be319c3908832fa4db50be01c
William Higgins <higginsiv@gmail.com>
2024-05-08 20:46:42 -0400

    2020 21 refactor for test

--- 2020/js/Day21/problem2.js
+++ 2020/js/Day21/problem2.js
@@ -1,61 +1,60 @@
-console.time();
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2020', '21', '2'];
+module.exports = { solve: solve };
 
-let allIngredients = [];
-let allAllergens = new Set();
-let allergenToPossible = new Map();
+function solve({ lines, rawData }) {
+    let allIngredients = [];
+    let allAllergens = new Set();
+    let allergenToPossible = new Map();
 
-const data = fr.getInput(year, day).forEach((line) => {
-    line = line.replace(')', '');
-    line = line.split(' (contains ');
-    let ingredients = line[0].split(' ');
-    let allergens = line[1].split(', ');
+    lines.forEach((line) => {
+        line = line.replace(')', '');
+        line = line.split(' (contains ');
+        let ingredients = line[0].split(' ');
+        let allergens = line[1].split(', ');
 
-    allIngredients = allIngredients.concat(ingredients);
-    allAllergens = new Set([...allAllergens, ...allergens]);
+        allIngredients = allIngredients.concat(ingredients);
+        allAllergens = new Set([...allAllergens, ...allergens]);
 
-    allergens.forEach((allergen) => {
-        if (allergenToPossible.get(allergen) == null) {
-            allergenToPossible.set(allergen, new Set(ingredients));
-        } else {
+        allergens.forEach((allergen) => {
+            if (allergenToPossible.get(allergen) == null) {
+                allergenToPossible.set(allergen, new Set(ingredients));
+            } else {
+                let possible = allergenToPossible.get(allergen);
+                possible.forEach((poss) => {
+                    if (!ingredients.includes(poss)) {
+                        possible.delete(poss);
+                    }
+                });
+            }
+        });
+    });
+
+    let answer = [];
+    let allergens = [...allAllergens];
+    allergens.sort();
+
+    let cont = true;
+    while (cont) {
+        cont = false;
+        allergens.forEach((allergen) => {
             let possible = allergenToPossible.get(allergen);
-            possible.forEach((poss) => {
-                if (!ingredients.includes(poss)) {
-                    possible.delete(poss);
-                }
-            });
-        }
-    });
-});
+            if (possible.size === 1) {
+                let toDel = [...possible][0];
+                allergens.forEach((a) => {
+                    let p = allergenToPossible.get(a);
+                    if (p.size !== 1) {
+                        cont = true;
+                        p.delete(toDel);
+                    }
+                });
+            }
+        });
+    }
 
-let answer = [];
-let allergens = [...allAllergens];
-allergens.sort();
-
-let cont = true;
-while (cont) {
-    cont = false;
     allergens.forEach((allergen) => {
-        let possible = allergenToPossible.get(allergen);
-        if (possible.size === 1) {
-            let toDel = [...possible][0];
-            allergens.forEach((a) => {
-                let p = allergenToPossible.get(a);
-                if (p.size !== 1) {
-                    cont = true;
-                    p.delete(toDel);
-                }
-            });
-        }
+        answer.push([...allergenToPossible.get(allergen)][0]);
     });
+
+    answer = answer.join();
+
+    return { value: answer };
 }
-
-allergens.forEach((allergen) => {
-    answer.push([...allergenToPossible.get(allergen)][0]);
-});
-
-answer = answer.join();
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
-console.timeEnd();
