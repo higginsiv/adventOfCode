f24ab7ff88f7eaf3ad3f2767d5ea26daeab01f76
William Higgins <higginsiv@gmail.com>
2024-05-12 14:42:51 -0400

    2021 2 refactor for tests

--- 2021/js/Day02/problem1.js
+++ 2021/js/Day02/problem1.js
@@ -1,18 +1,20 @@
-const fr = require('../../../tools/fileReader');
-const data = fr.getInput('2021', '02');
+module.exports = { solve: solve };
 
-let horizontal = 0;
-let depth = 0;
+function solve({ lines, rawData }) {
+    let horizontal = 0;
+    let depth = 0;
 
-data.forEach(function (element) {
-    let movement = element.split(' ');
-    if (movement[0] === 'forward') {
-        horizontal += parseInt(movement[1]);
-    } else if (movement[0] === 'down') {
-        depth += parseInt(movement[1]);
-    } else if (movement[0] === 'up') {
-        depth -= parseInt(movement[1]);
-    }
-});
+    lines.forEach(function (element) {
+        let movement = element.split(' ');
+        if (movement[0] === 'forward') {
+            horizontal += parseInt(movement[1]);
+        } else if (movement[0] === 'down') {
+            depth += parseInt(movement[1]);
+        } else if (movement[0] === 'up') {
+            depth -= parseInt(movement[1]);
+        }
+    });
 
-console.log('Day 2 Puzzle 1: ' + horizontal * depth);
+    const answer = horizontal * depth;
+    return { value: answer };
+}
--- 2021/js/Day02/problem2.js
+++ 2021/js/Day02/problem2.js
@@ -1,20 +1,22 @@
-const fr = require('../../../tools/fileReader');
-const data = fr.getInput('2021', '02');
+module.exports = { solve: solve };
 
-let horizontal = 0;
-let depth = 0;
-let aim = 0;
+function solve({ lines, rawData }) {
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
 
-data.forEach(function (element) {
-    let movement = element.split(' ');
-    if (movement[0] === 'forward') {
-        horizontal += parseInt(movement[1]);
-        depth += aim * parseInt(movement[1]);
-    } else if (movement[0] === 'down') {
-        aim += parseInt(movement[1]);
-    } else if (movement[0] === 'up') {
-        aim -= parseInt(movement[1]);
-    }
-});
+    lines.forEach((element) => {
+        let movement = element.split(' ');
+        if (movement[0] === 'forward') {
+            horizontal += parseInt(movement[1]);
+            depth += aim * parseInt(movement[1]);
+        } else if (movement[0] === 'down') {
+            aim += parseInt(movement[1]);
+        } else if (movement[0] === 'up') {
+            aim -= parseInt(movement[1]);
+        }
+    });
 
-console.log('Day 2 Puzzle 2: ' + horizontal * depth);
+    const answer = horizontal * depth;
+    return { value: answer };
+}
