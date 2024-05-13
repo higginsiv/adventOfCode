8738135b42c3b4d80b64a92e96a5a570b5929528
William Higgins <higginsiv@gmail.com>
2024-05-13 18:31:44 -0400

    2021 3 refactor for test

--- 2021/js/Day03/problem1.js
+++ 2021/js/Day03/problem1.js
@@ -1,31 +1,33 @@
-const fr = require('../../../tools/fileReader');
-const data = fr.getInput('2021', '03');
+module.exports = { solve: solve };
 
-let gamma = [];
-let epsilon = [];
-let digitCounter = [];
+function solve({ lines, rawData }) {
+    let gamma = [];
+    let epsilon = [];
+    let digitCounter = [];
 
-data.forEach((x) => {
-    x.split('').forEach((y, index) => {
-        if (digitCounter[index] == null) {
-            digitCounter[index] = {
-                numZero: 0,
-                numOne: 0,
-            };
-        }
-        if (y === '0') {
-            digitCounter[index].numZero++;
-        } else {
-            digitCounter[index].numOne++;
-        }
+    lines.forEach((x) => {
+        x.split('').forEach((y, index) => {
+            if (digitCounter[index] == null) {
+                digitCounter[index] = {
+                    numZero: 0,
+                    numOne: 0,
+                };
+            }
+            if (y === '0') {
+                digitCounter[index].numZero++;
+            } else {
+                digitCounter[index].numOne++;
+            }
+        });
     });
-});
 
-digitCounter.forEach((x, index) => {
-    gamma[index] = x.numZero > x.numOne ? 0 : 1;
-    epsilon[index] = gamma[index] * -1 + 1;
-});
+    digitCounter.forEach((x, index) => {
+        gamma[index] = x.numZero > x.numOne ? 0 : 1;
+        epsilon[index] = gamma[index] * -1 + 1;
+    });
 
-let gammaDecimal = parseInt(gamma.join(''), 2);
-let epsilonDecimal = parseInt(epsilon.join(''), 2);
-console.log('Day 3 Puzzle 1: ' + gammaDecimal * epsilonDecimal);
+    let gammaDecimal = parseInt(gamma.join(''), 2);
+    let epsilonDecimal = parseInt(epsilon.join(''), 2);
+    const answer = gammaDecimal * epsilonDecimal;
+    return { value: answer };
+}
--- 2021/js/Day03/problem2.js
+++ 2021/js/Day03/problem2.js
@@ -1,69 +1,70 @@
-const fr = require('../../../tools/fileReader');
-const data = fr.getInput('2021', '03');
+module.exports = { solve: solve };
 
-let oxygen = data;
-let iteration = 0;
-while (oxygen.length > 1) {
-    let digitCounter = getDigitCounts(oxygen);
-    let comparisonDigit =
-        digitCounter[iteration].numOne === digitCounter[iteration].numZero ||
-        digitCounter[iteration].numOne > digitCounter[iteration].numZero
-            ? 1
-            : 0;
-
-    oxygen = oxygen.filter((binaryString) => binaryString[iteration] == comparisonDigit);
-    iteration++;
-}
-
-let carbon = data;
-iteration = 0;
-
-while (oxygen.length > 1 || carbon.length > 1) {
-    if (oxygen.length > 1) {
-        let oxDigitCounter = getDigitCounts(oxygen);
+function solve({ lines, rawData }) {
+    let oxygen = [...lines];
+    let iteration = 0;
+    while (oxygen.length > 1) {
+        let digitCounter = getDigitCounts(oxygen);
         let comparisonDigit =
-            oxDigitCounter[iteration].numOne === oxDigitCounter[iteration].numZero ||
-            oxDigitCounter[iteration].numOne > oxDigitCounter[iteration].numZero
+            digitCounter[iteration].numOne === digitCounter[iteration].numZero ||
+            digitCounter[iteration].numOne > digitCounter[iteration].numZero
                 ? 1
                 : 0;
 
         oxygen = oxygen.filter((binaryString) => binaryString[iteration] == comparisonDigit);
+        iteration++;
     }
 
-    if (carbon.length > 1) {
-        let coDigitCounter = getDigitCounts(carbon);
-        let comparisonDigit =
-            coDigitCounter[iteration].numOne === coDigitCounter[iteration].numZero ||
-            coDigitCounter[iteration].numOne > coDigitCounter[iteration].numZero
-                ? 0
-                : 1;
-        carbon = carbon.filter((binaryString) => binaryString[iteration] == comparisonDigit);
+    let carbon = [...lines];
+    iteration = 0;
+
+    while (oxygen.length > 1 || carbon.length > 1) {
+        if (oxygen.length > 1) {
+            let oxDigitCounter = getDigitCounts(oxygen);
+            let comparisonDigit =
+                oxDigitCounter[iteration].numOne === oxDigitCounter[iteration].numZero ||
+                oxDigitCounter[iteration].numOne > oxDigitCounter[iteration].numZero
+                    ? 1
+                    : 0;
+
+            oxygen = oxygen.filter((binaryString) => binaryString[iteration] == comparisonDigit);
+        }
+
+        if (carbon.length > 1) {
+            let coDigitCounter = getDigitCounts(carbon);
+            let comparisonDigit =
+                coDigitCounter[iteration].numOne === coDigitCounter[iteration].numZero ||
+                coDigitCounter[iteration].numOne > coDigitCounter[iteration].numZero
+                    ? 0
+                    : 1;
+            carbon = carbon.filter((binaryString) => binaryString[iteration] == comparisonDigit);
+        }
+        iteration++;
     }
-    iteration++;
-}
 
-let oxygenDecimal = parseInt(oxygen, 2);
-let carbonDecimal = parseInt(carbon, 2);
+    let oxygenDecimal = parseInt(oxygen, 2);
+    let carbonDecimal = parseInt(carbon, 2);
+    const answer = oxygenDecimal * carbonDecimal;
 
-console.log('Day 3 Puzzle 2: ' + oxygenDecimal * carbonDecimal);
-
-function getDigitCounts(data) {
-    let digitCounter = [];
-    data.forEach((binaryString) => {
-        binaryString.split('').forEach((digit, index) => {
-            if (digitCounter[index] == null) {
-                digitCounter[index] = {
-                    numZero: 0,
-                    numOne: 0,
-                };
-            }
-            if (digit === '0') {
-                digitCounter[index].numZero++;
-            } else {
-                digitCounter[index].numOne++;
-            }
+    function getDigitCounts(data) {
+        let digitCounter = [];
+        data.forEach((binaryString) => {
+            binaryString.split('').forEach((digit, index) => {
+                if (digitCounter[index] == null) {
+                    digitCounter[index] = {
+                        numZero: 0,
+                        numOne: 0,
+                    };
+                }
+                if (digit === '0') {
+                    digitCounter[index].numZero++;
+                } else {
+                    digitCounter[index].numOne++;
+                }
+            });
         });
-    });
 
-    return digitCounter;
+        return digitCounter;
+    }
+    return { value: answer };
 }
