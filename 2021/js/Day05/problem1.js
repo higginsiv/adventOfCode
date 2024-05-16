15aa9362d12e972fdb3e707ac13b7be2357626ea
William Higgins <higginsiv@gmail.com>
2024-05-15 21:15:00 -0400

    2021 5 refactor for test

--- 2021/js/Day05/problem1.js
+++ 2021/js/Day05/problem1.js
@@ -1,68 +1,70 @@
-const fr = require('../../../tools/fileReader');
-const tool = require('./tools.js');
+module.exports = { solve: solve };
 
-const data = fr.getInput('2021', '05');
-// const data = fr.getInput("2021",5, /\r?\n/, 'testInput.txt');
+function solve({ lines, rawData }) {
+    const tool = require('./tools.js');
 
-let grid = [];
+    const data = lines;
 
-for (let i = 0; i < data.length; i++) {
-    const [point1p, point2p, horLine, vertLine] = tool.getPoints(data[i]);
+    let grid = [];
 
-    if (!horLine && !vertLine) {
-        // Only consider horizontal or vertical lines, no diagonals
-        continue;
+    for (let i = 0; i < data.length; i++) {
+        const [point1p, point2p, horLine, vertLine] = tool.getPoints(data[i]);
+
+        if (!horLine && !vertLine) {
+            // Only consider horizontal or vertical lines, no diagonals
+            continue;
+        }
+
+        let point1;
+        let point2;
+
+        if (horLine) {
+            if (point1p[0] < point2p[0]) {
+                point1 = point1p;
+                point2 = point2p;
+            } else {
+                point1 = point2p;
+                point2 = point1p;
+            }
+
+            for (let i = point1[0]; i <= point2[0]; i++) {
+                if (grid[i] == null) {
+                    grid[i] = [];
+                }
+                if (grid[i][point1[1]] == null) {
+                    grid[i][point1[1]] = 0;
+                }
+                grid[i][point1[1]]++;
+            }
+        } else if (vertLine) {
+            if (point1p[1] < point2p[1]) {
+                point1 = point1p;
+                point2 = point2p;
+            } else {
+                point1 = point2p;
+                point2 = point1p;
+            }
+
+            for (let i = point1[1]; i <= point2[1]; i++) {
+                if (grid[point1[0]] == null) {
+                    grid[point1[0]] = [];
+                }
+                if (grid[point1[0]][i] == null) {
+                    grid[point1[0]][i] = 0;
+                }
+
+                grid[point1[0]][i]++;
+            }
+        }
     }
 
-    let point1;
-    let point2;
-
-    if (horLine) {
-        if (point1p[0] < point2p[0]) {
-            point1 = point1p;
-            point2 = point2p;
-        } else {
-            point1 = point2p;
-            point2 = point1p;
-        }
-
-        for (let i = point1[0]; i <= point2[0]; i++) {
-            if (grid[i] == null) {
-                grid[i] = [];
+    let num = 0;
+    grid.forEach((x) => {
+        x.forEach((y) => {
+            if (y > 1) {
+                num++;
             }
-            if (grid[i][point1[1]] == null) {
-                grid[i][point1[1]] = 0;
-            }
-            grid[i][point1[1]]++;
-        }
-    } else if (vertLine) {
-        if (point1p[1] < point2p[1]) {
-            point1 = point1p;
-            point2 = point2p;
-        } else {
-            point1 = point2p;
-            point2 = point1p;
-        }
-
-        for (let i = point1[1]; i <= point2[1]; i++) {
-            if (grid[point1[0]] == null) {
-                grid[point1[0]] = [];
-            }
-            if (grid[point1[0]][i] == null) {
-                grid[point1[0]][i] = 0;
-            }
-
-            grid[point1[0]][i]++;
-        }
-    }
-}
-
-let num = 0;
-grid.forEach((x) => {
-    x.forEach((y) => {
-        if (y > 1) {
-            num++;
-        }
+        });
     });
-});
-console.log('Day 5 Puzzle 1: ' + num);
+    return { value: num };
+}
