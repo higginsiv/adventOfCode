2f533afa5c3bc0f5f7ca3302fb9cd31974372b21
William Higgins <higginsiv@gmail.com>
2024-05-16 21:09:41 -0400

    2021 6 refactor for tests

--- 2021/js/Day06/problem1.js
+++ 2021/js/Day06/problem1.js
@@ -1,20 +1,24 @@
-const fr = require('../../../tools/fileReader');
-let data = fr.getInput('2021', '06', ',').map((x) => parseInt(x));
+module.exports = { solve: solve };
 
-const numOfDays = 80;
-const fishReset = 6;
-const fishNew = 8;
+function solve({ lines, rawData }) {
+    let data = rawData.split(',').map((x) => parseInt(x));
 
-for (let i = 0; i < numOfDays; i++) {
-    let newFish = [];
-    data.forEach((fish, index) => {
-        data[index]--;
-        if (data[index] === -1) {
-            data[index] = fishReset;
-            newFish = newFish.concat([fishNew]);
-        }
-    });
-    data = data.concat(newFish);
+    const numOfDays = 80;
+    const fishReset = 6;
+    const fishNew = 8;
+
+    for (let i = 0; i < numOfDays; i++) {
+        let newFish = [];
+        data.forEach((fish, index) => {
+            data[index]--;
+            if (data[index] === -1) {
+                data[index] = fishReset;
+                newFish = newFish.concat([fishNew]);
+            }
+        });
+        data = data.concat(newFish);
+    }
+
+    const answer = data.length;
+    return { value: answer };
 }
-
-console.log('Day 6 Puzzle 1: ' + data.length);
--- 2021/js/Day06/problem2.js
+++ 2021/js/Day06/problem2.js
@@ -1,33 +1,35 @@
-const fr = require('../../../tools/fileReader');
-let data = fr.getInput('2021', '06', ',').map((x) => parseInt(x));
+module.exports = { solve: solve };
 
-let fishDays = [];
+function solve({ lines, rawData }) {
+    let data = rawData.split(',').map((x) => parseInt(x));
 
-data.forEach((x) => {
-    if (fishDays[x] == null) {
-        fishDays[x] = [0];
-    }
-    fishDays[x]++;
-});
+    let fishDays = [];
 
-const numOfDays = 256;
-
-for (let i = 0; i < numOfDays; i++) {
-    let newFish = Array(9).fill(0);
-    fishDays.forEach((fish, index) => {
-        if (index === 0) {
-            newFish[6] = fish;
-            newFish[8] = fish;
-        } else {
-            newFish[index - 1] += fish;
+    data.forEach((x) => {
+        if (fishDays[x] == null) {
+            fishDays[x] = [0];
         }
+        fishDays[x]++;
     });
 
-    fishDays = newFish;
+    const numOfDays = 256;
+
+    for (let i = 0; i < numOfDays; i++) {
+        let newFish = Array(9).fill(0);
+        fishDays.forEach((fish, index) => {
+            if (index === 0) {
+                newFish[6] = fish;
+                newFish[8] = fish;
+            } else {
+                newFish[index - 1] += fish;
+            }
+        });
+
+        fishDays = newFish;
+    }
+
+    const result = fishDays.reduce((previous, current) => {
+        return previous + current;
+    }, 0);
+    return { value: result };
 }
-
-const result = fishDays.reduce((previous, current) => {
-    return previous + current;
-}, 0);
-
-console.log('Day 6 Puzzle 2: ' + result);
