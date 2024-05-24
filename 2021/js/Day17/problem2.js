e4fb034e9a0a1672c877b1b6c9c390ff79c8062d
William Higgins <higginsiv@gmail.com>
2024-05-23 21:31:14 -0400

    2021 17 refactor for test

--- 2021/js/Day17/problem2.js
+++ 2021/js/Day17/problem2.js
@@ -1,88 +1,91 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2021', '17', '2'];
-const [MIN_X, MAX_X, MIN_Y, MAX_Y] = fr.getInput(year, day).map((x) => {
-    x = x.replace('target area: x=', '');
-    x = x.replaceAll('..', ' ');
-    x = x.replace(',', '');
-    x = x.replace('y=', '');
-    x = x.split(' ').map((y) => parseInt(y));
-    return x;
-})[0];
+module.exports = { solve: solve };
 
-const [xStart, yStart] = [0, 0];
+function solve({ lines, rawData }) {
+    const [MIN_X, MAX_X, MIN_Y, MAX_Y] = lines.map((x) => {
+        x = x.replace('target area: x=', '');
+        x = x.replaceAll('..', ' ');
+        x = x.replace(',', '');
+        x = x.replace('y=', '');
+        x = x.split(' ').map((y) => parseInt(y));
+        return x;
+    })[0];
 
-class Velocity {
-    xs = new Set();
-    y;
-    steps;
-    constructor(y, steps) {
-        this.y = y;
-        this.steps = steps;
-    }
-}
+    const [xStart, yStart] = [0, 0];
 
-const yVelMax = Math.abs(MIN_Y);
-const yVelMin = MIN_Y;
-
-let velocities = [];
-let stepsToXs = new Map();
-let uniqueVelocities = new Set();
-for (let yVel = yVelMin; yVel <= yVelMax; yVel++) {
-    let vel = yVel;
-    let pos = yStart;
-    let steps = 0;
-    while (true) {
-        steps++;
-        pos += vel;
-        if (pos >= MIN_Y && pos <= MAX_Y) {
-            velocities.push(new Velocity(yVel, steps));
+    class Velocity {
+        xs = new Set();
+        y;
+        steps;
+        constructor(y, steps) {
+            this.y = y;
+            this.steps = steps;
         }
-        if (pos <= MIN_Y) {
-            break;
-        }
-        vel--;
     }
-}
 
-velocities.forEach((vel) => {
-    let cachedXs = stepsToXs.get(vel.steps);
-    if (cachedXs != null) {
-        vel.xs = cachedXs;
-    } else {
-        for (let x = 0; x <= MAX_X; x++) {
-            const xVel = findXPosition(x, vel.steps);
-            if (xVel >= MIN_X && xVel <= MAX_X) {
-                vel.xs.add(x);
+    const yVelMax = Math.abs(MIN_Y);
+    const yVelMin = MIN_Y;
+
+    let velocities = [];
+    let stepsToXs = new Map();
+    let uniqueVelocities = new Set();
+    for (let yVel = yVelMin; yVel <= yVelMax; yVel++) {
+        let vel = yVel;
+        let pos = yStart;
+        let steps = 0;
+        while (true) {
+            steps++;
+            pos += vel;
+            if (pos >= MIN_Y && pos <= MAX_Y) {
+                velocities.push(new Velocity(yVel, steps));
             }
-        }
-        stepsToXs.set(vel.steps, vel.xs);
-    }
-    vel.xs.forEach((xVel) => {
-        uniqueVelocities.add(xVel + '.' + vel.y);
-    });
-});
-
-function findXPosition(vel, steps) {
-    let pos = xStart;
-    while (steps > 0) {
-        pos += vel;
-        if (vel > 0) {
+            if (pos <= MIN_Y) {
+                break;
+            }
             vel--;
         }
-        steps--;
     }
-    return pos;
-}
 
-/** Idk the math name but imagine a factorial that adds intstead of multiplying */
-function factorialAdditionWithLimits(int, limit) {
-    let res = 0;
-    while (int > 0 && limit > 0) {
-        res += int;
-        int--;
-        limit--;
+    velocities.forEach((vel) => {
+        let cachedXs = stepsToXs.get(vel.steps);
+        if (cachedXs != null) {
+            vel.xs = cachedXs;
+        } else {
+            for (let x = 0; x <= MAX_X; x++) {
+                const xVel = findXPosition(x, vel.steps);
+                if (xVel >= MIN_X && xVel <= MAX_X) {
+                    vel.xs.add(x);
+                }
+            }
+            stepsToXs.set(vel.steps, vel.xs);
+        }
+        vel.xs.forEach((xVel) => {
+            uniqueVelocities.add(xVel + '.' + vel.y);
+        });
+    });
+
+    function findXPosition(vel, steps) {
+        let pos = xStart;
+        while (steps > 0) {
+            pos += vel;
+            if (vel > 0) {
+                vel--;
+            }
+            steps--;
+        }
+        return pos;
     }
-    return res;
-}
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + uniqueVelocities.size);
+    /** Idk the math name but imagine a factorial that adds intstead of multiplying */
+    function factorialAdditionWithLimits(int, limit) {
+        let res = 0;
+        while (int > 0 && limit > 0) {
+            res += int;
+            int--;
+            limit--;
+        }
+        return res;
+    }
+
+    const answer = uniqueVelocities.size;
+    return { value: answer };
+}
