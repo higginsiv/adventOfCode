915ceb607b8a1650a97ba022fd339ae6c4430880
William Higgins <higginsiv@gmail.com>
2024-05-24 22:38:58 -0400

    2022 1 refactor for test

--- 2022/js/Day01/problem1.js
+++ 2022/js/Day01/problem1.js
@@ -1,24 +1,24 @@
-const fr = require('../../../tools/fileReader');
-const { EOL } = require('os');
+module.exports = { solve: solve };
 
-const year = '2022';
-const day = '01';
-const data = fr.getInput(year, day);
+function solve({ lines, rawData }) {
+    const data = lines;
 
-let elfCalories = [0];
-let elfIndex = 0;
-let calorieMax = 0;
+    let elfCalories = [0];
+    let elfIndex = 0;
+    let calorieMax = 0;
 
-data.forEach((element) => {
-    if (element == '') {
-        if (elfCalories[elfIndex] > calorieMax) {
-            calorieMax = elfCalories[elfIndex];
+    data.forEach((element) => {
+        if (element == '') {
+            if (elfCalories[elfIndex] > calorieMax) {
+                calorieMax = elfCalories[elfIndex];
+            }
+            elfIndex++;
+            elfCalories[elfIndex] = 0;
+        } else {
+            elfCalories[elfIndex] += parseInt(element);
         }
-        elfIndex++;
-        elfCalories[elfIndex] = 0;
-    } else {
-        elfCalories[elfIndex] += parseInt(element);
-    }
-});
+    });
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle 1: ' + calorieMax);
+    const answer = calorieMax;
+    return { value: answer };
+}
--- 2022/js/Day01/problem2.js
+++ 2022/js/Day01/problem2.js
@@ -1,26 +1,24 @@
-const fr = require('../../../tools/fileReader');
-const { EOL } = require('os');
+module.exports = { solve: solve };
 
-const year = '2022';
-const day = '01';
-const data = fr.getInput(year, day);
+function solve({ lines, rawData }) {
+    const data = lines;
 
-let elfCalories = [0];
-let elfIndex = 0;
-
-data.forEach((element) => {
-    if (element == '') {
-        elfIndex++;
-        elfCalories[elfIndex] = 0;
-    } else {
-        elfCalories[elfIndex] += parseInt(element);
-    }
-});
-
-elfCalories.sort((a, b) => {
-    return b - a;
-});
-
-let calorieTotal = elfCalories[0] + elfCalories[1] + elfCalories[2];
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle 2: ' + calorieTotal);
+    let elfCalories = [0];
+    let elfIndex = 0;
+    
+    data.forEach((element) => {
+        if (element == '') {
+            elfIndex++;
+            elfCalories[elfIndex] = 0;
+        } else {
+            elfCalories[elfIndex] += parseInt(element);
+        }
+    });
+    
+    elfCalories.sort((a, b) => {
+        return b - a;
+    });
+    
+    const calorieTotal = elfCalories[0] + elfCalories[1] + elfCalories[2];
+    return { value: calorieTotal };
+}
