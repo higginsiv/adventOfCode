527b6536280cf1966dff20b50122035dffbf786b
William Higgins <higginsiv@gmail.com>
2024-05-25 22:20:27 -0400

    2022 2 refactor

---
+++ 2022/js/Day02/tests/2022-02.test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2022', '02');
+
+describe(`2022 Day 02`, () => {
+    test('Part 1', () => {
+        expect(part1(data).value).toBe(8890);
+    });
+
+    test('Part 2', () => {
+        expect(part2(data).value).toBe(10238);
+    });
+});
