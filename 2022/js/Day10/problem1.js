70d791a5fa4ac3964f630ef2e402dac9cfa2705e
William Higgins <higginsiv@gmail.com>
2024-05-29 18:43:37 -0400

    2022 10 refactor

--- 2022/js/Day10/problem1.js
+++ 2022/js/Day10/problem1.js
@@ -1,43 +1,46 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2022', '10', '1'];
-const data = fr.getInput(year, day).map((x) => {
-    return x.split(' ').map((y) => {
-        if (y === 'noop') {
-            return 1;
-        } else if (y === 'addx') {
-            return 2;
-        } else {
-            return parseInt(y);
-        }
+module.exports = { solve: solve };
+
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => {
+        return x.split(' ').map((y) => {
+            if (y === 'noop') {
+                return 1;
+            } else if (y === 'addx') {
+                return 2;
+            } else {
+                return parseInt(y);
+            }
+        });
     });
-});
 
-const maxCycle = 220;
-const cycleInc = 40;
-const cycleStart = 20;
+    const maxCycle = 220;
+    const cycleInc = 40;
+    const cycleStart = 20;
 
-let sumCycles = [];
-for (let i = cycleStart; i <= maxCycle; i += cycleInc) {
-    sumCycles.push(i);
-}
-
-let x = 1;
-let cycle = 1;
-let cmdLine = 0;
-let sums = 0;
-
-while (cycle <= maxCycle) {
-    cycle++;
-
-    data[cmdLine][0]--;
-    if (data[cmdLine][0] <= 0) {
-        x += data[cmdLine][1] == null ? 0 : data[cmdLine][1];
-        cmdLine++;
+    let sumCycles = [];
+    for (let i = cycleStart; i <= maxCycle; i += cycleInc) {
+        sumCycles.push(i);
     }
 
-    if (sumCycles.includes(cycle)) {
-        sums += x * cycle;
-    }
-}
+    let x = 1;
+    let cycle = 1;
+    let cmdLine = 0;
+    let sums = 0;
 
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + sums);
+    while (cycle <= maxCycle) {
+        cycle++;
+
+        data[cmdLine][0]--;
+        if (data[cmdLine][0] <= 0) {
+            x += data[cmdLine][1] == null ? 0 : data[cmdLine][1];
+            cmdLine++;
+        }
+
+        if (sumCycles.includes(cycle)) {
+            sums += x * cycle;
+        }
+    }
+
+    const answer = sums;
+    return { value: answer };
+}
---
+++ 2022/js/Day10/tests/2022-10.test.js
@@ -0,0 +1,25 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2022', '10');
+
+describe(`2022 Day 10`, () => {
+    test('Part 1', () => {
+        expect(part1(data).value).toBe(14920);
+    });
+
+    test('Part 2', () => {
+        // Spells BUCACBUZ (but oddly with the bottom row missing)
+        const rawAnswer = part2(data).value[0];
+        let actual = '';
+        rawAnswer.forEach((m) => {
+            m.forEach((c) => {
+                actual += c;
+            });
+            actual += '_';
+        });
+
+        expect(actual).toBe('_###..#..#..##...##...##..###..#..#.####._#..#.#..#.#..#.#..#.#..#.#..#.#..#....#._###..#..#.#....#..#.#....###..#..#...#.._#..#.#..#.#....####.#....#..#.#..#..#..._#..#.#..#.#..#.#..#.#..#.#..#.#..#.#...._');
+    });
+});
