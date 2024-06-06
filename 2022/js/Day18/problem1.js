c34fc2d2fe3c15793b5eb60e79e485e763aef989
William Higgins <higginsiv@gmail.com>
2024-06-05 21:42:55 -0400

    2022 Day 18: Operation Order refactor for test

--- 2022/js/Day18/problem1.js
+++ 2022/js/Day18/problem1.js
@@ -1,27 +1,29 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2022', '18', '1'];
-const data = fr.getInput(year, day).map((x) => x.split(',').map((y) => parseInt(y)));
+module.exports = { solve: solve };
 
-let jsonData = data.slice().map((x) => JSON.stringify(x));
+function solve({ lines, rawData }) {
+    const data = lines.map((x) => x.split(',').map((y) => parseInt(y)));
 
-let surfaceArea = 0;
-data.forEach((p) => {
-    const [x, y, z] = p;
-    let freeSides = 6;
-    let left = [x - 1, y, z];
-    let right = [x + 1, y, z];
-    let front = [x, y, z + 1];
-    let back = [x, y, z - 1];
-    let top = [x, y + 1, z];
-    let bottom = [x, y - 1, z];
-    let sides = [left, right, front, back, top, bottom];
-    for (let i = 0; i < sides.length; i++) {
-        if (jsonData.includes(JSON.stringify(sides[i]))) {
-            freeSides--;
+    let jsonData = data.slice().map((x) => JSON.stringify(x));
+
+    let surfaceArea = 0;
+    data.forEach((p) => {
+        const [x, y, z] = p;
+        let freeSides = 6;
+        let left = [x - 1, y, z];
+        let right = [x + 1, y, z];
+        let front = [x, y, z + 1];
+        let back = [x, y, z - 1];
+        let top = [x, y + 1, z];
+        let bottom = [x, y - 1, z];
+        let sides = [left, right, front, back, top, bottom];
+        for (let i = 0; i < sides.length; i++) {
+            if (jsonData.includes(JSON.stringify(sides[i]))) {
+                freeSides--;
+            }
         }
-    }
-    surfaceArea += freeSides;
-});
+        surfaceArea += freeSides;
+    });
 
-let answer = surfaceArea;
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
+    const answer = surfaceArea;
+    return { value: answer };
+}
