7250f0746614ca46ba43c5667b2d8925fcc5e8fc
William Higgins <higginsiv@gmail.com>
2024-06-07 22:35:53 -0400

    2022 20 refactor

--- 2022/js/Day20/problem2.js
+++ 2022/js/Day20/problem2.js
@@ -1,43 +1,44 @@
-const fr = require('../../../tools/fileReader');
-const [year, day, part] = ['2022', '20', '2'];
-const KEY = 811589153;
+module.exports = { solve: solve };
 
-class Point {
-    value;
-    index;
-    constructor(value, index) {
-        this.value = value;
-        this.index = index;
+function solve({ lines, rawData }) {
+    const KEY = 811589153;
+
+    class Point {
+        value;
+        index;
+        constructor(value, index) {
+            this.value = value;
+            this.index = index;
+        }
     }
-}
 
-let data = fr.getInput(year, day).map((x, index) => new Point(parseInt(x) * KEY, index));
+    let data = lines.map((x, index) => new Point(parseInt(x) * KEY, index));
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < data.length; j++) {
-        for (let k = 0; k < data.length; k++) {
-            if (data[k].index === j) {
-                let el = data.slice(k, k + 1)[0];
-                data.splice(k, 1);
-                data.splice((k + el.value) % data.length, 0, el);
-                break;
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < data.length; j++) {
+            for (let k = 0; k < data.length; k++) {
+                if (data[k].index === j) {
+                    let el = data.slice(k, k + 1)[0];
+                    data.splice(k, 1);
+                    data.splice((k + el.value) % data.length, 0, el);
+                    break;
+                }
             }
         }
     }
+
+    let zeroIndex = data.reduce((ans, x, index) => {
+        if (x.value === 0) {
+            return index - 1;
+        } else {
+            return ans;
+        }
+    }, 0);
+
+    const stack = data[((zeroIndex + 1000) % data.length) + 1].value;
+    const dubs = data[((zeroIndex + 2000) % data.length) + 1].value;
+    const andre = data[((zeroIndex + 3000) % data.length) + 1].value;
+
+    const answer = stack + dubs + andre;
+    return { value: answer };
 }
-
-let zeroIndex = data.reduce((ans, x, index) => {
-    if (x.value === 0) {
-        return index - 1;
-    } else {
-        return ans;
-    }
-}, 0);
-
-const stack = data[((zeroIndex + 1000) % data.length) + 1].value;
-const dubs = data[((zeroIndex + 2000) % data.length) + 1].value;
-const andre = data[((zeroIndex + 3000) % data.length) + 1].value;
-
-let answer = stack + dubs + andre;
-
-console.log('Year ' + year + ' Day ' + day + ' Puzzle ' + part + ': ' + answer);
