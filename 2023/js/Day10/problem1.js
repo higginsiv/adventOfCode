a01a7e6513ae4f81403a3ca8268276c1dcfe7e95
William Higgins <higginsiv@gmail.com>
2024-06-15 21:40:01 -0400

    2023 10 refactor

--- 2023/js/Day10/problem1.js
+++ 2023/js/Day10/problem1.js
@@ -1,216 +1,217 @@
-const fr = require('../../../tools/fileReader');
-const [YEAR, DAY, PART] = ['2023', '10', '1'];
-let DATA = fr.getInput(YEAR, DAY).map((x) => x.split(''));
-const START = 'S';
+module.exports = { solve: solve };
 
-const [NORTH_SOUTH, EAST_WEST, NORTH_EAST, NORTH_WEST, SOUTH_WEST, SOUTH_EAST, GROUND] = [
-    '|',
-    '-',
-    'L',
-    'J',
-    '7',
-    'F',
-    '.',
-];
-const PIPES = [NORTH_SOUTH, EAST_WEST, NORTH_EAST, NORTH_WEST, SOUTH_WEST, SOUTH_EAST];
-let answer;
+function solve({ lines, rawData }) {
+    let DATA = lines.map((x) => x.split(''));
+    const START = 'S';
 
-class State {
-    x;
-    y;
-    pipe;
-    visited;
-    startPipe;
-    constructor(x, y, pipe, visited, startPipe) {
-        this.x = x;
-        this.y = y;
-        this.pipe = pipe;
-        this.visited = visited;
-        this.startPipe = startPipe;
-    }
-}
+    const [NORTH_SOUTH, EAST_WEST, NORTH_EAST, NORTH_WEST, SOUTH_WEST, SOUTH_EAST, GROUND] = [
+        '|',
+        '-',
+        'L',
+        'J',
+        '7',
+        'F',
+        '.',
+    ];
+    const PIPES = [NORTH_SOUTH, EAST_WEST, NORTH_EAST, NORTH_WEST, SOUTH_WEST, SOUTH_EAST];
+    let answer;
 
-// Create a new row filled with '.' characters
-let newRow = new Array(DATA[0].length + 2).fill('.');
-
-// Create a new 2D array with the new rows at the start and end
-let newData = [newRow];
-
-// Add the existing rows, with '.' characters at the start and end
-for (let row of DATA) {
-    newData.push(['.'].concat(row, ['.']));
-}
-
-// Add the last new row
-newData.push(newRow);
-
-// Replace the old DATA array with the new one
-DATA = newData;
-
-let startX;
-let startY;
-
-let queue = [];
-
-for (let i = 0; i < DATA.length; i++) {
-    let row = DATA[i];
-    if (row.includes(START)) {
-        startX = i;
-        startY = row.indexOf(START);
-        PIPES.forEach((pipe) => {
-            queue.push(
-                new State(
-                    i,
-                    row.indexOf(START),
-                    pipe,
-                    [getLocationKey(i, row.indexOf(START))],
-                    pipe,
-                ),
-            );
-        });
-        break;
-    }
-}
-
-while (queue.length > 0) {
-    let state = queue.shift();
-
-    if (state.x === startX && state.y === startY && state.visited.length > 1) {
-        answer = Math.floor(state.visited.length / 2);
-        break;
-    }
-
-    let coordsToLookAt = [];
-    let northNeighbor = DATA[state.x - 1][state.y];
-    let eastNeighbor = DATA[state.x][state.y + 1];
-    let southNeighbor = DATA[state.x + 1][state.y];
-    let westNeighbor = DATA[state.x][state.y - 1];
-
-    switch (state.pipe) {
-        case NORTH_SOUTH:
-            if (
-                [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
-                ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
-                    northNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
-            }
-            if (
-                [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
-                ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
-                    southNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
-            }
-            break;
-        case EAST_WEST:
-            if (
-                [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
-                ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
-                    eastNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
-            }
-            if (
-                [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
-                ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
-                    westNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
-            }
-            break;
-        case NORTH_EAST:
-            if (
-                [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
-                ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
-                    northNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
-            }
-            if (
-                [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
-                ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
-                    eastNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
-            }
-            break;
-        case NORTH_WEST:
-            if (
-                [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
-                ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
-                    northNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
-            }
-            if (
-                [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
-                ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
-                    westNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
-            }
-            break;
-        case SOUTH_WEST:
-            if (
-                [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
-                ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
-                    southNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
-            }
-            if (
-                [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
-                ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
-                    westNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
-            }
-            break;
-        case SOUTH_EAST:
-            if (
-                [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
-                ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
-                    southNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
-            }
-            if (
-                [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
-                ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
-                    eastNeighbor === START)
-            ) {
-                coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
-            }
-            break;
-        case GROUND:
-            break;
-    }
-
-    coordsToLookAt.forEach((coords) => {
-        let [x, y] = coords.split('|').map((x) => parseInt(x));
-
-        if (
-            !state.visited.includes(coords) ||
-            (coords === getLocationKey(startX, startY) && state.visited.length > 2)
-        ) {
-            let pipe = DATA[x][y];
-            let visited = state.visited.slice();
-            visited.push(coords);
-            queue.push(new State(x, y, pipe, visited, state.startPipe));
+    class State {
+        x;
+        y;
+        pipe;
+        visited;
+        startPipe;
+        constructor(x, y, pipe, visited, startPipe) {
+            this.x = x;
+            this.y = y;
+            this.pipe = pipe;
+            this.visited = visited;
+            this.startPipe = startPipe;
         }
-    });
-}
-
-function getLocationKey(x, y) {
-    return `${x}|${y}`;
-}
-
-function printGrid() {
-    for (let row of DATA) {
-        console.log(row.join(''));
     }
-    console.log('-------------------');
-}
 
-console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
+    // Create a new row filled with '.' characters
+    let newRow = new Array(DATA[0].length + 2).fill('.');
+
+    // Create a new 2D array with the new rows at the start and end
+    let newData = [newRow];
+
+    // Add the existing rows, with '.' characters at the start and end
+    for (let row of DATA) {
+        newData.push(['.'].concat(row, ['.']));
+    }
+
+    // Add the last new row
+    newData.push(newRow);
+
+    // Replace the old DATA array with the new one
+    DATA = newData;
+
+    let startX;
+    let startY;
+
+    let queue = [];
+
+    for (let i = 0; i < DATA.length; i++) {
+        let row = DATA[i];
+        if (row.includes(START)) {
+            startX = i;
+            startY = row.indexOf(START);
+            PIPES.forEach((pipe) => {
+                queue.push(
+                    new State(
+                        i,
+                        row.indexOf(START),
+                        pipe,
+                        [getLocationKey(i, row.indexOf(START))],
+                        pipe,
+                    ),
+                );
+            });
+            break;
+        }
+    }
+
+    while (queue.length > 0) {
+        let state = queue.shift();
+
+        if (state.x === startX && state.y === startY && state.visited.length > 1) {
+            answer = Math.floor(state.visited.length / 2);
+            break;
+        }
+
+        let coordsToLookAt = [];
+        let northNeighbor = DATA[state.x - 1][state.y];
+        let eastNeighbor = DATA[state.x][state.y + 1];
+        let southNeighbor = DATA[state.x + 1][state.y];
+        let westNeighbor = DATA[state.x][state.y - 1];
+
+        switch (state.pipe) {
+            case NORTH_SOUTH:
+                if (
+                    [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
+                    ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
+                        northNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
+                }
+                if (
+                    [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
+                    ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
+                        southNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
+                }
+                break;
+            case EAST_WEST:
+                if (
+                    [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
+                    ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
+                        eastNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
+                }
+                if (
+                    [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
+                    ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
+                        westNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
+                }
+                break;
+            case NORTH_EAST:
+                if (
+                    [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
+                    ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
+                        northNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
+                }
+                if (
+                    [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
+                    ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
+                        eastNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
+                }
+                break;
+            case NORTH_WEST:
+                if (
+                    [NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(northNeighbor) ||
+                    ([NORTH_SOUTH, SOUTH_EAST, SOUTH_WEST].includes(state.startPipe) &&
+                        northNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x - 1, state.y));
+                }
+                if (
+                    [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
+                    ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
+                        westNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
+                }
+                break;
+            case SOUTH_WEST:
+                if (
+                    [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
+                    ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
+                        southNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
+                }
+                if (
+                    [EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(westNeighbor) ||
+                    ([EAST_WEST, NORTH_EAST, SOUTH_EAST].includes(state.startPipe) &&
+                        westNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y - 1));
+                }
+                break;
+            case SOUTH_EAST:
+                if (
+                    [NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(southNeighbor) ||
+                    ([NORTH_SOUTH, NORTH_EAST, NORTH_WEST].includes(state.startPipe) &&
+                        southNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x + 1, state.y));
+                }
+                if (
+                    [EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(eastNeighbor) ||
+                    ([EAST_WEST, NORTH_WEST, SOUTH_WEST].includes(state.startPipe) &&
+                        eastNeighbor === START)
+                ) {
+                    coordsToLookAt.push(getLocationKey(state.x, state.y + 1));
+                }
+                break;
+            case GROUND:
+                break;
+        }
+
+        coordsToLookAt.forEach((coords) => {
+            let [x, y] = coords.split('|').map((x) => parseInt(x));
+
+            if (
+                !state.visited.includes(coords) ||
+                (coords === getLocationKey(startX, startY) && state.visited.length > 2)
+            ) {
+                let pipe = DATA[x][y];
+                let visited = state.visited.slice();
+                visited.push(coords);
+                queue.push(new State(x, y, pipe, visited, state.startPipe));
+            }
+        });
+    }
+
+    function getLocationKey(x, y) {
+        return `${x}|${y}`;
+    }
+
+    function printGrid() {
+        for (let row of DATA) {
+            console.log(row.join(''));
+        }
+        console.log('-------------------');
+    }
+    return { value: answer };
+}
---
+++ 2023/js/Day10/tests/2023-10.test.js
@@ -0,0 +1,15 @@
+const fr = require('../../../../tools/fileReader');
+const { solve: part1 } = require(`../problem1.js`);
+const { solve: part2 } = require(`../problem2.js`);
+
+const data = fr.getInputForFunction('2023', '10');
+
+describe(`2023 Day 10`, () => {
+    test('Part 1', () => {
+        expect(part1(data).value).toBe(6773);
+    });
+
+    test('Part 2', () => {
+        expect(part2(data).value).toBe(493);
+    });
+});
