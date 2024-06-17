cb443b29f8da55038b9d7d6abcce6c4d4699ac7b
William Higgins <higginsiv@gmail.com>
2024-06-17 19:11:47 -0400

    2023 Day13 refactor

--- 2023/js/Day13/problem1.js
+++ 2023/js/Day13/problem1.js
@@ -1,79 +1,80 @@
-const fr = require('../../../tools/fileReader');
-const [YEAR, DAY, PART] = ['2023', '13', '1'];
-const EOL = require('os').EOL;
-let answer = fr
-    .getInput(YEAR, DAY, EOL + EOL)
-    .map((x) => {
-        let grid = { rows: [], columns: [] };
+module.exports = { solve: solve };
 
-        x = x.split(EOL).map((x) => x.split(''));
+function solve({ lines, rawData }) {
+    const EOL = require('os').EOL;
+    const answer = rawData
+        .split(EOL + EOL)
+        .map((x) => {
+            let grid = { rows: [], columns: [] };
 
-        for (let i = 0; i < x.length; i++) {
-            let row = 0;
-            for (let j = 0; j < x[i].length; j++) {
-                row |= x[i][j] === '#' ? 1 << (x[i].length - j - 1) : 0;
-            }
-            grid.rows.push(row);
-        }
+            x = x.split(EOL).map((x) => x.split(''));
 
-        for (let j = 0; j < x[0].length; j++) {
-            let column = 0;
             for (let i = 0; i < x.length; i++) {
-                column |= x[i][j] === '#' ? 1 << (x.length - i - 1) : 0;
+                let row = 0;
+                for (let j = 0; j < x[i].length; j++) {
+                    row |= x[i][j] === '#' ? 1 << (x[i].length - j - 1) : 0;
+                }
+                grid.rows.push(row);
             }
-            grid.columns.push(column);
-        }
-        return grid;
-    })
-    .reduce((total, curr) => {
-        let rowMirrorIndex = findMirror(curr.rows);
-        let columnMirrorIndex = findMirror(curr.columns);
 
-        if (rowMirrorIndex !== -1) {
-            return total + 100 * rowMirrorIndex;
-        }
+            for (let j = 0; j < x[0].length; j++) {
+                let column = 0;
+                for (let i = 0; i < x.length; i++) {
+                    column |= x[i][j] === '#' ? 1 << (x.length - i - 1) : 0;
+                }
+                grid.columns.push(column);
+            }
+            return grid;
+        })
+        .reduce((total, curr) => {
+            let rowMirrorIndex = findMirror(curr.rows);
+            let columnMirrorIndex = findMirror(curr.columns);
 
-        if (columnMirrorIndex !== -1) {
-            return total + columnMirrorIndex;
-        }
+            if (rowMirrorIndex !== -1) {
+                return total + 100 * rowMirrorIndex;
+            }
 
-        console.log('hmmmm');
-        return total;
-    }, 0);
+            if (columnMirrorIndex !== -1) {
+                return total + columnMirrorIndex;
+            }
 
-function findMirror(row) {
-    for (let i = 0; i < row.length - 1; i++) {
-        if (i == 0) {
-            for (let j = row.length - 1; j > i; j--) {
-                if (row[i] !== row[j]) {
+            console.log('hmmmm');
+            return total;
+        }, 0);
+
+    function findMirror(row) {
+        for (let i = 0; i < row.length - 1; i++) {
+            if (i == 0) {
+                for (let j = row.length - 1; j > i; j--) {
+                    if (row[i] !== row[j]) {
+                        continue;
+                    }
+                    let subRow = row.slice(i, j + 1);
+                    if (isMirror(subRow)) {
+                        return i + Math.floor(subRow.length / 2);
+                    }
+                }
+            } else {
+                if (row[i] !== row[row.length - 1]) {
                     continue;
                 }
-                let subRow = row.slice(i, j + 1);
+                let subRow = row.slice(i);
                 if (isMirror(subRow)) {
                     return i + Math.floor(subRow.length / 2);
                 }
             }
-        } else {
-            if (row[i] !== row[row.length - 1]) {
-                continue;
-            }
-            let subRow = row.slice(i);
-            if (isMirror(subRow)) {
-                return i + Math.floor(subRow.length / 2);
+        }
+        return -1;
+    }
+
+    function isMirror(arr) {
+        let len = arr.length;
+        for (let i = 0; i < Math.floor(len / 2); i++) {
+            if (arr[i] !== arr[len - i - 1]) {
+                return false;
             }
         }
+        return true;
     }
-    return -1;
+    return { value: answer };
 }
-
-function isMirror(arr) {
-    let len = arr.length;
-    for (let i = 0; i < Math.floor(len / 2); i++) {
-        if (arr[i] !== arr[len - i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
