60bdc6cbdb5d8a8332030689aca1f19784f55c6d
William Higgins <higginsiv@gmail.com>
2024-06-18 18:58:29 -0400

    2023 14 refactor

--- 2023/js/Day14/problem1.js
+++ 2023/js/Day14/problem1.js
@@ -1,21 +1,23 @@
-const fr = require('../../../tools/fileReader');
-const [YEAR, DAY, PART] = ['2023', '14', '1'];
-const DATA = fr.getInput(YEAR, DAY).map((x) => x.split(''));
-const [ROUND, CUBE] = ['O', '#'];
+module.exports = { solve: solve };
 
-let totalLoad = 0;
-for (let j = 0; j < DATA[0].length; j++) {
-    let currentBlocker = -1;
-    for (let i = 0; i < DATA.length; i++) {
-        const rock = DATA[i][j];
-        if (rock === CUBE) {
-            currentBlocker = i;
-        } else if (rock === ROUND) {
-            currentBlocker += 1;
-            totalLoad += DATA.length - currentBlocker;
+function solve({ lines, rawData }) {
+    const DATA = lines.map((x) => x.split(''));
+    const [ROUND, CUBE] = ['O', '#'];
+
+    let totalLoad = 0;
+    for (let j = 0; j < DATA[0].length; j++) {
+        let currentBlocker = -1;
+        for (let i = 0; i < DATA.length; i++) {
+            const rock = DATA[i][j];
+            if (rock === CUBE) {
+                currentBlocker = i;
+            } else if (rock === ROUND) {
+                currentBlocker += 1;
+                totalLoad += DATA.length - currentBlocker;
+            }
         }
     }
-}
 
-let answer = totalLoad;
-console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
+    const answer = totalLoad;
+    return { value: answer };
+}
--- 2023/js/Day14/problem2.js
+++ 2023/js/Day14/problem2.js
@@ -1,144 +1,146 @@
-const fr = require('../../../tools/fileReader');
-const [YEAR, DAY, PART] = ['2023', '14', '2'];
-const DATA = fr.getInput(YEAR, DAY).map((x) => x.split(''));
-const [ROUND, CUBE] = ['O', '#'];
-const DIRECTIONS = ['N', 'W', 'S', 'E'];
-const CYCLES = 1000000000;
+module.exports = { solve: solve };
 
-let grid = DATA;
+function solve({ lines, rawData }) {
+    const DATA = lines.map((x) => x.split(''));
+    const [ROUND, CUBE] = ['O', '#'];
+    const DIRECTIONS = ['N', 'W', 'S', 'E'];
+    const CYCLES = 1000000000;
 
-let cycles = new Map();
+    let grid = DATA;
 
-let ignoreCycle = false;
-let i = 0;
-while (i < CYCLES) {
-    if (!ignoreCycle) {
-        let key = generateKey(grid);
-        if (cycles.has(key)) {
-            let originalIndex = cycles.get(key);
-            let cycleLength = i - originalIndex;
+    let cycles = new Map();
 
-            let fullCycles = Math.floor((CYCLES - originalIndex) / cycleLength);
-            i = originalIndex + fullCycles * cycleLength;
+    let ignoreCycle = false;
+    let i = 0;
+    while (i < CYCLES) {
+        if (!ignoreCycle) {
+            let key = generateKey(grid);
+            if (cycles.has(key)) {
+                let originalIndex = cycles.get(key);
+                let cycleLength = i - originalIndex;
 
-            if (i < CYCLES) {
-                i -= cycleLength;
+                let fullCycles = Math.floor((CYCLES - originalIndex) / cycleLength);
+                i = originalIndex + fullCycles * cycleLength;
+
+                if (i < CYCLES) {
+                    i -= cycleLength;
+                }
+
+                ignoreCycle = true;
+                continue;
+            } else {
+                cycles.set(key, i);
             }
+        }
 
-            ignoreCycle = true;
-            continue;
-        } else {
-            cycles.set(key, i);
+        DIRECTIONS.forEach((direction) => {
+            switch (direction) {
+                case 'N':
+                    grid = tiltNorth(grid);
+                    break;
+                case 'W':
+                    grid = tiltWest(grid);
+                    break;
+                case 'S':
+                    grid = tiltSouth(grid);
+                    break;
+                case 'E':
+                    grid = tiltEast(grid);
+                    break;
+            }
+        });
+
+        i++;
+    }
+
+    let totalLoad = 0;
+    for (let j = 0; j < grid[0].length; j++) {
+        for (let i = 0; i < grid.length; i++) {
+            const rock = grid[i][j];
+            if (rock === ROUND) {
+                totalLoad += grid.length - i;
+            }
         }
     }
 
-    DIRECTIONS.forEach((direction) => {
-        switch (direction) {
-            case 'N':
-                grid = tiltNorth(grid);
-                break;
-            case 'W':
-                grid = tiltWest(grid);
-                break;
-            case 'S':
-                grid = tiltSouth(grid);
-                break;
-            case 'E':
-                grid = tiltEast(grid);
-                break;
-        }
-    });
-
-    i++;
-}
-
-let totalLoad = 0;
-for (let j = 0; j < grid[0].length; j++) {
-    for (let i = 0; i < grid.length; i++) {
-        const rock = grid[i][j];
-        if (rock === ROUND) {
-            totalLoad += grid.length - i;
-        }
+    function tiltEast(grid) {
+        return grid.map((row) => {
+            let segments = row.join('').split('#');
+            return segments
+                .map((segment) => {
+                    let countO = (segment.match(/O/g) || []).length;
+                    let countDot = segment.length - countO;
+                    return '.'.repeat(countDot) + 'O'.repeat(countO);
+                })
+                .join('#')
+                .split('');
+        });
     }
-}
 
-function tiltEast(grid) {
-    return grid.map((row) => {
-        let segments = row.join('').split('#');
-        return segments
-            .map((segment) => {
-                let countO = (segment.match(/O/g) || []).length;
-                let countDot = segment.length - countO;
-                return '.'.repeat(countDot) + 'O'.repeat(countO);
+    function tiltNorth(grid) {
+        let transposedGrid = transpose(grid);
+        let result = transposedGrid.map((col) => {
+            let segments = col.join('').split('#');
+            return segments
+                .map((segment) => {
+                    let countO = (segment.match(/O/g) || []).length;
+                    let countDot = segment.length - countO;
+                    return 'O'.repeat(countO) + '.'.repeat(countDot);
+                })
+                .join('#')
+                .split('');
+        });
+        return transpose(result);
+    }
+
+    function tiltSouth(grid) {
+        let transposedGrid = transpose(grid);
+        let result = transposedGrid.map((col) => {
+            let segments = col.join('').split('#');
+            return segments
+                .map((segment) => {
+                    let countO = (segment.match(/O/g) || []).length;
+                    let countDot = segment.length - countO;
+                    return '.'.repeat(countDot) + 'O'.repeat(countO);
+                })
+                .join('#')
+                .split('');
+        });
+        return transpose(result);
+    }
+
+    function tiltWest(grid) {
+        return grid.map((row) => {
+            let segments = row.join('').split('#');
+            return segments
+                .map((segment) => {
+                    let countO = (segment.match(/O/g) || []).length;
+                    let countDot = segment.length - countO;
+                    return 'O'.repeat(countO) + '.'.repeat(countDot);
+                })
+                .join('#')
+                .split('');
+        });
+    }
+
+    function transpose(grid) {
+        return grid[0].map((_, i) => grid.map((row) => row[i]));
+    }
+
+    function generateKey(grid) {
+        return grid
+            .map((row) => {
+                row = row.map((x) => (x === null ? '.' : x));
+                return row.join('');
             })
-            .join('#')
-            .split('');
-    });
-}
+            .join('');
+    }
 
-function tiltNorth(grid) {
-    let transposedGrid = transpose(grid);
-    let result = transposedGrid.map((col) => {
-        let segments = col.join('').split('#');
-        return segments
-            .map((segment) => {
-                let countO = (segment.match(/O/g) || []).length;
-                let countDot = segment.length - countO;
-                return 'O'.repeat(countO) + '.'.repeat(countDot);
-            })
-            .join('#')
-            .split('');
-    });
-    return transpose(result);
-}
+    function printGrid(grid) {
+        grid.forEach((row) => console.log(row.join('')));
+        console.log();
+    }
 
-function tiltSouth(grid) {
-    let transposedGrid = transpose(grid);
-    let result = transposedGrid.map((col) => {
-        let segments = col.join('').split('#');
-        return segments
-            .map((segment) => {
-                let countO = (segment.match(/O/g) || []).length;
-                let countDot = segment.length - countO;
-                return '.'.repeat(countDot) + 'O'.repeat(countO);
-            })
-            .join('#')
-            .split('');
-    });
-    return transpose(result);
+    const answer = totalLoad;
+    return { value: answer };
 }
-
-function tiltWest(grid) {
-    return grid.map((row) => {
-        let segments = row.join('').split('#');
-        return segments
-            .map((segment) => {
-                let countO = (segment.match(/O/g) || []).length;
-                let countDot = segment.length - countO;
-                return 'O'.repeat(countO) + '.'.repeat(countDot);
-            })
-            .join('#')
-            .split('');
-    });
-}
-
-function transpose(grid) {
-    return grid[0].map((_, i) => grid.map((row) => row[i]));
-}
-
-function generateKey(grid) {
-    return grid
-        .map((row) => {
-            row = row.map((x) => (x === null ? '.' : x));
-            return row.join('');
-        })
-        .join('');
-}
-
-function printGrid(grid) {
-    grid.forEach((row) => console.log(row.join('')));
-    console.log();
-}
-
-let answer = totalLoad;
-console.log(`Year ${YEAR} Day ${DAY} Puzzle ${PART}: ${answer}`);
