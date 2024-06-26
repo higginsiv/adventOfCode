1147d81943bc1900a996256764631919a7e24322
William Higgins <higginsiv@gmail.com>
2024-06-26 15:53:36 -0400

    2023 22 refactor

--- 2023/js/Day22/problem2.js
+++ 2023/js/Day22/problem2.js
@@ -1,121 +1,119 @@
-const fr = require('../../../tools/fileReader');
-const OUTPUT = require('../../../tools/output');
-const [YEAR, DAY, PART] = ['2023', '22', '1'];
+module.exports = { solve: solve };
 
-let zToDroppedBricks = new Map();
-let brickSupports = new Map();
-let brickSupportedBy = new Map();
-const DATA = fr
-    .getInput(YEAR, DAY)
-    .map((line, index) => {
-        let matches = line.match(/(\d+)/g).map((x) => parseInt(x));
-        return [
-            { x: matches[0], y: matches[1], z: matches[2] },
-            { x: matches[3], y: matches[4], z: matches[5] },
-            index,
-        ].toSorted((a, b) => a.z - b.z);
-    })
-    .toSorted((a, b) => a[0].z - b[0].z);
+function solve({ lines, rawData }) {
+    let zToDroppedBricks = new Map();
+    let brickSupports = new Map();
+    let brickSupportedBy = new Map();
+    const DATA = lines
+        .map((line, index) => {
+            let matches = line.match(/(\d+)/g).map((x) => parseInt(x));
+            return [
+                { x: matches[0], y: matches[1], z: matches[2] },
+                { x: matches[3], y: matches[4], z: matches[5] },
+                index,
+            ].toSorted((a, b) => a.z - b.z);
+        })
+        .toSorted((a, b) => a[0].z - b[0].z);
 
-DATA.forEach((brick) => {
-    let dropping = true;
-    while (dropping) {
-        let overlap = false;
+    DATA.forEach((brick) => {
+        let dropping = true;
+        while (dropping) {
+            let overlap = false;
 
-        let startZ = brick[0].z;
-        let zDelta = brick[1].z - brick[0].z;
+            let startZ = brick[0].z;
+            let zDelta = brick[1].z - brick[0].z;
 
-        for (let z = startZ - 1; z > 0; z--) {
-            if (!zToDroppedBricks.has(z)) {
+            for (let z = startZ - 1; z > 0; z--) {
+                if (!zToDroppedBricks.has(z)) {
+                    brick[0].z--;
+                    brick[1].z--;
+                    continue;
+                }
+                let droppedBricks = zToDroppedBricks.get(z);
+                droppedBricks.forEach((droppedBrick) => {
+                    if (brickOverlaps(brick, droppedBrick)) {
+                        let droppedBrickKey = droppedBrick[2];
+                        let brickKey = brick[2];
+                        if (!zToDroppedBricks.has(z + zDelta + 1)) {
+                            zToDroppedBricks.set(z + zDelta + 1, new Set());
+                        }
+                        zToDroppedBricks.get(z + zDelta + 1).add(brick);
+
+                        if (!brickSupportedBy.has(brickKey)) {
+                            brickSupportedBy.set(brickKey, new Set());
+                        }
+                        brickSupportedBy.get(brickKey).add(droppedBrickKey);
+
+                        if (!brickSupports.has(droppedBrickKey)) {
+                            brickSupports.set(droppedBrickKey, new Set());
+                        }
+                        brickSupports.get(droppedBrickKey).add(brickKey);
+                        overlap = true;
+                    }
+                });
+                if (overlap) {
+                    dropping = false;
+                    break;
+                }
                 brick[0].z--;
                 brick[1].z--;
-                continue;
             }
-            let droppedBricks = zToDroppedBricks.get(z);
-            droppedBricks.forEach((droppedBrick) => {
-                if (brickOverlaps(brick, droppedBrick)) {
-                    let droppedBrickKey = droppedBrick[2];
-                    let brickKey = brick[2];
-                    if (!zToDroppedBricks.has(z + zDelta + 1)) {
-                        zToDroppedBricks.set(z + zDelta + 1, new Set());
-                    }
-                    zToDroppedBricks.get(z + zDelta + 1).add(brick);
+            if (!overlap) {
+                if (!zToDroppedBricks.has(zDelta + 1)) {
+                    zToDroppedBricks.set(zDelta + 1, new Set());
+                }
+                zToDroppedBricks.get(zDelta + 1).add(brick);
+                dropping = false;
+            }
+        }
+    });
 
-                    if (!brickSupportedBy.has(brickKey)) {
-                        brickSupportedBy.set(brickKey, new Set());
-                    }
-                    brickSupportedBy.get(brickKey).add(droppedBrickKey);
+    const answer = DATA.reduce((total, curr) => {
+        let deadBricks = new Set([curr[2]]);
+        totalBrickFalls(curr[2], deadBricks);
 
-                    if (!brickSupports.has(droppedBrickKey)) {
-                        brickSupports.set(droppedBrickKey, new Set());
-                    }
-                    brickSupports.get(droppedBrickKey).add(brickKey);
-                    overlap = true;
+        return total + deadBricks.size - 1;
+    }, 0);
+
+    function totalBrickFalls(brick, deadBricks) {
+        let currBrickSupports = brickSupports.get(brick);
+
+        if (currBrickSupports == null) {
+            return;
+        }
+
+        let localDeadBricks = new Set();
+        currBrickSupports.forEach((x) => {
+            supportedBy = brickSupportedBy.get(x);
+            let noLongerSupported = true;
+            supportedBy.forEach((y) => {
+                if (!deadBricks.has(y)) {
+                    noLongerSupported = false;
                 }
             });
-            if (overlap) {
-                dropping = false;
-                break;
-            }
-            brick[0].z--;
-            brick[1].z--;
-        }
-        if (!overlap) {
-            if (!zToDroppedBricks.has(zDelta + 1)) {
-                zToDroppedBricks.set(zDelta + 1, new Set());
-            }
-            zToDroppedBricks.get(zDelta + 1).add(brick);
-            dropping = false;
-        }
-    }
-});
 
-let answer = DATA.reduce((total, curr) => {
-    let deadBricks = new Set([curr[2]]);
-    totalBrickFalls(curr[2], deadBricks);
-
-    return total + deadBricks.size - 1;
-}, 0);
-
-function totalBrickFalls(brick, deadBricks) {
-    let currBrickSupports = brickSupports.get(brick);
-
-    if (currBrickSupports == null) {
-        return;
-    }
-
-    let localDeadBricks = new Set();
-    currBrickSupports.forEach((x) => {
-        supportedBy = brickSupportedBy.get(x);
-        let noLongerSupported = true;
-        supportedBy.forEach((y) => {
-            if (!deadBricks.has(y)) {
-                noLongerSupported = false;
+            if (noLongerSupported) {
+                localDeadBricks.add(x);
+                deadBricks.add(x);
             }
         });
 
-        if (noLongerSupported) {
-            localDeadBricks.add(x);
-            deadBricks.add(x);
+        localDeadBricks.forEach((x) => {
+            totalBrickFalls(x, deadBricks);
+        });
+        return;
+    }
+
+    function brickOverlaps(brick1, brick2) {
+        if (brick1[1].x < brick2[0].x || brick2[1].x < brick1[0].x) {
+            return false;
         }
-    });
 
-    localDeadBricks.forEach((x) => {
-        totalBrickFalls(x, deadBricks);
-    });
-    return;
-}
+        if (brick1[1].y < brick2[0].y || brick2[1].y < brick1[0].y) {
+            return false;
+        }
 
-function brickOverlaps(brick1, brick2) {
-    if (brick1[1].x < brick2[0].x || brick2[1].x < brick1[0].x) {
-        return false;
+        return true;
     }
-
-    if (brick1[1].y < brick2[0].y || brick2[1].y < brick1[0].y) {
-        return false;
-    }
-
-    return true;
+    return { value: answer };
 }
-
-OUTPUT.output(YEAR, DAY, PART, answer);
