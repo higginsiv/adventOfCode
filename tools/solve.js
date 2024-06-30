b1f4c9e1bd8bd50d5d5aa92159ab975799e0db98
William Higgins <higginsiv@gmail.com>
2024-06-29 22:33:18 -0400

    chore(): remove legacy from solver

--- tools/solve.js
+++ tools/solve.js
@@ -5,8 +5,6 @@ const { Solution, LogStrategy } = require('./solution');
 // Get year, day, and part from arguments
 let [YEAR, DAY, PART] = process.argv.slice(2);
 
-const LEGACY = process.env.npm_config_legacy;
-
 if (!YEAR || !DAY) {
     console.error('Usage: node solve.js <year> <day> [<part>]');
     process.exit(1);
@@ -28,31 +26,21 @@ if (!PART) {
 }
 
 parts.forEach(async (part) => {
-    if (LEGACY) {
-        // Get the path to the solution file
-        const path = `../${YEAR}/js/Day${DAY}/problem${part}.js`;
+    let data = fr.getInputForFunction(YEAR, DAY);
 
-        // run file at the path
-        console.time('Solve!');
-        require(path);
-        console.timeEnd('Solve!');
-    } else {
-        let data = fr.getInputForFunction(YEAR, DAY);
+    // Get the path to the solution file
+    const path = `../${YEAR}/js/Day${DAY}/problem${part}.js`;
+    const { solve } = require(path);
 
-        // Get the path to the solution file
-        const path = `../${YEAR}/js/Day${DAY}/problem${part}.js`;
-        const { solve } = require(path);
+    // Run file at the path
+    const timerKey = `${YEAR} Day ${DAY} Part ${part}`;
+    console.time(timerKey);
+    let answer = await solve(data);
+    console.timeEnd(timerKey);
 
-        // Run file at the path
-        const timerKey = `${YEAR} Day ${DAY} Part ${part}`;
-        console.time(timerKey);
-        let answer = await solve(data);
-        console.timeEnd(timerKey);
-
-        // Output the answer
-        if (!(answer instanceof Solution)) {
-            answer = new Solution(answer.value, new LogStrategy());
-        }
-        answer.output();
+    // Output the answer
+    if (!(answer instanceof Solution)) {
+        answer = new Solution(answer.value, new LogStrategy());
     }
+    answer.output();
 });
