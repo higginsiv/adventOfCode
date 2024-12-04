d61a0b1370b50402881d0f766a2f089fea5c8720
William Higgins <higginsiv@gmail.com>
2024-12-03 22:05:43 -0500

    feat(2024-3): fixed regex even though the input doesn't require it

--- 2024/js/Day03/problem1.js
+++ 2024/js/Day03/problem1.js
@@ -2,7 +2,7 @@ import { Solution } from '../../../tools/solution.js';
 
 export default function solve({ lines, rawData }) {
     const answer = rawData
-        .match(/mul\(\d+,\d+\)/g)
+        .match(/mul\(\d{1,3},\d{1,3}\)/g)
         .map((match) => {
             const [a, b] = match.match(/\d+/g);
             return a * b;
--- 2024/js/Day03/problem2.js
+++ 2024/js/Day03/problem2.js
@@ -1,7 +1,7 @@
 import { Solution } from '../../../tools/solution.js';
 
 export default function solve({ lines, rawData }) {
-    const matches = rawData.match(/mul\(\d+,\d+\)|don't\(\)|do\(\)/g);
+    const matches = rawData.match(/mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)/g);
 
     let answer = 0;
     let mulEnabled = true;
