6b8416a6ca5d17eba38feacd70c8cd4911be9fa1
William Higgins <higginsiv@gmail.com>
2024-12-05 00:32:27 -0500

    feat(2024-5): Complete Day 5

---
+++ 2024/js/Day05/problem1.js
@@ -0,0 +1,41 @@
+import { Solution } from '../../../tools/solution.js';
+import { EOL } from 'os';
+
+export default function solve({ lines, rawData }) {
+    let [rules, updates] = rawData.split(EOL + EOL);
+    let ruleToLaterRules = new Map();
+
+    rules = rules.split(EOL).map((rule) => {
+        rule = rule.split('|').map(Number);
+        return rule;
+    });
+
+    rules.forEach((rule) => {
+        const [before, after] = rule;
+        const laterRules = ruleToLaterRules.get(before) ?? [];
+        laterRules.push(after);
+        ruleToLaterRules.set(before, laterRules);
+    });
+
+    updates = updates.split(EOL).map((update) => update.split(',').map(Number));
+
+    const answer = updates.reduce((acc, update) => {
+        if (updateInOrder(update)) {
+            return acc + update[Math.floor(update.length / 2)];
+        }
+        return acc;
+    }, 0);
+
+    return new Solution(answer);
+
+    function updateInOrder(update) {
+        for (let i = 0; i < update.length; i++) {
+            for (let j = 0; j < i; j++) {
+                if (ruleToLaterRules.get(update[i])?.includes(update[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ 2024/js/Day05/problem2.js
@@ -0,0 +1,51 @@
+import { Solution } from '../../../tools/solution.js';
+import { EOL } from 'os';
+
+export default function solve({ lines, rawData }) {
+    let [rules, updates] = rawData.split(EOL + EOL);
+    let ruleToLaterRules = new Map();
+
+    rules = rules.split(EOL).map((rule) => {
+        rule = rule.split('|').map(Number);
+        return rule;
+    });
+
+    rules.forEach((rule) => {
+        const [before, after] = rule;
+        const laterRules = ruleToLaterRules.get(before) ?? [];
+        laterRules.push(after);
+        ruleToLaterRules.set(before, laterRules);
+    });
+
+    updates = updates.split(EOL).map((update) => update.split(',').map(Number));
+
+    let answer = 0;
+    updates.forEach((update) => {
+        if (!updateInOrder(update)) {
+            update.sort((a, b) => {
+                if (ruleToLaterRules.get(a)?.includes(b)) {
+                    return -1;
+                }
+                if (ruleToLaterRules.get(b)?.includes(a)) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            answer += update[Math.floor(update.length / 2)];
+        }
+    });
+
+    return new Solution(answer);
+
+    function updateInOrder(update) {
+        for (let i = 0; i < update.length; i++) {
+            for (let j = 0; j < i; j++) {
+                if (ruleToLaterRules.get(update[i])?.includes(update[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
