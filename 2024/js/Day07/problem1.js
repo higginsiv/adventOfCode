b4db5d98eb850b7132d6f1db2f16ea20473bfb83
William Higgins <higginsiv@gmail.com>
2024-12-07 00:51:49 -0500

    feat(2024-7): performance optimizations

--- 2024/js/Day07/problem1.js
+++ 2024/js/Day07/problem1.js
@@ -1,10 +1,6 @@
 import { Solution } from '#tools/solution.js';
-import { getCombos } from '#tools/math.js';
 
 export default function solve({ lines, rawData }) {
-    const [ADD, MULT] = [0, 1];
-    const comboCache = new Map();
-
     const answer = lines
         .map((line) => {
             line = line.match(/\d+/g).map(Number);
@@ -12,39 +8,37 @@ export default function solve({ lines, rawData }) {
         })
         .reduce((acc, line) => {
             const expected = line.shift();
-            const operatorCount = line.length - 1;
-            const combos = getCombosFromCache(operatorCount);
-            for (const combo of combos) {
-                let total = line[0];
-                for (let i = 0; i < operatorCount; i++) {
-                    switch (combo[i]) {
-                        case ADD:
-                            total += line[i + 1];
-                            break;
-                        case MULT:
-                            total *= line[i + 1];
-                            break;
-                    }
-                    
-                    if (total > expected) {
-                        break;
-                    }
-                }
-                if (total === expected) {
-                    return acc + total;
-                }
+
+            if (canReachExpectedTotal(line[0], 0, line, expected)) {
+                return acc + expected;
             }
             return acc;
         }, 0);
 
     return new Solution(answer);
 
-    function getCombosFromCache(operatorCount) {
-        if (comboCache.has(operatorCount)) {
-            return comboCache.get(operatorCount);
+    function canReachExpectedTotal(current, operatorCount, line, expected) {
+        if (operatorCount === line.length - 1) {
+            return current === expected;
         }
-        const combos = getCombos(operatorCount, 2);
-        comboCache.set(operatorCount, combos);
-        return combos;
+
+        if (current > expected) {
+            return false;
+        }
+
+        return (
+            canReachExpectedTotal(
+                current + line[operatorCount + 1],
+                operatorCount + 1,
+                line,
+                expected,
+            ) ||
+            canReachExpectedTotal(
+                current * line[operatorCount + 1],
+                operatorCount + 1,
+                line,
+                expected,
+            )
+        );
     }
 }
