8052e43b8415fb06ac2246dbfa845fdecb748508
William Higgins <higginsiv@gmail.com>
2024-12-19 00:14:05 -0500

    feat(2024-19): complete day 19

---
+++ 2024/js/Day19/problem2.js
@@ -0,0 +1,31 @@
+import { Solution } from '#tools/solution.js';
+import { EOL } from 'os';
+export default function solve({ lines, rawData }) {
+    rawData = rawData.split(EOL + EOL);
+    let towels = rawData[0].split(', ');
+    let displays = rawData[1].split(EOL);
+
+    let displayToValidCombinations = new Map();
+    const answer = displays.reduce((acc, display) => {
+        return acc + getValidCombinations(display);
+    }, 0);
+    return new Solution(answer);
+
+    function getValidCombinations(display) {
+        if (display.length === 0) {
+            return 1;
+        }
+
+        if (displayToValidCombinations.has(display)) {
+            return displayToValidCombinations.get(display);
+        }
+        const count = towels.reduce((acc, towel) => {
+            if (display.startsWith(towel)) {
+                return acc + getValidCombinations(display.slice(towel.length));
+            }
+            return acc;
+        }, 0);
+        displayToValidCombinations.set(display, count);
+        return count;
+    }
+}
