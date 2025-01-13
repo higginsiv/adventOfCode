5a1829abf566aab1bf3c0b0056220628eca8178e
William Higgins <higginsiv@gmail.com>
2025-01-12 22:04:45 -0500

    feat(2019-19): performance improvements from ~40s to ~1s

--- 2019/js/Day19/part2.js
+++ 2019/js/Day19/part2.js
@@ -2,23 +2,54 @@ import { IntCode } from '../common/IntCode.js';
 
 export default function solve({ lines, rawData }) {
     let x = 0;
-    let y = 10;
+    let y = 150;
 
     const corners = [
         [99, 0],
-        [99, 99],
-        [0, 99],
+        [99, -99],
+        [0, -99],
     ];
 
-    let baseMemory = rawData.split(',').map(Number);
+    const baseMemory = rawData.split(',').map(Number);
 
-    let hasHitBeamOnRow = false;
-    let furthestLeftX = 0;
     let input = [];
     let output = [];
     let ic = new IntCode(rawData, null, 0, input, output);
+
     while (true) {
+        // Find furthest left on row that has beam
         let hasBeam = false;
+        while (!hasBeam) {
+            hasBeam = checkPoint(x, y, input, ic);
+            if (!hasBeam) {
+                x++;
+            }
+        }
+
+        // Check other 3 corners
+        for (let i = 0; i < corners.length; i++) {
+            let [dx, dy] = corners[i];
+            if (!checkPoint(x + dx, y + dy, input, ic)) {
+                hasBeam = false;
+                break;
+            }
+        }
+
+        if (hasBeam) {
+            break;
+        }
+        y++;
+    }
+
+    // Subtract 99 from y to get the top left corner of the square
+    const answer = x * 10000 + (y - 99);
+    return { value: answer };
+
+    function checkPoint(x, y, input, ic) {
+        if (x < 0 || y < 0) {
+            return false;
+        }
+
         input.push(x);
         input.push(y);
         ic.setState({
@@ -27,48 +58,7 @@ export default function solve({ lines, rawData }) {
             pointer: 0,
         });
         let { output } = ic.run();
-        let result = output.pop();
-        if (result === 1) {
-            hasBeam = true;
-        }
+        return output.pop() === 1;
 
-        if (hasBeam) {
-            if (!hasHitBeamOnRow) {
-                furthestLeftX = x;
-            }
-            hasHitBeamOnRow = true;
-            let allCorners = true;
-            for (let i = 0; i < corners.length; i++) {
-                let corner = corners[i];
-                input.push(x + corner[0]);
-                input.push(y + corner[1]);
-                ic.setState({
-                    memory: baseMemory.slice(),
-                    relative: 0,
-                    pointer: 0,
-                });
-                let { output } = ic.run();
-                let result = output.pop();
-                if (result !== 1) {
-                    allCorners = false;
-                    break;
-                }
-            }
-            if (allCorners) {
-                break;
-            }
-            x++;
-        } else {
-            if (hasHitBeamOnRow) {
-                x = furthestLeftX;
-                hasHitBeamOnRow = false;
-                y++;
-            } else {
-                x++;
-            }
-        }
     }
-
-    const answer = x * 10000 + y;
-    return { value: answer };
 }
