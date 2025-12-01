8f654ca19bfc201368192cd5ada318778ed83b34
William Higgins <higginsiv@gmail.com>
2025-12-01 00:30:57 -0500

    feat(2025-1): complete Day 01 solutions and tests

---
+++ 2025/js/Day01/part1.js
@@ -0,0 +1,29 @@
+import { Solution } from '#tools/solution.js';
+
+export default function solve({ lines, rawData }) {
+    const MULTS = new Map([
+        ['R', 1],
+        ['L', -1]
+    ]);
+    let answer = 0;
+    let dial = 50;
+    lines.map(line => {
+        return {
+            dir: MULTS.get(line[0]),
+            value: parseInt(line.slice(1), 10)
+        }
+    }).forEach(({ dir, value }) => {
+        dial += dir * (value % 100);
+        if (dial < 0) {
+            dial += 100;
+        } else if (dial >= 100) {
+            dial -= 100;
+        } 
+        
+        if (dial === 0) {
+            answer++;
+        }
+    });
+
+    return new Solution(answer);
+}
